d3eb82b977503a5ba8233632443572749b06df8e
james <james@team-mediaportal.com>
2008-01-02 07:18:50 +0000

    Fixed AssemblyInfo.cs Changed txt and html names

--- MP-II/Build/Build Report - Release x86.bat
+++ MP-II/Build/Build Report - Release x86.bat
@@ -9,6 +9,6 @@ IF EXIST ..\MediaPortal.Tools\BuildReport\bin\Release\BuildReport.exe GOTO BUILT
 xcopy /Y ..\MediaPortal.Tools\BuildReport\css\*.* .\css\
 xcopy /Y ..\MediaPortal.Tools\BuildReport\images\*.* .\images\
 
-"%ProgramFiles%\Microsoft Visual Studio 8\Common7\IDE\devenv.com" /rebuild Release /projectconfig x86 ..\MediaPortal.sln > build-report.txt
+"%ProgramFiles%\Microsoft Visual Studio 8\Common7\IDE\devenv.com" /rebuild Release /projectconfig x86 ..\MediaPortal.sln > Build_Report-Release_x86.txt
 
-..\MediaPortal.Tools\BuildReport\bin\Release\BuildReport.exe build-report.txt build-report.html MediaPortal
+..\MediaPortal.Tools\BuildReport\bin\Release\BuildReport.exe Build_Report-Release_x86.txt Build_Report-Release_x86.html MediaPortal
--- MP-II/MediaPortal.Tools/BuildReport/Properties/AssemblyInfo.cs
+++ MP-II/MediaPortal.Tools/BuildReport/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("BuildReport")]
-[assembly: AssemblyCopyright("Copyright © .0")]
+[assembly: AssemblyCopyright("Copyright © Team MediaPortal")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.0.")]
-[assembly: AssemblyFileVersion("1.0.0.")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
