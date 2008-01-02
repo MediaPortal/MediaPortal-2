bb5bc0a01946c91d179e9bd0b8c829a70fcf9817
james <james@team-mediaportal.com>
2008-01-02 07:24:11 +0000

    Fixed

--- MP-II/MediaPortal.Tools/BuildReport/Properties/AssemblyInfo.cs
+++ MP-II/MediaPortal.Tools/BuildReport/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("2e29f232-d6d2-43e9-a6f1-2b07bf2a1c.0")]
+[assembly: Guid("2e29f232-d6d2-43e9-a6f1-f3fd6aeb2571")]
 
 // Version information for an assembly consists of the following four values:
 //
