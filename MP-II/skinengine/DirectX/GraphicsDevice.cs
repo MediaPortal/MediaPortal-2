661ca02b3839b5c22acec995035c47efcb077112
Erwin Beckers <dev@team-mediaportal.com>
2008-01-03 13:34:08 +0000

    MP now checks vga card for enough video memory & pixel shader support If not ok a message box is shown

--- MP-II/skinengine/DirectX/GraphicsDevice.cs
+++ MP-II/skinengine/DirectX/GraphicsDevice.cs
@@ -72,7 +72,7 @@ namespace SkinEngine
         tmpDev=null;
 
         int videoMemory = caps.HardwareCaps.VideoMemoryTotal / (1000 * 1000);
-        ServiceScope.Get<ILogger>().Info("Directx: Total Video Memory:{0}", videoMemory);
+        ServiceScope.Get<ILogger>().Info("Directx: Total Video Memory:{0} MB", videoMemory);
         if (videoMemory < 128)
         {
           string text = String.Format("MediaPortal-II needs a graphics card with at least 128 MB video memory\nYour card does only has {0} MB.\nMediaportal-II will continue but migh run slow", videoMemory);
