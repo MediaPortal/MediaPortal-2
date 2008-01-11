4829a2ec2a74a25c4302a043e8d3b91dd3782662
james <james@team-mediaportal.com>
2008-01-11 19:01:14 +0000

    Fixed build batches

--- MP-II/Build/VS2005_Build_Report_Release_x86.bat
+++ MP-II/Build/VS2005_Build_Report_Release_x86.bat
@@ -9,6 +9,6 @@ IF EXIST ..\MediaPortal.Tools\BuildReport\bin\Release\BuildReport.exe GOTO BUILT
 xcopy /Y ..\MediaPortal.Tools\BuildReport\css\*.* .\css\
 xcopy /Y ..\MediaPortal.Tools\BuildReport\images\*.* .\images\
 
-"%ProgramFiles%\Microsoft Visual Studio 8\Common7\IDE\devenv.com" /rebuild Release /projectconfig x86 ..\MediaPortal.sln > VS2005_Build_Report_Release_x86.txt
+"%ProgramFiles%\Microsoft Visual Studio 8\Common7\IDE\devenv.com" ..\MediaPortal.sln /Rebuild "Release|x86" > VS2005_Build_Report_Release_x86.txt
 
 ..\MediaPortal.Tools\BuildReport\bin\Release\BuildReport.exe /Input=VS2005_Build_Report_Release_x86.txt /Output=VS2005_Build_Report_Release_x86.html /Solution=MediaPortal.sln /Title="MediaPortal II - Build Report"
