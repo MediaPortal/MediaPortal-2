75fbbda2bace56cb9722aa5878aabf4399a832a3
james <james@team-mediaportal.com>
2008-01-26 08:35:59 +0000

    Fixed copyright notice

--- MP-II/MediaPortal.Services/PluginManager/PluginDetails/PluginManifest.cs
+++ MP-II/MediaPortal.Services/PluginManager/PluginDetails/PluginManifest.cs
@@ -1,50 +1,26 @@
 ﻿#region Copyright (C) 2007-2008 Team MediaPortal
 
 /*
-    Copyright (C) 2007-2008 Team MediaPortal
-    http://www.team-mediaportal.com
- 
-    This file is part of MediaPortal II
-
-    MediaPortal II is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    MediaPortal II is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with MediaPortal II.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
-#endregion
-
-#region Copyright (C) 2005-2008 Team MediaPortal
-
-/* 
- *	Copyright (C) 2005-2008 Team MediaPortal
- *	http://www.team-mediaportal.com
+ *  Copyright (C) 2007-2008 Team MediaPortal
+ *  http://www.team-mediaportal.com
  *
- *  This Program is free software; you can redistribute it and/or modify
+ *  This file is part of MediaPortal II
+ *
+ *  MediaPortal II is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2, or (at your option)
- *  any later version.
- *   
- *  This Program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- *  GNU General Public License for more details.
- *   
- *  You should have received a copy of the GNU General Public License
- *  along with GNU Make; see the file COPYING.  If not, write to
- *  the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA. 
- *  http://www.gnu.org/copyleft/gpl.html
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
  *
+ *  MediaPortal II is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU General Public License
+ *  along with MediaPortal II.  If not, see <http://www.gnu.org/licenses/>.
+ * 
  *  Code modified from SharpDevelop AddIn code
- *  Thanks goes to: Mike Kr�ger
+ *  Thanks goes to: Mike Krüger
  */
 
 #endregion
