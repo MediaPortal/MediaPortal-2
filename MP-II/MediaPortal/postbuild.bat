a2dba7ab56c3dfbf000c0cdf879bab21c1fabfce
hwahrmann <helmut.wahrmann@gmx.at>
2008-02-27 12:30:30 +0000

    Reorg of Models (Completed). Still need to be made plugins tough

--- MP-II/MediaPortal/postbuild.bat
+++ MP-II/MediaPortal/postbuild.bat
@@ -9,16 +9,18 @@ del /Q models\*.*
 del /Q skin\default\Scripts\*.* 
 del /Q skin\default\Scripts\Precompiled\*.* 
 xcopy %1\MediaPortal.Base\*.* . /E /Y /D
-copy %1\myhelloworld\bin\x86\%2\myhelloworld*.* models
-copy %1\mymovies\bin\x86\%2\mymovies*.* models
-copy %1\mypictures\bin\x86\%2\mypic*.* models
-copy %1\mymusic\bin\x86\%2\mymusic*.* models
-copy %1\mymedia\bin\x86\%2\mymedia*.* models
-copy %1\myweather\bin\x86\%2\myweather*.* models
-copy %1\mylogin\bin\x86\%2\mylogin*.* models
-copy %1\menu\bin\x86\%2\menu*.* models
-copy %1\Settings\bin\x86\%2\Settings*.* models
-copy %1\PlayList\bin\x86\%2\PlayList*.* models
 copy %1\vmr9Helper\release\vmr9helper.dll .
-copy %1\myextensions\bin\x86\%2\myextensions*.* models
-copy %1\shares\bin\x86\%2\shares*.* models
+REM ------------------------- Models ------------------------------------------
+copy %1\MediaPortal.Models\helloworld\bin\x86\%2\myhelloworld*.* models
+copy %1\MediaPortal.Models\movies\bin\x86\%2\mymovies*.* models
+copy %1\MediaPortal.Models\pictures\bin\x86\%2\mypic*.* models
+copy %1\MediaPortal.Models\music\bin\x86\%2\mymusic*.* models
+copy %1\MediaPortal.Models\media\bin\x86\%2\mymedia*.* models
+copy %1\MediaPortal.Models\weather\bin\x86\%2\myweather*.* models
+copy %1\MediaPortal.Models\login\bin\x86\%2\mylogin*.* models
+copy %1\MediaPortal.Models\menu\bin\x86\%2\menu*.* models
+copy %1\MediaPortal.Models\Settings\bin\x86\%2\Settings*.* models
+copy %1\MediaPortal.Models\PlayList\bin\x86\%2\PlayList*.* models
+copy %1\MediaPortal.Models\extensions\bin\x86\%2\myextensions*.* models
+copy %1\MediaPortal.Models\shares\bin\x86\%2\shares*.* models
+REM ---------------------- End of Models ------------------------------------
