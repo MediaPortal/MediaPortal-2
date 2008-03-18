f841c66263222e91880fb3186b581b1d4e6de0c7
Albert78 <albert@team-mediaportal.com>
2008-03-18 18:37:23 +0000

    Fixed assembly name

--- MP-II/MediaPortal.Models/HelloWorld/Properties/AssemblyInfo.cs
+++ MP-II/MediaPortal.Models/HelloWorld/Properties/AssemblyInfo.cs
@@ -29,11 +29,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 
-[assembly : AssemblyTitle("MyMovies")]
+[assembly: AssemblyTitle("MyHelloWorld")]
 [assembly : AssemblyDescription("")]
 [assembly : AssemblyConfiguration("")]
 [assembly : AssemblyCompany("")]
-[assembly : AssemblyProduct("MyMovies")]
+[assembly: AssemblyProduct("MyHelloWorld")]
 [assembly : AssemblyCopyright("Copyright ©  2007")]
 [assembly : AssemblyTrademark("")]
 [assembly : AssemblyCulture("")]
