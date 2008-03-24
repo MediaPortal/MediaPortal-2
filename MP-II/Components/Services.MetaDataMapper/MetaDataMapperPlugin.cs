5b2783d45e45cb480fd1813a03adf2e17c1541eb
james <james@team-mediaportal.com>
2008-03-24 20:24:13 +0000

    missing file added

---
+++ MP-II/Components/Services.MetaDataMapper/MetaDataMapperPlugin.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+using MediaPortal.Core.PluginManager;
+
+namespace Components.Services.MetaDataMapper
+{
+  class MetaDataMapperPlugin : IPlugin
+  {
+    #region IPlugin Implementation
+    /// <summary>
+    /// Initializes this instance.
+    /// </summary>
+    public void Initialize(string id)
+    {
+    }
+
+    public void Dispose()
+    {
+    }
+    #endregion
+  }
+}
