81a308251fc626041c44a838be79aea71ecb7802
Albert78 <albert@team-mediaportal.com>
2008-08-18 14:27:04 +0000

    New utility method for parsing version number strings

--- MP-II/MediaPortal.Utilities/Strings/StringUtils.cs
+++ MP-II/MediaPortal.Utilities/Strings/StringUtils.cs
@@ -22,6 +22,8 @@
 
 #endregion
 
+using System;
+
 namespace MediaPortal.Utilities.Strings
 {
   /// <summary>
@@ -29,6 +31,47 @@ namespace MediaPortal.Utilities.Strings
   /// </summary>
   public class StringUtils
   {
-    // Add utility methods here, when needed...
+    /// <summary>
+    /// Tries to parse the specified version string in the format <c>#.#</c> or <c>#</c>, where # stands
+    /// for an int number.
+    /// </summary>
+    /// <param name="versionStr">The string to parse. This string should be in the format
+    /// <c>#.#</c> or <c>#</c>.</param>
+    /// <param name="verHigh">Returns the version high number.</param>
+    /// <param name="verHigh">Returns the version low number, if the string contains both. Else, this
+    /// parameter will return <c>0</c>.</param>
+    /// <returns><c>true</c>, if the version string could correctly be parsed, else <c>false</c>.</returns>
+    public static bool TryParseVersionString(string versionStr, out int verHigh, out int verLow)
+    {
+      string[] numbers = versionStr.Split(new char[] { '.' });
+      verLow = 0;
+      verHigh = 0;
+      if (numbers.Length < 1 || numbers.Length > 2)
+        return false;
+      if (!Int32.TryParse(numbers[0], out verHigh))
+        return false;
+      if (numbers.Length > 1)
+        if (!Int32.TryParse(numbers[0], out verLow))
+          return false;
+      return true;
+    }
+
+    /// <summary>
+    /// Helper method to check the given version string to be equal or greater than the
+    /// specified version number.
+    /// </summary>
+    public static void CheckVersionEG(string versionStr, int expectedHigh, int expectedLow)
+    {
+      int verHigh;
+      int verLow;
+      if (!TryParseVersionString(versionStr, out verHigh, out verLow))
+        throw new ArgumentException("Illegal version number '" + versionStr + "', expected format: '#.#'");
+      if (verHigh >= expectedHigh)
+        return;
+      if (verLow >= expectedLow)
+        return;
+      throw new ArgumentException("Version number '" + versionStr +
+          "' is too low, at least '" + expectedHigh + "." + expectedLow + "' is needed");
+    }
   }
 }
