044b52dd14b3994fe2f3df06e7479e09d8e798ed
Albert78 <albert@team-mediaportal.com>
2008-08-30 09:03:46 +0000

    Fixed imports

--- MP-II/MediaPortal.Interfaces/ExtensionManager/IExtensionFileItem.cs
+++ MP-II/MediaPortal.Interfaces/ExtensionManager/IExtensionFileItem.cs
@@ -22,11 +22,6 @@
 
 #endregion
 
-using System;
-using System.Collections.Generic;
-using System.Text;
-
-
 namespace MediaPortal.Core.ExtensionManager
 {
 
--- MP-II/MediaPortal.Interfaces/ExtensionManager/IExtensionQueueObject.cs
+++ MP-II/MediaPortal.Interfaces/ExtensionManager/IExtensionQueueObject.cs
@@ -22,10 +22,6 @@
 
 #endregion
 
-using System;
-using System.Collections.Generic;
-using System.Text;
-
 namespace MediaPortal.Core.ExtensionManager
 {
   /// <summary>
