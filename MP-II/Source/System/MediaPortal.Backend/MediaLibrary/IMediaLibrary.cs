daeebae901903a66d91f74ca0ab60403dab63563
Albert78 <albert@team-mediaportal.com>
2009-08-17 17:09:08 +0000

    Changed IMediaLibrary interface: removed IGlobalSharesManagement interface from super-interfaces: The media library has almost the same methods but with a slightly different method contract

--- MP-II/Source/System/MediaPortal.Backend/MediaLibrary/IMediaLibrary.cs
+++ MP-II/Source/System/MediaPortal.Backend/MediaLibrary/IMediaLibrary.cs
@@ -22,18 +22,22 @@
 
 #endregion
 
+using System;
 using System.Collections.Generic;
+using MediaPortal.Core.General;
 using MediaPortal.Core.MediaManagement;
-using MediaPortal.MediaManagement;
 
 namespace MediaPortal.MediaLibrary
 {
-  public interface IMediaLibrary : IGlobalSharesManagement
+  /// <summary>
+  /// The media library is a "dumb" data store. It provides search/update methods for all kinds of content stored.
+  /// All media-related management functions (re-imports triggered by a special situation etc.) are handled by the media manager.
+  /// </summary>
+  public interface IMediaLibrary
   {
-    #region Startup & shutdown
+    #region Startup & Shutdown
 
     void Startup();
-
     void Shutdown();
 
     #endregion
@@ -60,6 +64,84 @@ namespace MediaPortal.MediaLibrary
     void AddMediaItemAspectStorage(MediaItemAspectMetadata miam);
 
     void RemoveMediaItemAspectStorage(MediaItemAspectMetadata miam);
+
+    #endregion
+
+    #region Shares management
+
+    /// <summary>
+    /// Adds a share to the media library's collection of registered shares.
+    /// </summary>
+    /// <param name="nativeSystem">System where the media provider for the new share is located.</param>
+    /// <param name="providerId">ID of the media provider of the specified <paramref name="nativeSystem"/>.</param>
+    /// <param name="path">Lookup path for the specified provider in the specified system.</param>
+    /// <param name="shareName">Name of the new share.</param>
+    /// <param name="mediaCategories">Categories of media items which are supposed to be contained in
+    /// the new share. If set to <c>null</c>, the new share is a general share without attached media
+    /// categories.</param>
+    /// <param name="metadataExtractorIds">Ids of metadata extractors to attach to the new share.</param>
+    /// <returns>ID of the new share.</returns>
+    Guid RegisterShare(SystemName nativeSystem, Guid providerId, string path,
+        string shareName, IEnumerable<string> mediaCategories, IEnumerable<Guid> metadataExtractorIds);
+
+    /// <summary>
+    /// Removes the share with the specified id.
+    /// </summary>
+    /// <param name="shareId">The id of the share to be removed. The share id is part of the
+    /// <see cref="ShareDescriptor"/> which was returned by the <see cref="RegisterShare"/> method.</param>
+    void RemoveShare(Guid shareId);
+
+    /// <summary>
+    /// Reconfigures the share with the specified <paramref name="shareId"/>.
+    /// </summary>
+    /// <remarks>
+    /// The share's native system cannot be changed by this method, else we would have to consider much more security problems.
+    /// </remarks>
+    /// <param name="shareId">Id of the share to be changed.</param>
+    /// <param name="nativeSystem">System where the share is located.</param>
+    /// <param name="providerId">ID of the media provider which should be installed in the native system of the share.</param>
+    /// <param name="path">Lookup path for the specified provider in the specified system.</param>
+    /// <param name="shareName">Name of the share.</param>
+    /// <param name="mediaCategories">Categories of media items which are supposed to be contained in
+    /// the share. If set to <c>null</c>, the new share is a general share without attached media
+    /// categories.</param>
+    /// <param name="metadataExtractorIds">Ids of metadata extractors to be attached to the share.</param>
+    /// <param name="relocateMediaItems">If set to <c>true</c>, the paths of all media items from the
+    /// specified share will be adapted to the new base path.</param>
+    /// <returns>Changed share descriptor.</returns>
+    void UpdateShare(Guid shareId, SystemName nativeSystem, Guid providerId, string path, string shareName, IEnumerable<string> mediaCategories, IEnumerable<Guid> metadataExtractorIds, bool relocateMediaItems);
+
+    /// <summary>
+    /// Returns all shares which are registered in the MediaPortal server's media library.
+    /// </summary>
+    /// <param name="onlyConnectedShares">If set to <c>true</c>, only shares of connected clients will be returned.</param>
+    /// <returns>Mapping of share's GUIDs to shares.</returns>
+    IDictionary<Guid, ShareDescriptor> GetShares(bool onlyConnectedShares);
+
+    /// <summary>
+    /// Returns the share descriptor for the share with the specified <paramref name="shareId"/>.
+    /// </summary>
+    /// <param name="shareId">Id of the share to return.</param>
+    /// <returns>Descriptor of the share with the specified <paramref name="shareId"/>. If the specified
+    /// share doesn't exist, the method returns <c>null</c>.</returns>
+    ShareDescriptor GetShare(Guid shareId);
+
+    /// <summary>
+    /// Returns a collection of shares for the specified <paramref name="systemName"/>.
+    /// </summary>
+    /// <param name="systemName">System whose shares should be returned.</param>
+    /// <returns>Mapping of share's GUIDs to shares.</returns>
+    IDictionary<Guid, ShareDescriptor> GetSharesBySystem(SystemName systemName);
+
+    #endregion
+
+    #region Client management
+
+    /// <summary>
+    /// Returns a collection of all known managed MediaPortal clients in the system.
+    /// </summary>
+    /// <returns>Collection of client system names.</returns>
+    ICollection<SystemName> GetManagedClients();
 
     #endregion
   }
