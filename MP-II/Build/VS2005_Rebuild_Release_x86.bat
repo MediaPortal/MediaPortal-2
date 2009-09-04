45f44577d285edbd8f77f43f244116d360b897c4
Albert78 <albert@team-mediaportal.com>
2009-09-04 08:51:12 +0000

    Updated build scripts to new solution filenames

--- MP-II/Build/VS2005_Rebuild_Release_x86.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-
-set ProgramDir=%ProgramFiles%
-if exist "%ProgramFiles(x86)%" set ProgramDir=%ProgramFiles(x86)%
-
-"%ProgramDir%\Microsoft Visual Studio 8\Common7\IDE\devenv.com" ..\Source\MP-II-Client-VS2005.sln /Rebuild "Release|x86" /Out build.log
--- MP-II/Build/VS2008_Build_Report_Release_x86.bat
+++ MP-II/Build/VS2008_Build_Report_Release_x86.bat
@@ -8,6 +8,6 @@ if exist "%ProgramFiles(x86)%" set ProgramDir=%ProgramFiles(x86)%
 xcopy /Y ..\Tools\BuildReport\css\*.* .\css\
 xcopy /Y ..\Tools\BuildReport\images\*.* .\images\
 
-"%ProgramDir%\Microsoft Visual Studio 9.0\Common7\IDE\devenv.com" ..\Source\MP-II-Client-VS2008.sln /Rebuild "Release|x86" > VS2008_Build_Report_Release_x86.txt
+"%ProgramDir%\Microsoft Visual Studio 9.0\Common7\IDE\devenv.com" ..\Source\MP-II-Client.sln /Rebuild "Release|x86" > VS2008_Build_Report_Release_x86.txt
 
-..\Tools\BuildReport\bin\Release\BuildReport.exe /VS2008 /Input=VS2008_Build_Report_Release_x86.txt /Output=VS2008_Build_Report_Release_x86.html /Solution=MP-II-Client-VS2008.sln /Title="MediaPortal II - Build Report"
+..\Tools\BuildReport\bin\Release\BuildReport.exe /VS2008 /Input=VS2008_Build_Report_Release_x86.txt /Output=VS2008_Build_Report_Release_x86.html /Solution=MP-II-Client.sln /Title="MediaPortal II - Build Report"
