d56526c1725be661570a9b003ce7adde68734e69
Albert78 <albert@team-mediaportal.com>
2009-09-13 18:16:43 +0000

    Added MP server Base directory with default paths and database creation scripts for the MediaPortal-Basis schema and the MediaLibrary schema

---
+++ MP-II/Source/Base/Server/Scripts/MediaPortal-Basis-create-1.0.sql
@@ -0,0 +1,16 @@
+-- This script creates the MediaPortal basis schema. DO NOT MODIFY!
+-- Albert, 2009-08-15
+
+-- Basis-Table where all sub schemas are registered with their current version number.
+CREATE TABLE MEDIAPORTAL_BASIS (
+  SUBSCHEMA_NAME VARCHAR(500) NOT NULL,
+  VERSION_MAJOR INTEGER NOT NULL,
+  VERSION_MINOR INTEGER NOT NULL,
+
+  CONSTRAINT MEDIAPORTAL_BASIS_PK PRIMARY KEY (SUBSCHEMA_NAME)
+);
+
+INSERT INTO MEDIAPORTAL_BASIS (SUBSCHEMA_NAME, VERSION_MAJOR, VERSION_MINOR) VALUES (
+  'MediaPortal-Basis', 1, 0
+);
+
