657dadf8b46f3b2cf4b219ec38254f5648a1eace
Albert78 <albert@team-mediaportal.com>
2010-03-25 12:13:59 +0000

    Added docs to AudioAspect and VideoAspect attributes

--- MP-II/Source/System/MediaPortal.Core/MediaManagement/DefaultItemAspects/AudioAspect.cs
+++ MP-II/Source/System/MediaPortal.Core/MediaManagement/DefaultItemAspects/AudioAspect.cs
@@ -36,30 +36,81 @@ namespace MediaPortal.Core.MediaManagement.DefaultItemAspects
     /// </summary>
     public static Guid ASPECT_ID = new Guid("9BA3C559-41F7-4a5f-917C-E3EF65516D14");
 
+    /// <summary>
+    /// Enumeration of artist name strings.
+    /// </summary>
     public static MediaItemAspectMetadata.AttributeSpecification ATTR_ARTISTS =
         MediaItemAspectMetadata.CreateStringAttributeSpecification("Artists", 100, Cardinality.ManyToMany, true);
+
+    /// <summary>
+    /// Album string.
+    /// </summary>
     public static MediaItemAspectMetadata.AttributeSpecification ATTR_ALBUM =
         MediaItemAspectMetadata.CreateStringAttributeSpecification("Album", 100, Cardinality.Inline, true);
+
+    /// <summary>
+    /// Enumeration of genre strings.
+    /// </summary>
     public static MediaItemAspectMetadata.AttributeSpecification ATTR_GENRES =
         MediaItemAspectMetadata.CreateStringAttributeSpecification("Genres", 100, Cardinality.ManyToMany, true);
+
+    /// <summary>
+    /// Duration in seconds.
+    /// </summary>
     public static MediaItemAspectMetadata.AttributeSpecification ATTR_DURATION =
         MediaItemAspectMetadata.CreateAttributeSpecification("Duration", typeof(long), Cardinality.Inline, false);
+
+    /// <summary>
+    /// Track number.
+    /// </summary>
     public static MediaItemAspectMetadata.AttributeSpecification ATTR_TRACK =
         MediaItemAspectMetadata.CreateAttributeSpecification("Track", typeof(int), Cardinality.Inline, false);
+
+    /// <summary>
+    /// Number of tracks on the CD.
+    /// </summary>
     public static MediaItemAspectMetadata.AttributeSpecification ATTR_NUMTRACKS =
         MediaItemAspectMetadata.CreateAttributeSpecification("NumTracks", typeof(int), Cardinality.Inline, false);
+
+    /// <summary>
+    /// Number of times played.
+    /// </summary>
     public static MediaItemAspectMetadata.AttributeSpecification ATTR_PLAYCOUNT =
         MediaItemAspectMetadata.CreateAttributeSpecification("PlayCount", typeof(int), Cardinality.Inline, false);
+
+    /// <summary>
+    /// Enumeration of album artist name strings.
+    /// </summary>
     public static MediaItemAspectMetadata.AttributeSpecification ATTR_ALBUMARTISTS =
         MediaItemAspectMetadata.CreateStringAttributeSpecification("AlbumArtists", 100, Cardinality.ManyToMany, false);
+
+    /// <summary>
+    /// Enumeration of composer name strings.
+    /// </summary>
     public static MediaItemAspectMetadata.AttributeSpecification ATTR_COMPOSERS =
         MediaItemAspectMetadata.CreateStringAttributeSpecification("Composers", 100, Cardinality.ManyToMany, false);
+
+    /// <summary>
+    /// Encoding as string. TODO: Describe format.
+    /// </summary>
     public static MediaItemAspectMetadata.AttributeSpecification ATTR_ENCODING =
         MediaItemAspectMetadata.CreateStringAttributeSpecification("Encoding", 50, Cardinality.Inline, false);
+
+    /// <summary>
+    /// Bitrate in bits/second.
+    /// </summary>
     public static MediaItemAspectMetadata.AttributeSpecification ATTR_BITRATE =
         MediaItemAspectMetadata.CreateAttributeSpecification("BitRate", typeof(int), Cardinality.Inline, false);
+
+    /// <summary>
+    /// ID of the disc. TODO: Specification.
+    /// </summary>
     public static MediaItemAspectMetadata.AttributeSpecification ATTR_DISCID =
         MediaItemAspectMetadata.CreateAttributeSpecification("DiscId", typeof(int), Cardinality.Inline, false);
+
+    /// <summary>
+    /// Number of discs in the collection.
+    /// </summary>
     public static MediaItemAspectMetadata.AttributeSpecification ATTR_NUMDISCS =
         MediaItemAspectMetadata.CreateAttributeSpecification("NumDiscs", typeof(int), Cardinality.Inline, false);
 
--- MP-II/Source/System/MediaPortal.Core/MediaManagement/DefaultItemAspects/VideoAspect.cs
+++ MP-II/Source/System/MediaPortal.Core/MediaManagement/DefaultItemAspects/VideoAspect.cs
@@ -36,34 +36,94 @@ namespace MediaPortal.Core.MediaManagement.DefaultItemAspects
     /// </summary>
     public static Guid ASPECT_ID = new Guid("8F8B7A4F-767C-4180-B58E-7C8999C52067");
 
+    /// <summary>
+    /// Genre string.
+    /// </summary>
     public static MediaItemAspectMetadata.AttributeSpecification ATTR_GENRE =
         MediaItemAspectMetadata.CreateStringAttributeSpecification("Genre", 100, Cardinality.ManyToOne, true);
+
+    /// <summary>
+    /// Duration in seconds.
+    /// </summary>
     public static MediaItemAspectMetadata.AttributeSpecification ATTR_DURATION =
         MediaItemAspectMetadata.CreateAttributeSpecification("Duration", typeof(long), Cardinality.Inline, false);
+
+    /// <summary>
+    /// Director name string.
+    /// </summary>
     public static MediaItemAspectMetadata.AttributeSpecification ATTR_DIRECTOR =
         MediaItemAspectMetadata.CreateStringAttributeSpecification("Director", 100, Cardinality.ManyToOne, false);
+
+    /// <summary>
+    /// Number of audio streams for this video.
+    /// </summary>
     public static MediaItemAspectMetadata.AttributeSpecification ATTR_AUDIOSTREAMCOUNT =
         MediaItemAspectMetadata.CreateAttributeSpecification("AudioStreamCount", typeof(int), Cardinality.Inline, false);
+
+    /// <summary>
+    /// Encoding. TODO: Describe format.
+    /// </summary>
     public static MediaItemAspectMetadata.AttributeSpecification ATTR_AUDIOENCODING =
         MediaItemAspectMetadata.CreateStringAttributeSpecification("AudioEncoding", 50, Cardinality.Inline, false);
+
+    /// <summary>
+    /// Bitrate of the first audio stream in bits/second.
+    /// </summary>
     public static MediaItemAspectMetadata.AttributeSpecification ATTR_AUDIOBITRATE =
         MediaItemAspectMetadata.CreateAttributeSpecification("AudioBitRate", typeof(long), Cardinality.Inline, false);
+
+    /// <summary>
+    /// Encoding of the video. TODO: Describe format.
+    /// </summary>
     public static MediaItemAspectMetadata.AttributeSpecification ATTR_VIDEOENCODING =
         MediaItemAspectMetadata.CreateStringAttributeSpecification("VideoEncoding", 50, Cardinality.Inline, false);
+
+    /// <summary>
+    /// Bitrate of the video in bits/second.
+    /// </summary>
     public static MediaItemAspectMetadata.AttributeSpecification ATTR_VIDEOBITRATE =
         MediaItemAspectMetadata.CreateAttributeSpecification("VideoBitRate", typeof(long), Cardinality.Inline, false);
+
+    /// <summary>
+    /// Width of the video in pixels.
+    /// </summary>
     public static MediaItemAspectMetadata.AttributeSpecification ATTR_WIDTH =
         MediaItemAspectMetadata.CreateAttributeSpecification("Width", typeof(int), Cardinality.Inline, false);
+
+    /// <summary>
+    /// Height of the video in pixels.
+    /// </summary>
     public static MediaItemAspectMetadata.AttributeSpecification ATTR_HEIGHT =
         MediaItemAspectMetadata.CreateAttributeSpecification("Height", typeof(int), Cardinality.Inline, false);
+
+    /// <summary>
+    /// Aspect ratio of the resulting video in width/height. Might differ from the quotient width/height of the properties
+    /// <see cref="ATTR_WIDTH"/> and <see cref="ATTR_HEIGHT"/> if the resulting video must be stretched.
+    /// </summary>
     public static MediaItemAspectMetadata.AttributeSpecification ATTR_ASPECTRATIO =
         MediaItemAspectMetadata.CreateAttributeSpecification("AspectRatio", typeof(float), Cardinality.Inline, false);
+
+    /// <summary>
+    /// Frames/second of the video.
+    /// </summary>
     public static MediaItemAspectMetadata.AttributeSpecification ATTR_FPS =
         MediaItemAspectMetadata.CreateAttributeSpecification("FPS", typeof(int), Cardinality.Inline, false);
+
+    /// <summary>
+    /// Enumeration of actor name strings.
+    /// </summary>
     public static MediaItemAspectMetadata.AttributeSpecification ATTR_ACTORS =
         MediaItemAspectMetadata.CreateStringAttributeSpecification("Actors", 100, Cardinality.ManyToMany, true);
+
+    /// <summary>
+    /// Set to <c>true</c> if this video item represents a DVD.
+    /// </summary>
     public static MediaItemAspectMetadata.AttributeSpecification ATTR_ISDVD =
         MediaItemAspectMetadata.CreateAttributeSpecification("IsDVD", typeof(bool), Cardinality.Inline, false);
+
+    /// <summary>
+    /// String describing the story plot of the video.
+    /// </summary>
     public static MediaItemAspectMetadata.AttributeSpecification ATTR_STORYPLOT =
         MediaItemAspectMetadata.CreateStringAttributeSpecification("StoryPlot", 10000, Cardinality.Inline, false);
 
