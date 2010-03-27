3e823fe71a86c1e9b8bf26db1aea20e3e9f7a7dc
Albert78 <albert@team-mediaportal.com>
2010-03-27 10:02:32 +0000

    Updated MediaPortal II name in all file headers to MediaPortal 2

--- MP-II/Source/System/MediaPortal.Core/Services/PluginManager/PluginManager.cs
+++ MP-II/Source/System/MediaPortal.Core/Services/PluginManager/PluginManager.cs
@@ -4,20 +4,20 @@
  *  Copyright (C) 2007-2010 Team MediaPortal
  *  http://www.team-mediaportal.com
  *
- *  This file is part of MediaPortal II
+ *  This file is part of MediaPortal 2
  *
- *  MediaPortal II is free software: you can redistribute it and/or modify
+ *  MediaPortal 2 is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
  *
- *  MediaPortal II is distributed in the hope that it will be useful,
+ *  MediaPortal 2 is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
  * 
  *  You should have received a copy of the GNU General Public License
- *  along with MediaPortal II.  If not, see <http://www.gnu.org/licenses/>.
+ *  along with MediaPortal 2.  If not, see <http://www.gnu.org/licenses/>.
  * 
  */
 
