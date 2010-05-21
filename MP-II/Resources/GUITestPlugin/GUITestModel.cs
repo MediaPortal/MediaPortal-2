21c5102d7f23ada86228ab0460b6e9633734b571
Albert78 <albert@team-mediaportal.com>
2010-05-21 16:57:20 +0000

    Added GUI test plugin for MP 2 SkinEngine

---
+++ MP-II/Resources/GUITestPlugin/GUITestModel.cs
@@ -0,0 +1,103 @@
+#region Copyright (C) 2007-2008 Team MediaPortal
+
+/*
+    Copyright (C) 2007-2008 Team MediaPortal
+    http://www.team-mediaportal.com
+ 
+    This file is part of MediaPortal II
+
+    MediaPortal II is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    MediaPortal II is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with MediaPortal II.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#endregion
+
+using System;
+using System.Collections.Generic;
+using MediaPortal.Core;
+using MediaPortal.Core.General;
+using MediaPortal.Core.MediaManagement;
+using MediaPortal.Core.Services.MediaManagement;
+using MediaPortal.UI.Presentation.DataObjects;
+using MediaPortal.UI.Presentation.Models;
+using MediaPortal.UI.Presentation.Players;
+using MediaPortal.UI.Presentation.Screens;
+using MediaPortal.UI.Presentation.Workflow;
+
+namespace Test.GUITestPlugin
+{
+  /// <summary>
+  /// Model which holds the GUI state for the GUI test model.
+  /// </summary>
+  public class GUITestModel : IWorkflowModel
+  {
+    public const string MODEL_ID_STR = "F4FC1599-F412-40d0-82BF-46FC352E93BE";
+    public const string TEST_MAIN_WORKFLOW_STATE_ID_STR = "F660C2CA-A340-4694-A7F4-9E68AB9721C4";
+
+    protected static Guid TEST_MAIN_WORKFLOW_STATE_ID = new Guid(TEST_MAIN_WORKFLOW_STATE_ID_STR);
+
+    #region Protected fields
+
+    #endregion
+
+    public GUITestModel()
+    {
+    }
+
+    #region IWorkflowModel implementation
+
+    public Guid ModelId
+    {
+      get { return new Guid(MODEL_ID_STR); }
+    }
+
+    public bool CanEnterState(NavigationContext oldContext, NavigationContext newContext)
+    {
+      return true;
+    }
+
+    public void EnterModelContext(NavigationContext oldContext, NavigationContext newContext)
+    {
+      // We could initialize some data here when entering the media navigation state
+    }
+
+    public void ExitModelContext(NavigationContext oldContext, NavigationContext newContext)
+    {
+      // We could dispose some data here when exiting media navigation context
+    }
+
+    public void ChangeModelContext(NavigationContext oldContext, NavigationContext newContext, bool push)
+    {
+      // We could initialize some data here when changing the media navigation state
+    }
+
+    public void Deactivate(NavigationContext oldContext, NavigationContext newContext)
+    {
+    }
+
+    public void ReActivate(NavigationContext oldContext, NavigationContext newContext)
+    {
+    }
+
+    public void UpdateMenuActions(NavigationContext context, IDictionary<Guid, WorkflowAction> actions)
+    {
+    }
+
+    public ScreenUpdateMode UpdateScreen(NavigationContext context, ref string screen)
+    {
+      return ScreenUpdateMode.AutoWorkflowManager;
+    }
+
+    #endregion
+  }
+}
