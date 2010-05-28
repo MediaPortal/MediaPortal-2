2bb832d04a584da81b03ca7c2267c35edf28cf15
Albert78 <albert@team-mediaportal.com>
2010-05-28 20:28:22 +0000

    Updated comments in general template class

--- MP-II/Resources/Templates/Template_Class.cs
+++ MP-II/Resources/Templates/Template_Class.cs
@@ -1,23 +1,23 @@
-#region Copyright (C) 2007-2008 Team MediaPortal
+#region Copyright (C) 2007-2010 Team MediaPortal
 
 /*
-    Copyright (C) 2007-2008 Team MediaPortal
+    Copyright (C) 2007-2010 Team MediaPortal
     http://www.team-mediaportal.com
 
-    This file is part of MediaPortal II
+    This file is part of MediaPortal 2
 
-    MediaPortal II is free software: you can redistribute it and/or modify
+    MediaPortal 2 is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
-    MediaPortal II is distributed in the hope that it will be useful,
+    MediaPortal 2 is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with MediaPortal II.  If not, see <http://www.gnu.org/licenses/>.
+    along with MediaPortal 2.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #endregion
@@ -27,11 +27,24 @@ using System;
 namespace MediaPortal
 {
   /// <summary>
-  /// This is a MediaPortal-II class. Please describe any class, if
-  /// the meaning is not absolutely clear by its name.
+  /// This is a MediaPortal 2 class. Please describe any class, if the meaning is not absolutely clear by its name.
   /// </summary>
   /// <remarks>
+  /// <para>
   /// Further description for very central or complex/difficult classes.
+  /// </para>
+  /// <para>
+  /// Aspects which might be worth to describe here are:
+  /// <list type="bullet">
+  /// <item>General idea of this class</item>
+  /// <item>Does the class have an internal state which must be known by the user? For example, is it necessary
+  /// to call methods in a certain order? Which state dependencies to utilized objects are important?</item>
+  /// <item>Usage examples</item>
+  /// <item>Multithreading behaviour</item>
+  /// <item>Related classes</item>
+  /// <item>...</item>
+  /// </list>
+  /// </para>
   /// </remarks>
   public class Template_Class
   {
