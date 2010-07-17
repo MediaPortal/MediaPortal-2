029c8377a233611b29e4d721120443deecaf15c3
Albert78 <albert@team-mediaportal.com>
2010-07-17 09:21:38 +0000

    Don't do a device reset when the window is minimized

--- MediaPortal/Source/UI/SkinEngine/GUI/MainForm.cs
+++ MediaPortal/Source/UI/SkinEngine/GUI/MainForm.cs
@@ -558,7 +558,7 @@ namespace MediaPortal.UI.SkinEngine.GUI
     {
       base.OnSizeChanged(e);
       // This method override is only necessary to capture the window state change event. All other cases aren't interesting here.
-      if (_previousWindowState != WindowState && _adaptToSizeEnabled)
+      if (WindowState != FormWindowState.Minimized && _previousWindowState != WindowState && _adaptToSizeEnabled)
         AdaptToSize();
     }
 
