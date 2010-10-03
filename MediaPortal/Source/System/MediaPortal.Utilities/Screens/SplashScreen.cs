9c3a63349fe11a038bf177eaa658bda5d1876583
Albert78 <albert@team-mediaportal.com>
2010-10-03 09:07:20 +0000

    Some refactorings of splash screen

--- MediaPortal/Source/System/MediaPortal.Utilities/Screens/SplashScreen.cs
+++ MediaPortal/Source/System/MediaPortal.Utilities/Screens/SplashScreen.cs
@@ -23,288 +23,280 @@
 #endregion
 
 // This file was taken from DGDev from CodeProject (http://www.codeproject.com/KB/dialog/DGDevSplashScreen.aspx).
+// Some refactorings and minimal reworks have been made.
 
 using System;
 using System.ComponentModel;
 using System.Drawing;
 using System.Threading;
 using System.Windows.Forms;
+using Timer=System.Windows.Forms.Timer;
 
-namespace DGDev
+namespace MediaPortal.Utilities.Screens
 {
-    public class SplashScreen : Form
+  public class SplashScreen : Form
+  {
+    private const double _opacityDecrement = .08;
+    private const double _opacityIncrement = .05;
+    private const int _timerInterval = 50;
+    private static Boolean _fadeMode;
+    private static Boolean _fadeInOut;
+    private static Image _splashBGImage;
+    private static String _infoText;
+    private static String _statusText;
+    private static SplashScreen _splashScreenForm;
+    private static Thread _splashScreenThread;
+    private static Color _transparentColor;
+    private Label ProgramInfoLabel;
+    private Label _statusLabel;
+    private Timer _splashTimer;
+    private IContainer _components;
+    private delegate void UpdateLabel();
+    private delegate void CloseSplash();
+
+    #region Public Properties & Methods
+
+    /// <summary>
+    /// Sets the content of the info label.
+    /// </summary>
+    public String InfoText
     {
-        private const double OpacityDecrement = .08;
-        private const double OpacityIncrement = .05;
-        private const int TimerInterval = 50;
-        private static Boolean FadeMode;
-        private static Boolean FadeInOut;
-        private static Image BGImage;
-        private static String Information;
-        private static String Status;
-        private static SplashScreen SplashScreenForm;
-        private static Thread SplashScreenThread;
-        private static Color TransparentKey;
-        private Label ProgramInfoLabel;
-        private Label StatusLabel;
-        private System.Windows.Forms.Timer SplashTimer;
-        private IContainer components;
-        private delegate void UpdateLabel();
-        private delegate void CloseSplash();
-
-        #region Public Properties & Methods
-
-        /// <summary>
-        /// These methods can all be called to set configurable 
-                  /// parameters for the Splash Screen
-        /// </summary>
-        public String SetInfo
+      get { return _infoText; }
+      set
+      {
+        _infoText = value;
+        if (ProgramInfoLabel.InvokeRequired)
         {
-            get { return Information; }
-            set
-            {
-                Information = value;
-                if(ProgramInfoLabel.InvokeRequired)
-                {
-                    var InfoUpdate = 
-                        new UpdateLabel(UpdateInfo);
-                    Invoke(InfoUpdate);
-                }
-                else
-                {
-                    UpdateInfo();
-                }
-            }
+          var InfoUpdate = new UpdateLabel(UpdateInfo);
+          Invoke(InfoUpdate);
         }
-
-        public String SetStatus
-        {
-            get { return Status; }
-            set
-            {
-                Status = value;
-                if(StatusLabel.InvokeRequired)
-                {
-                    var StatusUpdate = 
-                        new UpdateLabel(UpdateStatus);
-                    Invoke(StatusUpdate);
-                }
-                else
-                {
-                    UpdateStatus();
-                }
-            }
-        }
-
-        public Image SetBackgroundImage
-        {
-            get { return BGImage; }
-            set
-            {
-                BGImage = value;
-                if (value != null)
-                {
-                    BackgroundImage = BGImage;
-                    ClientSize = BackgroundImage.Size;
-                }
-            }
-        }
-
-        public Color SetTransparentKey
-        {
-            get { return TransparentKey; }
-            set
-            {
-                TransparentKey = value;
-                if (value != Color.Empty)
-                    TransparencyKey = SetTransparentKey;
-            }
-        }
-
-        public Boolean SetFade
-        {
-            get { return FadeInOut; }
-            set
-            {
-                FadeInOut = value;
-                Opacity = value ? .00 : 1.00;
-            }
-        }
-
-        public static SplashScreen Current
-        {
-            get
-            {
-                if (SplashScreenForm == null)
-                    SplashScreenForm = new SplashScreen();
-                return SplashScreenForm;
-            }
-        }
-
-        public void SetStatusLabel(Point StatusLabelLocation, 
-                Int32 StatusLabelWidth, Int32 StatusLabelHeight)
-        {
-            if (StatusLabelLocation != Point.Empty)
-                StatusLabel.Location = StatusLabelLocation;
-            if (StatusLabelWidth == 0 && StatusLabelHeight == 0)
-                StatusLabel.AutoSize = true;
-            else
-            {
-                if (StatusLabelWidth > 0)
-                    StatusLabel.Width = StatusLabelWidth;
-                if (StatusLabelHeight > 0)
-                    StatusLabel.Height = StatusLabelHeight;
-            }
-        }
-
-        public void SetInfoLabel(Point InfoLabelLocation, 
-                Int32 InfoLabelWidth, Int32 InfoLabelHeight)
-        {
-            if (InfoLabelLocation != Point.Empty)
-                ProgramInfoLabel.Location = InfoLabelLocation;
-            if (InfoLabelWidth == 0 && InfoLabelHeight == 0)
-                ProgramInfoLabel.AutoSize = true;
-            else
-            {
-                if (InfoLabelWidth > 0)
-                    ProgramInfoLabel.Width = InfoLabelWidth;
-                if (InfoLabelHeight > 0)
-                    ProgramInfoLabel.Height = InfoLabelHeight;
-            }
-        }
-
-        public void ShowSplashScreen()
-        {
-            SplashScreenThread = new Thread(ShowForm) 
-                {IsBackground = true, Name = "SplashScreenThread"};
-            SplashScreenThread.Start();
-        }
-
-        public void CloseSplashScreen()
-        {
-            if (SplashScreenForm != null)
-            {
-                if(InvokeRequired)
-                {
-                    var ClosingDelegate = 
-                        new CloseSplash(HideSplash);
-                    Invoke(ClosingDelegate);
-                }
-                else
-                {
-                    HideSplash();
-                }
-            }
-        }
-        #endregion
-
-        public SplashScreen()
-        {
-            InitializeComponent();
-        }
-
-        private static void ShowForm()
-        {
-            Application.Run(SplashScreenForm);
-        }
-
-        private void UpdateStatus()
-        {
-            StatusLabel.Text = SetStatus;
-        }
-
-        private void UpdateInfo()
-        {
-            ProgramInfoLabel.Text = SetInfo;
-        }
-
-        private void SplashTimer_Tick(object sender, EventArgs e)
-        {
-            if(FadeMode) // Form is opening (Increment)
-            {
-                if (Opacity < 1.00)
-                    Opacity += OpacityIncrement;
-                else
-                    SplashTimer.Stop();
-            }
-            else // Form is closing (Decrement)
-            {
-                if(Opacity > .00)
-                    Opacity -= OpacityDecrement;
-                else
-                    Dispose();
-            }
-            
-        }
-
-        #region InitComponents
-
-        private void InitializeComponent()
-        {
-            this.components = new System.ComponentModel.Container();
-            this.ProgramInfoLabel = new System.Windows.Forms.Label();
-            this.StatusLabel = new System.Windows.Forms.Label();
-            this.SplashTimer = new System.Windows.Forms.Timer
-                            (this.components);
-            this.SuspendLayout();
-            // 
-            // ProgramInfoLabel
-            // 
-            this.ProgramInfoLabel.BackColor = 
-                    System.Drawing.Color.Transparent;
-            this.ProgramInfoLabel.Location = 
-                    new System.Drawing.Point(56, 52);
-            this.ProgramInfoLabel.Name = "ProgramInfoLabel";
-            this.ProgramInfoLabel.Size = 
-                    new System.Drawing.Size(100, 23);
-            this.ProgramInfoLabel.TabIndex = 0;
-            // 
-            // StatusLabel
-            // 
-            this.StatusLabel.BackColor = 
-                    System.Drawing.Color.Transparent;
-            this.StatusLabel.Location = 
-                    new System.Drawing.Point(59, 135);
-            this.StatusLabel.Name = "StatusLabel";
-            this.StatusLabel.Size = new System.Drawing.Size(100, 23);
-            this.StatusLabel.TabIndex = 1;
-            // 
-            // SplashScreen
-            // 
-            this.ClientSize = new System.Drawing.Size(292, 273);
-            this.Controls.Add(this.StatusLabel);
-            this.Controls.Add(this.ProgramInfoLabel);
-            this.FormBorderStyle = 
-                System.Windows.Forms.FormBorderStyle.None;
-            this.Name = "SplashScreen";
-            this.ShowInTaskbar = false;
-            this.StartPosition = 
-                System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.ResumeLayout(false);
-
-        }
-
-        #endregion
-
-        private void SplashScreen_Load(object sender, EventArgs e)
-        {
-            if (SetFade)
-            {
-                FadeMode = true;
-                SplashTimer.Interval = TimerInterval;
-                SplashTimer.Start();
-            }
-        }
-
-        private void HideSplash()
-        {
-            if(SetFade)
-            {
-                FadeMode = false;
-                SplashTimer.Start();
-            }
-            else
-                Dispose();
-        }
+        else
+          UpdateInfo();
+      }
     }
+
+    /// <summary>
+    /// Sets the content of the status label.
+    /// </summary>
+    public String StatusText
+    {
+      get { return _statusText; }
+      set
+      {
+        _statusText = value;
+        if (_statusLabel.InvokeRequired)
+        {
+          var StatusUpdate = new UpdateLabel(UpdateStatus);
+          Invoke(StatusUpdate);
+        }
+        else
+          UpdateStatus();
+      }
+    }
+
+    /// <summary>
+    /// Sets the background image and automatically sets the size to match the image size.
+    /// </summary>
+    public Image SplashBackgroundImage
+    {
+      get { return _splashBGImage; }
+      set
+      {
+        _splashBGImage = value;
+        if (value != null)
+        {
+          BackgroundImage = _splashBGImage;
+          ClientSize = _splashBGImage.Size;
+        }
+      }
+    }
+
+    /// <summary>
+    /// Sets the color of the background image which will be presented transparent.
+    /// </summary>
+    public Color TransparentColor
+    {
+      get { return _transparentColor; }
+      set
+      {
+        _transparentColor = value;
+        if (value != Color.Empty)
+          TransparencyKey = TransparentColor;
+      }
+    }
+
+    public Boolean Fade
+    {
+      get { return _fadeInOut; }
+      set
+      {
+        _fadeInOut = value;
+        Opacity = value ? .00 : 1.00;
+      }
+    }
+
+    public static SplashScreen Current
+    {
+      get
+      {
+        if (_splashScreenForm == null)
+          _splashScreenForm = new SplashScreen();
+        return _splashScreenForm;
+      }
+    }
+
+    public void PositionStatusLabel(Point location, int width, int height)
+    {
+      if (location != Point.Empty)
+        _statusLabel.Location = location;
+      if (width == 0 && height == 0)
+        _statusLabel.AutoSize = true;
+      else
+      {
+        if (width > 0)
+          _statusLabel.Width = width;
+        if (height > 0)
+          _statusLabel.Height = height;
+      }
+    }
+
+    public void PositionInfoLabel(Point location, int width, int height)
+    {
+      if (location != Point.Empty)
+        ProgramInfoLabel.Location = location;
+      if (width == 0 && height == 0)
+        ProgramInfoLabel.AutoSize = true;
+      else
+      {
+        if (width > 0)
+          ProgramInfoLabel.Width = width;
+        if (height > 0)
+          ProgramInfoLabel.Height = height;
+      }
+    }
+
+    public void ShowSplashScreen()
+    {
+      _splashScreenThread = new Thread(ShowForm) {IsBackground = true, Name = "SplashScreenThread"};
+      _splashScreenThread.Start();
+    }
+
+    public void CloseSplashScreen()
+    {
+      if (_splashScreenForm != null)
+      {
+        if (InvokeRequired)
+        {
+          CloseSplash dlgt = HideSplash;
+          Invoke(dlgt);
+        }
+        else
+          HideSplash();
+      }
+    }
+
+    #endregion
+
+    public SplashScreen()
+    {
+      InitializeComponent();
+    }
+
+    private static void ShowForm()
+    {
+      Application.Run(_splashScreenForm);
+    }
+
+    private void UpdateStatus()
+    {
+      _statusLabel.Text = _statusText;
+    }
+
+    private void UpdateInfo()
+    {
+      ProgramInfoLabel.Text = _infoText;
+    }
+
+    #region InitComponents
+
+    private void InitializeComponent()
+    {
+      _components = new Container();
+      ProgramInfoLabel = new Label();
+      _statusLabel = new Label();
+      _splashTimer = new Timer(_components);
+      SuspendLayout();
+      // 
+      // ProgramInfoLabel
+      // 
+      ProgramInfoLabel.BackColor = Color.Transparent;
+      ProgramInfoLabel.Location = new Point(56, 52);
+      ProgramInfoLabel.Name = "ProgramInfoLabel";
+      ProgramInfoLabel.Size = new Size(100, 23);
+      ProgramInfoLabel.TabIndex = 0;
+      // 
+      // _statusLabel
+      // 
+      _statusLabel.BackColor = Color.Transparent;
+      _statusLabel.Location = new Point(59, 135);
+      _statusLabel.Name = "_statusLabel";
+      _statusLabel.Size = new Size(100, 23);
+      _statusLabel.TabIndex = 1;
+      // 
+      // SplashScreen
+      // 
+      ClientSize = new Size(292, 273);
+      Controls.Add(_statusLabel);
+      Controls.Add(ProgramInfoLabel);
+      FormBorderStyle = FormBorderStyle.None;
+      Name = "SplashScreen";
+      ShowInTaskbar = false;
+      StartPosition = FormStartPosition.CenterScreen;
+      ResumeLayout(false);
+      Load += SplashScreen_Load;
+      _splashTimer.Tick += SplashTimer_Tick;
+
+    }
+
+    #endregion
+
+    private void SplashTimer_Tick(object sender, EventArgs e)
+    {
+      if (_fadeMode) // Form is opening (Increment)
+      {
+        if (Opacity < 1.00)
+          Opacity += _opacityIncrement;
+        else
+          _splashTimer.Stop();
+      }
+      else // Form is closing (Decrement)
+      {
+        if (Opacity > .00)
+          Opacity -= _opacityDecrement;
+        else
+          Dispose();
+      }
+    }
+
+    private void SplashScreen_Load(object sender, EventArgs e)
+    {
+      if (Fade)
+      {
+        _fadeMode = true;
+        _splashTimer.Interval = _timerInterval;
+        _splashTimer.Start();
+      }
+    }
+
+    private void HideSplash()
+    {
+      if (Fade)
+      {
+        _fadeMode = false;
+        _splashTimer.Start();
+      }
+      else
+        Dispose();
+    }
+  }
 }
