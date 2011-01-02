d9d7eb85b870f5bd6b3f68b7425a9aed9dc3339f
Albert78 <albert@team-mediaportal.com>
2011-01-02 10:25:52 +0000

    Potential update problem in Label when Content is changed

--- MediaPortal/Source/UI/SkinEngine/Controls/Visuals/Label.cs
+++ MediaPortal/Source/UI/SkinEngine/Controls/Visuals/Label.cs
@@ -126,7 +126,7 @@ namespace MediaPortal.UI.SkinEngine.Controls.Visuals
       InitializeResourceString();
       if (_asset != null)
         _asset.Text = _resourceString;
-      InvalidateLayout(true, false);
+      InvalidateLayout(true, true);
     }
 
     void OnLayoutPropertyChanged(AbstractProperty prop, object oldValue)
