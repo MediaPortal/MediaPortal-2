48681dbe24ad5905eb16457a05bfa0bf30fd6ec9
Albert <albert@team-mediaportal.com>
2011-03-23 11:42:25 +0100

    Changed debug output of importer from "Processing resource ..." to "Importing ..."

--- MediaPortal/Source/System/MediaPortal.Core/Services/MediaManagement/ImporterWorker.cs
+++ MediaPortal/Source/System/MediaPortal.Core/Services/MediaManagement/ImporterWorker.cs
@@ -568,7 +568,7 @@ namespace MediaPortal.Core.Services.MediaManagement
               IFileSystemResourceAccessor fsra = pendingImportResource.ResourceAccessor;
               int numPending = importJob.PendingResources.Count;
               string moreResources = numPending > 1 ? string.Format(" ({0} more resources pending)", numPending) : string.Empty;
-              ServiceRegistration.Get<ILogger>().Info("ImporterWorker: Processing resource '{0}'{1}", fsra.ResourcePathName, moreResources);
+              ServiceRegistration.Get<ILogger>().Info("ImporterWorker: Importing '{0}'{1}", fsra.ResourcePathName, moreResources);
               if (fsra.IsFile)
                 ImportResource(fsra, pendingImportResource.ParentDirectory, metadataExtractors, mediaItemAspectTypes, resultHandler, mediaAccessor);
               else if (fsra.IsDirectory)
