df9fce25a9715b76fd8745cde16516616f9797f6
Albert <albert@team-mediaportal.com>
2011-04-23 14:49:39 +0200

    Improved ZipMediaProvider
    - Moved temp file preparation code to method PrepareStreamAccess, reuse cached file
    - Removed redundant code docs
    - Implemented ToString() method

--- MediaPortal/Source/Extensions/MediaProviders/ZipMediaProvider/ZipResourceAccessor.cs
+++ MediaPortal/Source/Extensions/MediaProviders/ZipMediaProvider/ZipResourceAccessor.cs
@@ -139,7 +139,7 @@ namespace MediaPortal.Extensions.MediaProviders.ZipMediaProvider
 			Dispose();
 		}
 
-    #region Implementation of IDisposable
+    #region IDisposable implementation
 
     /// <summary>
     /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
@@ -167,63 +167,32 @@ namespace MediaPortal.Extensions.MediaProviders.ZipMediaProvider
 
     #region Implementation of IResourceAccessor
 
-    /// <summary>
-    /// Returns the media provider which provides this resource, if available. If this resource accessor is not hosted
-    /// by a media provider, this property returns <c>null</c>.
-    /// </summary>
     public IMediaProvider ParentProvider
     {
       get { return _provider; }
     }
 
 
-    /// <summary>
-    /// Returns the information if this resource is a directory which might contain files and sub directories.
-    /// </summary>
-    /// <value><c>true</c>, if this resource denotes a directory.</value>
     public bool IsDirectory
     {
-      get
-      {
-        return _isDirectory;
-      }
+      get { return _isDirectory; }
     }
 
-    /// <summary>
-    /// Returns the information if this resource is a file which can be opened to an input stream.
-    /// </summary>
-    /// <value><c>true</c>, if this resource denotes a file which can be opened, else <c>false</c>.</value>
     public bool IsFile
     {
-      get
-      {
-        return !_isDirectory;
-      }
+      get { return !_isDirectory; }
     }
 
-    /// <summary>
-    /// Returns a short, human readable name for this resource.
-    /// </summary>
-    /// <value>A human readable name of this resource. For a filesystem resource accessor,
-    /// this could be the file name or directory name, for example.</value>
     public string ResourceName
     {
       get { return _resourceName; }
     }
 
-    /// <summary>
-    /// Returns the full human readable path name for this resource.
-    /// </summary>
-    /// <value>A human readable name of this resource. For a filesystem resource accessor,
-    /// this could be the file path, for example.</value>
     public string ResourcePathName
     {
       get { return _resourcePath; }
     }
 
-    /// <summary>
-    /// Returns the technical resource path which points to this resource.
-    /// </summary>
     public ResourcePath LocalResourcePath
     {
       get
@@ -237,37 +206,20 @@ namespace MediaPortal.Extensions.MediaProviders.ZipMediaProvider
       }
     }
 
-    /// <summary>
-    /// Gets the date and time when this resource was changed for the last time.
-    /// </summary>
     public DateTime LastChanged
     {
       get { return _lastChanged; }
     }
 
-    /// <summary>
-    /// Gets the file size in bytes, if this resource represents a file. Else returns <c>-1</c>.
-    /// </summary>
     public long Size
     {
       get { return _size; }
     }
 
-    /// <summary>
-    /// Prepares this resource accessor to get a stream for the resource's contents.
-    /// This might take some time, so this method might block some seconds.
-    /// </summary>
     public void PrepareStreamAccess()
     {
-    }
-
-    /// <summary>
-    /// Opens a stream to read this resource.
-    /// </summary>
-    /// <returns>Stream opened for read operations, if supported. Else, <c>null</c> is returned.</returns>
-    /// <exception cref="IllegalCallException">If this resource is not a file (see <see cref="IResourceAccessor.IsFile"/>).</exception>
-    public Stream OpenRead()
-    {
+      if (!string.IsNullOrEmpty(_tempPathFile))
+        return;
       string path = _pathOrFile;
       if (path.StartsWith("/"))
         path = path.Substring(1);
@@ -285,17 +237,18 @@ namespace MediaPortal.Extensions.MediaProviders.ZipMediaProvider
               StreamUtils.Copy(zFile.GetInputStream(entry), streamWriter, buffer);
             }
           }
-          return File.OpenRead(_tempPathFile); 
+          return;
         }
       }
-      return null;
     }
 
-    /// <summary>
-    /// Opens a stream to write this resource.
-    /// </summary>
-    /// <returns>Stream opened for write operations, if supported. Else, <c>null</c> is returned.</returns>
-    /// <exception cref="IllegalCallException">If this resource is not a file (see <see cref="IResourceAccessor.IsFile"/>).</exception>
+    public Stream OpenRead()
+    {
+      if (string.IsNullOrEmpty(_tempPathFile))
+        PrepareStreamAccess();
+      return File.OpenRead(_tempPathFile); 
+    }
+
     public Stream OpenWrite()
     {
       return null;
@@ -305,18 +258,6 @@ namespace MediaPortal.Extensions.MediaProviders.ZipMediaProvider
 
     #region Implementation of IFileSystemResourceAccessor
 
-    /// <summary>
-    /// Returns the information if the resource at the given path exists in the media provider of this resource.
-    /// </summary>
-    /// <remarks>
-    /// This method is defined in interface <see cref="IFileSystemResourceAccessor"/> rather than in interface
-    /// <see cref="IMediaProvider"/> because we would need two different signatures for
-    /// <see cref="IBaseMediaProvider"/> and <see cref="IChainedMediaProvider"/>, which is not convenient.
-    /// Furthermore, this method supports relative paths which are related to this resource.
-    /// </remarks>
-    /// <param name="path">Absolute or relative path to check for a resource.</param>
-    /// <returns><c>true</c> if a resource at the given path exists in the <see cref="IResourceAccessor.ParentProvider"/>,
-    /// else <c>false</c>.</returns>
     public bool Exists(string path)
     {
       if (path.Equals("/") && _currentDirList.Count > 0)
@@ -324,22 +265,11 @@ namespace MediaPortal.Extensions.MediaProviders.ZipMediaProvider
       return _currentDirList.Any(entry => (entry.IsDirectory) && (entry.Name.Equals(path, StringComparison.OrdinalIgnoreCase)));
     }
 
-    /// <summary>
-    /// Returns a resource which is located in the same underlaying media provider and which might be located relatively
-    /// to this resource.
-    /// </summary>
-    /// <param name="path">Relative or absolute path which is valid in the underlaying media provider.</param>
-    /// <returns>Resource accessor for the desired resource, if it exists, else <c>null</c>.</returns>
     public IResourceAccessor GetResource(string path)
     {
       return _provider.CreateResourceAccessor(_zipPath, path);
     }
 
-    /// <summary>
-    /// Returns the resource accessors for all child files of this directory resource.
-    /// </summary>
-    /// <returns>Collection of child resource accessors of sub files or <c>null</c>, if this resource
-    /// is no directory resource or if it is invalid.</returns>
     public ICollection<IFileSystemResourceAccessor> GetFiles()
     {
       if (string.IsNullOrEmpty(_pathOrFile))
@@ -349,11 +279,6 @@ namespace MediaPortal.Extensions.MediaProviders.ZipMediaProvider
       return files;
     }
 
-    /// <summary>
-    /// Returns the resource accessors for all child directories of this directory resource.
-    /// </summary>
-    /// <returns>Collection of child resource accessors of sub directories or <c>null</c>, if
-    /// this resource is no directory resource or if it is invalid in this provider.</returns>
     public ICollection<IFileSystemResourceAccessor> GetChildDirectories()
     {
       if (string.IsNullOrEmpty(_pathOrFile))
@@ -361,6 +286,15 @@ namespace MediaPortal.Extensions.MediaProviders.ZipMediaProvider
       ICollection<IFileSystemResourceAccessor> directories = new List<IFileSystemResourceAccessor>();
       CollectionUtils.AddAll(directories, _currentDirList.Where(entry => entry.IsDirectory).Select(directoryEntry => new ZipResourceAccessor(_provider, _zipPath, "/" + directoryEntry.Name)));
       return directories;
+    }
+
+    #endregion
+
+    #region Base overrides
+
+    public override string ToString()
+    {
+      return _resourcePath;
     }
 
     #endregion
