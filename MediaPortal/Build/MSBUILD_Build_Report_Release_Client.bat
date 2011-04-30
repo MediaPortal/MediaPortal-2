6abae4eda6cffce9b7e3055b13bfdc2b73146144
Albert <albert@team-mediaportal.com>
2011-04-30 22:12:50 +0200

    Switched MSBUILD build scripts to .net 4.0

--- MediaPortal/Build/MSBUILD_Build_Report_Release_Client.bat
+++ MediaPortal/Build/MSBUILD_Build_Report_Release_Client.bat
@@ -1,12 +1,12 @@
 rem @ECHO OFF
 
 
-"%WINDIR%\Microsoft.NET\Framework\v3.5\MSBUILD.exe" ..\Tools\BuildReport\BuildReport.sln /target:Rebuild  /property:Configuration=Release
+"%WINDIR%\Microsoft.NET\Framework\v4.0.30319\MSBUILD.exe" ..\Tools\BuildReport\BuildReport.sln /target:Rebuild  /property:Configuration=Release
 
 
 xcopy /Y ..\Tools\BuildReport\css\*.* .\css\
 xcopy /Y ..\Tools\BuildReport\images\*.* .\images\
 
-"%WINDIR%\Microsoft.NET\Framework\v3.5\MSBUILD.exe" ..\Source\MP2-Client.sln /target:Rebuild /property:Configuration=Release;Platform=x86 > MSBUILD_Build_Report_Release_x86.txt
+"%WINDIR%\Microsoft.NET\Framework\v4.0.30319\MSBUILD.exe" ..\Source\MP2-Client.sln /target:Rebuild /property:Configuration=Release;Platform=x86 > MSBUILD_Build_Report_Release_x86.txt
 
 ..\Tools\BuildReport\bin\x86\Release\BuildReport.exe --Input=MSBUILD_Build_Report_Release_x86.txt --Output=MSBUILD_Build_Report_Release_x86.html --Solution=MP2-Client.sln --Title="MediaPortal 2 Client - Build Report"
