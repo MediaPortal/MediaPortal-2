236e210a10a2ea01c8c5c39e3b40cfe90c8cefbe
morpheusxx <morpheus_xx@team-mediaportal.com>
2011-05-12 17:17:53 +0200

    Skin: renamed "NewSkin" to "Rising"

--- MediaPortal/Incubator/RisingSkin/Properties/AssemblyInfo.cs
+++ MediaPortal/Incubator/RisingSkin/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Allgemeine Informationen über eine Assembly werden über die folgenden 
 // Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
 // die mit einer Assembly verknüpft sind.
-[assembly: AssemblyTitle("EasyThemePack")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Rising Skin")]
+[assembly: AssemblyDescription("MP2 skin by morpheus_xx")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Team MediaPortal")]
-[assembly: AssemblyProduct("EasyThemePack")]
+[assembly: AssemblyProduct("MediaPortal 2")]
 [assembly: AssemblyCopyright("Team MediaPortal")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
