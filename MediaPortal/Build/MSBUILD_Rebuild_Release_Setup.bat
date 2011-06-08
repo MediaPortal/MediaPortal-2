4847a0d7c9ef8d64d7322e14b2ade5ab29f854f8
chefkoch <chefkoch@team-mediaportal.com>
2011-06-09 00:03:56 +0200

    ignore a windows firewall registration failure and continue with the installation (thx smeulf)

--- MediaPortal/Build/MSBUILD_Rebuild_Release_Setup.bat
+++ MediaPortal/Build/MSBUILD_Rebuild_Release_Setup.bat
@@ -2,6 +2,8 @@
 
 "%WINDIR%\Microsoft.NET\Framework\v4.0.30319\MSBUILD.exe" ..\Tools\MergeMSI\MergeMSI.sln /target:Rebuild  /property:Configuration=Release
 
+
 "%WINDIR%\Microsoft.NET\Framework\v4.0.30319\MSBUILD.exe" ..\Setup\MP2-Setup.sln /target:Rebuild  /property:Configuration=Release;Platform=x86 >> build_setup.log
 
-..\Tools\MergeMSI\bin\Release\MergeMSI.exe --TargetFileName="..\Setup\MP2-Setup\bin\Release\" --TargetDir="MP2-Setup.msi" >> build_setup.log
+
+..\Tools\MergeMSI\bin\Release\MergeMSI.exe --TargetDir=..\Setup\MP2-Setup\bin\Release\ --TargetFileName=MP2-Setup.msi >> build_setup.log
