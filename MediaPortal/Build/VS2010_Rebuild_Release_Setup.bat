8c40a9800dc24093a23bcc46f35aa67546996d21
chefkoch <chefkoch@team-mediaportal.com>
2011-06-09 00:50:03 +0200

    fixed MergeMSI parameters for vs build script, too

--- MediaPortal/Build/VS2010_Rebuild_Release_Setup.bat
+++ MediaPortal/Build/VS2010_Rebuild_Release_Setup.bat
@@ -7,4 +7,5 @@ if exist "%ProgramFiles(x86)%" set ProgramDir=%ProgramFiles(x86)%
 
 "%ProgramDir%\Microsoft Visual Studio 10.0\Common7\IDE\devenv.com" ..\Setup\MP2-Setup.sln /Rebuild "Release|x86" /Out build_setup.log
 
-..\Tools\MergeMSI\bin\Release\MergeMSI.exe --TargetFileName="..\Setup\MP2-Setup\bin\Release\" --TargetDir="MP2-Setup.msi" >> build_setup.log
+
+..\Tools\MergeMSI\bin\Release\MergeMSI.exe --TargetDir=..\Setup\MP2-Setup\bin\Release\ --TargetFileName=MP2-Setup.msi >> build_setup.log
