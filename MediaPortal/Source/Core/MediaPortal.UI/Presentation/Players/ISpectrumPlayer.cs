2f41582cc48546cc47f2e2b67894023ea3e760df
Albert <albert@team-mediaportal.com>
2012-01-16 19:18:26 +0100

    Improved docs in ISpectrumPlayer

--- MediaPortal/Source/Core/MediaPortal.UI/Presentation/Players/ISpectrumPlayer.cs
+++ MediaPortal/Source/Core/MediaPortal.UI/Presentation/Players/ISpectrumPlayer.cs
@@ -49,29 +49,30 @@
 namespace MediaPortal.UI.Presentation.Players
 {
   /// <summary>
-  /// Provides access to sound player functionality needed to
-  /// render a spectrum analyzer.
+  /// Provides access to sound player functionality needed to render a spectrum analyzer.
   /// </summary>
   public interface ISpectrumPlayer : IAudioPlayer
   {
     /// <summary>
-    /// Assigns current FFT data to a buffer.
+    /// Copies the current FFT data to a buffer.
     /// </summary>
     /// <remarks>
     /// The FFT data in the buffer should consist only of the real number intensity values. This means that if your FFT algorithm returns
     /// complex numbers (as many do), you'd run an algorithm similar to:
+    /// <code>
     /// for(int i = 0; i &lt; complexNumbers.Length / 2; i++)
     ///     fftResult[i] = Math.Sqrt(complexNumbers[i].Real * complexNumbers[i].Real + complexNumbers[i].Imaginary * complexNumbers[i].Imaginary);
+    /// </code>
     /// </remarks>
-    /// <param name="fftDataBuffer">The buffer to copy FFT data. The buffer should consist of only non-imaginary numbers.</param>
+    /// <param name="fftDataBuffer">The buffer to copy the FFT data to. The buffer should consist of only non-imaginary numbers.</param>
     /// <returns>True if data was written to the buffer, otherwise false.</returns>
     bool GetFFTData(float[] fftDataBuffer);
 
     /// <summary>
     /// Gets the index in the FFT data buffer for a given frequency.
     /// </summary>
-    /// <param name="frequency">The frequency for which to obtain a buffer index</param>
-    /// <returns>An index in the FFT data buffer</returns>
+    /// <param name="frequency">The frequency for which to obtain a buffer index.</param>
+    /// <returns>An index in the FFT data buffer which was returned by method <see cref="GetFFTData"/>.</returns>
     int GetFFTFrequencyIndex(int frequency);
   }
 }
