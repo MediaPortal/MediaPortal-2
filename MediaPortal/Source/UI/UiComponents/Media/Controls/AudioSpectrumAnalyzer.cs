b93d5f64e0ba05c9d2de1776fd0ce8f03b729c12
Albert <albert@team-mediaportal.com>
2012-01-17 17:33:30 +0100

    Removed project AudioSpectrumAnalyser, moved class and skin files to Media plugin

--- MediaPortal/Source/UI/UiComponents/Media/Controls/AudioSpectrumAnalyzer.cs
+++ MediaPortal/Source/UI/UiComponents/Media/Controls/AudioSpectrumAnalyzer.cs
@@ -60,8 +60,30 @@ using MediaPortal.UI.SkinEngine.MpfElements;
 using MediaPortal.UI.SkinEngine.Rendering;
 using MediaPortal.Utilities.DeepCopy;
 
-namespace MediaPortal.Plugins.AudioSpectrumAnalyzer
+namespace MediaPortal.UiComponents.Media.Controls
 {
+  /// <summary>
+  /// The different ways that the bar height can be scaled by the spectrum analyzer.
+  /// </summary>
+  public enum BarHeightScalingStyles
+  {
+    /// <summary>
+    /// A decibel scale. Formula: 20 * Log10(FFTValue). Total bar height
+    /// is scaled from -90 to 0 dB.
+    /// </summary>
+    Decibel,
+
+    /// <summary>
+    /// A non-linear squareroot scale. Formula: Sqrt(FFTValue) * 2 * BarHeight.
+    /// </summary>
+    Sqrt,
+
+    /// <summary>
+    /// A linear scale. Formula: 9 * FFTValue * BarHeight.
+    /// </summary>
+    Linear
+  }
+
   public class AudioSpectrumAnalyzer : Control
   {
     #region Consts
