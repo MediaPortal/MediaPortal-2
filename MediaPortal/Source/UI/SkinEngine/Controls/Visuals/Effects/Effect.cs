6947b32700bfcab63213ec5b2c883716da13f09c
morpheusxx <morpheus_xx@team-mediaportal.com>
2012-01-27 19:14:47 +0100

    Created Effect base class, extended UIElement with Effect property

---
+++ MediaPortal/Source/UI/SkinEngine/Controls/Visuals/Effects/Effect.cs
@@ -0,0 +1,9 @@
+﻿using MediaPortal.UI.SkinEngine.MpfElements;
+
+namespace MediaPortal.UI.SkinEngine.Controls.Visuals.Effects
+{
+  public abstract class Effect : DependencyObject
+  {
+    // TODO: what to have in base class?  
+  }
+}
