54e16194a32fdf68cd9401ceae6e3e6ace7148f3
Albert <albert@team-mediaportal.com>
2012-02-22 19:22:47 +0100

    Added table to identify the database type as an MP2 database

--- MediaPortal/Source/Base/Server/Scripts/MediaPortal-Basis-create-1.0.sql
+++ MediaPortal/Source/Base/Server/Scripts/MediaPortal-Basis-create-1.0.sql
@@ -1,5 +1,15 @@
 -- This script creates the MediaPortal basis schema. DO NOT MODIFY!
 
+-- Table is not used by the application - it's just to identify the database belongs to MP2
+-- when explored with an universal DB tool
+CREATE TABLE PRODUCT_VERSION (
+  PRODUCT %STRING(100)% NOT NULL,
+  VERSION %STRING(10)%
+);
+
+INSERT INTO PRODUCT_VERSION (PRODUCT, VERSION) VALUES ('MediaPortal', '2.0');
+
+
 -- Dummy table, like "DUAL" in Oracle
 CREATE TABLE DUMMY (
   X %INTEGER%
