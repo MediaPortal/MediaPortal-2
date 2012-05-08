59f2a4abf52c00375e5ddefe31ea52c0005c4f0f
chefkoch <chefkoch@team-mediaportal.com>
2012-05-08 21:54:47 +0200

    added pull script

---
+++ MediaPortal/tx_4_Pull_Translations.bat
@@ -0,0 +1,11 @@
+@echo off
+
+echo.
+echo   pulling translations from the server. for more info read this: 
+echo      http://help.transifex.com/features/client/index.html#pull
+echo.
+echo   to execute it un-echo the following command.
+echo      tx pull -f
+echo.
+
+pause
