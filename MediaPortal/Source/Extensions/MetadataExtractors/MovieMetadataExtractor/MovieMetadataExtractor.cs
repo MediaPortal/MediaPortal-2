73f16b76f17453d1b937fc99d9dfc07a12bd7f04
morpheusxx <morpheus_xx@team-mediaportal.com>
2012-06-01 20:32:47 +0200

    New MovieMetadataExtractor, does online lookup based on filled title (and optional year) information

---
+++ MediaPortal/Source/Extensions/MetadataExtractors/MovieMetadataExtractor/MovieMetadataExtractor.cs
@@ -0,0 +1,140 @@
+#region Copyright (C) 2007-2012 Team MediaPortal
+
+/*
+    Copyright (C) 2007-2012 Team MediaPortal
+    http://www.team-mediaportal.com
+
+    This file is part of MediaPortal 2
+
+    MediaPortal 2 is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    MediaPortal 2 is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with MediaPortal 2. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#endregion
+
+using System;
+using System.Collections.Generic;
+using MediaPortal.Common;
+using MediaPortal.Common.Logging;
+using MediaPortal.Common.MediaManagement;
+using MediaPortal.Common.MediaManagement.DefaultItemAspects;
+using MediaPortal.Common.MediaManagement.Helpers;
+using MediaPortal.Common.ResourceAccess;
+using MediaPortal.Common.Services.ResourceAccess.StreamedResourceToLocalFsAccessBridge;
+using MediaPortal.Extensions.OnlineLibraries;
+
+namespace MediaPortal.Extensions.MetadataExtractors.MovieMetadataExtractor
+{
+  /// <summary>
+  /// MediaPortal 2 metadata extractor implementation for Series.
+  /// </summary>
+  public class MovieMetadataExtractor : IMetadataExtractor
+  {
+    #region Public constants
+
+    /// <summary>
+    /// GUID string for the video metadata extractor.
+    /// </summary>
+    public const string METADATAEXTRACTOR_ID_STR = "C2800928-8A57-4979-A95F-3CE6F3EBAB92";
+
+    /// <summary>
+    /// Video metadata extractor GUID.
+    /// </summary>
+    public static Guid METADATAEXTRACTOR_ID = new Guid(METADATAEXTRACTOR_ID_STR);
+
+    #endregion
+
+    #region Protected fields and classes
+
+    protected static IList<string> SHARE_CATEGORIES = new List<string>();
+    protected static IList<string> VIDEO_FILE_EXTENSIONS = new List<string>();
+
+    protected MetadataExtractorMetadata _metadata;
+
+    #endregion
+
+    #region Ctor
+
+    static MovieMetadataExtractor()
+    {
+      SHARE_CATEGORIES.Add(SpecializedCategory.Movie.ToString());
+    }
+
+    public MovieMetadataExtractor()
+    {
+      _metadata = new MetadataExtractorMetadata(METADATAEXTRACTOR_ID, "Movies metadata extractor", MetadataExtractorPriority.External, true,
+          SHARE_CATEGORIES, new[]
+              {
+                MediaAspect.Metadata,
+                MovieAspect.Metadata
+              });
+    }
+
+    #endregion
+
+    #region IMetadataExtractor implementation
+
+    public MetadataExtractorMetadata Metadata
+    {
+      get { return _metadata; }
+    }
+
+    public bool TryExtractMetadata(IResourceAccessor mediaItemAccessor, IDictionary<Guid, MediaItemAspect> extractedAspectData, bool forceQuickMode)
+    {
+      try
+      {
+        if (forceQuickMode)
+          return false;
+
+        using (IResourceAccessor ra = mediaItemAccessor.Clone())
+        using (ILocalFsResourceAccessor lfsra = StreamedResourceToLocalFsAccessBridge.GetLocalFsResourceAccessor(ra))
+        {
+          string localFsPath = lfsra.LocalFileSystemPath;
+          return ExtractMovieData(localFsPath, extractedAspectData);
+        }
+      }
+      catch (Exception e)
+      {
+        // Only log at the info level here - And simply return false. This lets the caller know that we
+        // couldn't perform our task here.
+        ServiceRegistration.Get<ILogger>().Info("MoviesMetadataExtractor: Exception reading resource '{0}' (Text: '{1}')", mediaItemAccessor.CanonicalLocalResourcePath, e.Message);
+      }
+      return false;
+    }
+
+    private bool ExtractMovieData(string localFsPath, IDictionary<Guid, MediaItemAspect> extractedAspectData)
+    {
+      string title;
+      if (MediaItemAspect.TryGetAttribute(extractedAspectData, MediaAspect.ATTR_TITLE, out title) && !string.IsNullOrEmpty(title))
+      {
+        MovieInfo movieInfo = new MovieInfo
+        {
+          MovieName = title,
+        };
+      
+        DateTime recordingDate;
+        if (MediaItemAspect.TryGetAttribute(extractedAspectData, MediaAspect.ATTR_RECORDINGTIME, out recordingDate))
+          movieInfo.Year = recordingDate.Year;
+
+        if (MovieTheMovieDbMatcher.Instance.FindAndUpdateMovie(movieInfo))
+        {
+          movieInfo.SetMetadata(extractedAspectData);
+          return true;
+        }
+      }
+      return false;
+    }
+
+    #endregion
+  }
+}
