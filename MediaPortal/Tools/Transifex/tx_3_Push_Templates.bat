fc2877a95645454b074800faf38b97ac0578d967
chefkoch <chefkoch@team-mediaportal.com>
2012-06-05 19:43:00 +0200

    modified push & pull scripts

--- MediaPortal/Tools/Transifex/tx_3_Push_Templates.bat
+++ MediaPortal/Tools/Transifex/tx_3_Push_Templates.bat
@@ -1,8 +1,5 @@
 @echo off
 
-set ProgramDir=%ProgramFiles%
-if exist "%ProgramFiles(x86)%" set ProgramDir=%ProgramFiles(x86)%
+tx push -s
 
-"%ProgramDir%\Microsoft Visual Studio 10.0\Common7\IDE\devenv.com" /rebuild Release ..\TransifexHelper\TransifexHelper.sln > push.log
-
-..\TransifexHelper\bin\Release\TransifexHelper_MP2.exe --push -t "%cd%\..\.." >> push.log
+pause
--- MediaPortal/Tools/Transifex/tx_4_Pull_Translations.bat
+++ MediaPortal/Tools/Transifex/tx_4_Pull_Translations.bat
@@ -1,8 +1,5 @@
 @echo off
 
-set ProgramDir=%ProgramFiles%
-if exist "%ProgramFiles(x86)%" set ProgramDir=%ProgramFiles(x86)%
+tx pull -a --mode translator
 
-"%ProgramDir%\Microsoft Visual Studio 10.0\Common7\IDE\devenv.com" /rebuild Release ..\TransifexHelper\TransifexHelper.sln > pull.log
-
-..\TransifexHelper\bin\Release\TransifexHelper_MP2.exe --pull -t "%cd%\..\.." >> pull.log
+pause
