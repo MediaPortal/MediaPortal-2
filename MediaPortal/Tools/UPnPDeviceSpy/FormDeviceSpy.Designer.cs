840a44a0380ebb254e2315d0756eb71b7bfef25d
Albert <albert@team-mediaportal.com>
2012-06-13 20:19:06 +0200

    Added UPnP device spy tool

---
+++ MediaPortal/Tools/UPnPDeviceSpy/FormDeviceSpy.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace UPnPDeviceSpy
+{
+  partial class FormDeviceSpy
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormDeviceSpy));
+      this.menuStripMainMenu = new System.Windows.Forms.MenuStrip();
+      this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+      this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+      this.splitContainerMain = new System.Windows.Forms.SplitContainer();
+      this.tvDeviceTree = new System.Windows.Forms.TreeView();
+      this.tbDetails = new System.Windows.Forms.TextBox();
+      this.toolStrip = new System.Windows.Forms.ToolStrip();
+      this.bRefresh = new System.Windows.Forms.ToolStripButton();
+      this.bUPnPSearch = new System.Windows.Forms.ToolStripButton();
+      this.menuStripMainMenu.SuspendLayout();
+      this.splitContainerMain.Panel1.SuspendLayout();
+      this.splitContainerMain.Panel2.SuspendLayout();
+      this.splitContainerMain.SuspendLayout();
+      this.toolStrip.SuspendLayout();
+      this.SuspendLayout();
+      // 
+      // menuStripMainMenu
+      // 
+      this.menuStripMainMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+      this.menuStripMainMenu.Location = new System.Drawing.Point(0, 0);
+      this.menuStripMainMenu.Name = "menuStripMainMenu";
+      this.menuStripMainMenu.Size = new System.Drawing.Size(907, 24);
+      this.menuStripMainMenu.TabIndex = 0;
+      // 
+      // fileToolStripMenuItem
+      // 
+      this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+      this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+      this.fileToolStripMenuItem.Size = new System.Drawing.Size(65, 20);
+      this.fileToolStripMenuItem.Text = "&Program";
+      // 
+      // exitToolStripMenuItem
+      // 
+      this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+      this.exitToolStripMenuItem.Size = new System.Drawing.Size(92, 22);
+      this.exitToolStripMenuItem.Text = "&Exit";
+      this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+      // 
+      // splitContainerMain
+      // 
+      this.splitContainerMain.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+      this.splitContainerMain.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+      this.splitContainerMain.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+      this.splitContainerMain.Location = new System.Drawing.Point(0, 52);
+      this.splitContainerMain.Name = "splitContainerMain";
+      // 
+      // splitContainerMain.Panel1
+      // 
+      this.splitContainerMain.Panel1.AutoScroll = true;
+      this.splitContainerMain.Panel1.Controls.Add(this.tvDeviceTree);
+      // 
+      // splitContainerMain.Panel2
+      // 
+      this.splitContainerMain.Panel2.Controls.Add(this.tbDetails);
+      this.splitContainerMain.Size = new System.Drawing.Size(907, 615);
+      this.splitContainerMain.SplitterDistance = 250;
+      this.splitContainerMain.TabIndex = 2;
+      // 
+      // tvDeviceTree
+      // 
+      this.tvDeviceTree.Dock = System.Windows.Forms.DockStyle.Fill;
+      this.tvDeviceTree.HideSelection = false;
+      this.tvDeviceTree.HotTracking = true;
+      this.tvDeviceTree.Location = new System.Drawing.Point(0, 0);
+      this.tvDeviceTree.Name = "tvDeviceTree";
+      this.tvDeviceTree.Size = new System.Drawing.Size(246, 611);
+      this.tvDeviceTree.TabIndex = 2;
+      this.tvDeviceTree.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.tvDeviceTree_AfterSelect);
+      // 
+      // tbDetails
+      // 
+      this.tbDetails.Dock = System.Windows.Forms.DockStyle.Fill;
+      this.tbDetails.Location = new System.Drawing.Point(0, 0);
+      this.tbDetails.Multiline = true;
+      this.tbDetails.Name = "tbDetails";
+      this.tbDetails.ReadOnly = true;
+      this.tbDetails.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+      this.tbDetails.Size = new System.Drawing.Size(649, 611);
+      this.tbDetails.TabIndex = 0;
+      // 
+      // toolStrip
+      // 
+      this.toolStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.bRefresh,
+            this.bUPnPSearch});
+      this.toolStrip.LayoutStyle = System.Windows.Forms.ToolStripLayoutStyle.HorizontalStackWithOverflow;
+      this.toolStrip.Location = new System.Drawing.Point(0, 24);
+      this.toolStrip.Name = "toolStrip";
+      this.toolStrip.Size = new System.Drawing.Size(907, 25);
+      this.toolStrip.TabIndex = 3;
+      this.toolStrip.Text = "toolStrip1";
+      // 
+      // bRefresh
+      // 
+      this.bRefresh.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+      this.bRefresh.Image = ((System.Drawing.Image)(resources.GetObject("bRefresh.Image")));
+      this.bRefresh.ImageTransparentColor = System.Drawing.Color.Magenta;
+      this.bRefresh.Name = "bRefresh";
+      this.bRefresh.Size = new System.Drawing.Size(23, 22);
+      this.bRefresh.Text = "&Refresh";
+      this.bRefresh.Click += new System.EventHandler(this.bRefresh_Click);
+      // 
+      // bUPnPSearch
+      // 
+      this.bUPnPSearch.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+      this.bUPnPSearch.Image = ((System.Drawing.Image)(resources.GetObject("bUPnPSearch.Image")));
+      this.bUPnPSearch.ImageTransparentColor = System.Drawing.Color.Magenta;
+      this.bUPnPSearch.Name = "bUPnPSearch";
+      this.bUPnPSearch.Size = new System.Drawing.Size(23, 22);
+      this.bUPnPSearch.Text = "UPnP &search";
+      this.bUPnPSearch.Click += new System.EventHandler(this.bUPnPSearch_Click);
+      // 
+      // FormDeviceSpy
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(907, 666);
+      this.Controls.Add(this.toolStrip);
+      this.Controls.Add(this.splitContainerMain);
+      this.Controls.Add(this.menuStripMainMenu);
+      this.MainMenuStrip = this.menuStripMainMenu;
+      this.Name = "FormDeviceSpy";
+      this.Text = "Device Spy";
+      this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FormDeviceSpy_FormClosing);
+      this.Shown += new System.EventHandler(this.FormDeviceSpy_Shown);
+      this.menuStripMainMenu.ResumeLayout(false);
+      this.menuStripMainMenu.PerformLayout();
+      this.splitContainerMain.Panel1.ResumeLayout(false);
+      this.splitContainerMain.Panel2.ResumeLayout(false);
+      this.splitContainerMain.Panel2.PerformLayout();
+      this.splitContainerMain.ResumeLayout(false);
+      this.toolStrip.ResumeLayout(false);
+      this.toolStrip.PerformLayout();
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.MenuStrip menuStripMainMenu;
+    private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+    private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+    private System.Windows.Forms.SplitContainer splitContainerMain;
+    private System.Windows.Forms.TreeView tvDeviceTree;
+    private System.Windows.Forms.ToolStrip toolStrip;
+    private System.Windows.Forms.ToolStripButton bRefresh;
+    private System.Windows.Forms.TextBox tbDetails;
+    private System.Windows.Forms.ToolStripButton bUPnPSearch;
+  }
+}
+
---
+++ MediaPortal/Tools/UPnPDeviceSpy/FormDeviceSpy.cs
@@ -0,0 +1,150 @@
+﻿#region Copyright (C) 2007-2012 Team MediaPortal
+
+/*
+    Copyright (C) 2007-2012 Team MediaPortal
+    http://www.team-mediaportal.com
+
+    This file is part of MediaPortal 2
+
+    MediaPortal 2 is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    MediaPortal 2 is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with MediaPortal 2. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#endregion
+
+using System.Collections.Generic;
+using System.Windows.Forms;
+using UPnP.Infrastructure.CP;
+using UPnP.Infrastructure.CP.Description;
+
+namespace UPnPDeviceSpy
+{
+  public partial class FormDeviceSpy : Form
+  {
+    protected UPnPNetworkTracker _networkTracker;
+
+    public FormDeviceSpy()
+    {
+      InitializeComponent();
+      CPData cpData = new CPData();
+      _networkTracker = new UPnPNetworkTracker(cpData);
+    }
+
+    protected void UpdateTreeView()
+    {
+      UpdateTreeView(false);
+    }
+
+    protected void UpdateTreeView(bool showPending)
+    {
+      TreeNodeCollection nodes = tvDeviceTree.Nodes;
+      nodes.Clear();
+      TreeNode devicesNode = nodes.Add("DevicesKey", "Devices");
+      IDictionary<string, RootDescriptor> knownDevices = _networkTracker.KnownRootDevices;
+      try
+      {
+        if (knownDevices == null)
+        {
+          if (showPending)
+            devicesNode.Nodes.Add(null, " - pending -");
+          return;
+        }
+        foreach (RootDescriptor rootDescriptor in knownDevices.Values)
+        {
+          if (rootDescriptor.State != RootDescriptorState.Ready)
+            continue;
+          string deviceUUID = rootDescriptor.SSDPRootEntry.RootDeviceUUID;
+          DeviceDescriptor deviceDescriptor = DeviceDescriptor.CreateRootDeviceDescriptor(rootDescriptor);
+          TreeNode deviceNode = devicesNode.Nodes.Add(deviceUUID, BuildDeviceName(deviceDescriptor));
+          deviceNode.Tag = new UPnPDeviceDescriptor(deviceDescriptor);
+        }
+      }
+      finally
+      {
+        devicesNode.Expand();
+      }
+    }
+
+    protected void ClearDetails()
+    {
+      tbDetails.Clear();
+    }
+
+    protected void ShowDetails(UPnPNodeDescriptor descriptor)
+    {
+      tbDetails.Clear();
+      UPnPDeviceDescriptor deviceDescriptor = descriptor as UPnPDeviceDescriptor;
+      if (deviceDescriptor != null)
+        tbDetails.Text = deviceDescriptor.Descriptor.RootDescriptor.DeviceDescription.CreateNavigator().OuterXml;
+    }
+
+    protected string BuildDeviceName(DeviceDescriptor descriptor)
+    {
+      return descriptor.FriendlyName + " (" + descriptor.RootDescriptor.SSDPRootEntry.PreferredLink.DescriptionLocation + ")";
+    }
+
+    private void exitToolStripMenuItem_Click(object sender, System.EventArgs e)
+    {
+      Close();
+    }
+
+    private void FormDeviceSpy_Shown(object sender, System.EventArgs e)
+    {
+      UpdateTreeView(true);
+      _networkTracker.RootDeviceAdded += networkTracker_RootDeviceAdded;
+      _networkTracker.RootDeviceRemoved += networkTracker_RootDeviceRemoved;
+      _networkTracker.Start();
+    }
+
+    protected delegate void Dlgt();
+
+    protected void ExecuteInUIThread(Dlgt dlgt)
+    {
+      Invoke(dlgt);
+    }
+
+    private void networkTracker_RootDeviceAdded(RootDescriptor rootdescriptor)
+    {
+      ExecuteInUIThread(UpdateTreeView);
+    }
+
+    private void networkTracker_RootDeviceRemoved(RootDescriptor rootdescriptor)
+    {
+      ExecuteInUIThread(UpdateTreeView);
+    }
+
+    private void FormDeviceSpy_FormClosing(object sender, FormClosingEventArgs e)
+    {
+      _networkTracker.Close();
+    }
+
+    private void bRefresh_Click(object sender, System.EventArgs e)
+    {
+      UpdateTreeView();
+    }
+
+    private void tvDeviceTree_AfterSelect(object sender, TreeViewEventArgs e)
+    {
+      TreeNode selectedNode = tvDeviceTree.SelectedNode;
+      if (selectedNode == null)
+        ClearDetails();
+      else
+        ShowDetails(selectedNode.Tag as UPnPNodeDescriptor);
+    }
+
+    private void bUPnPSearch_Click(object sender, System.EventArgs e)
+    {
+      _networkTracker.SharedControlPointData.SSDPController.SearchAll(null);
+    }
+  }
+}
