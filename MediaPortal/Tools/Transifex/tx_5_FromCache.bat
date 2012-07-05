9def3027e5e7da2943aea1bf92516ad840b5333c
chefkoch <chefkoch@team-mediaportal.com>
2012-07-05 18:45:42 +0200

    fixed log file names

--- MediaPortal/Tools/Transifex/tx_5_FromCache.bat
+++ MediaPortal/Tools/Transifex/tx_5_FromCache.bat
@@ -1,3 +1,3 @@
 @echo off
-@"%WINDIR%\Microsoft.NET\Framework\v4.0.30319\MSBUILD.exe" /target:Rebuild /property:Configuration=Release;Platform=x86 ..\TransifexHelper\TransifexHelper.sln > verify.log
-..\TransifexHelper\bin\x86\Release\TransifexHelper.exe --FromCache -t "%cd%\..\.." >> push.log
+@"%WINDIR%\Microsoft.NET\Framework\v4.0.30319\MSBUILD.exe" /target:Rebuild /property:Configuration=Release;Platform=x86 ..\TransifexHelper\TransifexHelper.sln > FromCache.log
+..\TransifexHelper\bin\x86\Release\TransifexHelper.exe --FromCache -t "%cd%\..\.." >> FromCache.log
