b29e5d106de2d5ae19fa5c1ad912a9256b3ad882
Albert <albert@team-mediaportal.com>
2012-07-22 11:46:59 +0200

    Added code test plugin

---
+++ MediaPortal/Resources/CodeTestPlugin/CodeTestModel.cs
@@ -0,0 +1,100 @@
+#region Copyright (C) 2007-2012 Team MediaPortal
+
+/*
+    Copyright (C) 2007-2012 Team MediaPortal
+    http://www.team-mediaportal.com
+
+    This file is part of MediaPortal II
+
+    MediaPortal II is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    MediaPortal II is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with MediaPortal II.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#endregion
+
+using System;
+using System.Collections.Generic;
+using MediaPortal.Common;
+using MediaPortal.UI.Presentation.Models;
+using MediaPortal.UI.Presentation.Workflow;
+
+namespace MediaPortal.Test.CodeTest
+{
+  /// <summary>
+  /// Model which atends the code test workflow state.
+  /// </summary>
+  public class CodeTestModel : IWorkflowModel
+  {
+    public const string MODEL_ID_STR = "3E07F585-C3DE-4FB0-BD18-707AD9C78861";
+
+    #region Protected fields
+
+    #endregion
+
+    #region Public members
+
+    public void ShowScreenInTransientState(string screen)
+    {
+      IWorkflowManager workflowManager = ServiceRegistration.Get<IWorkflowManager>();
+      workflowManager.NavigatePushTransient(new WorkflowState(Guid.NewGuid(), screen, screen, true, screen, false, true, ModelId, WorkflowType.Workflow), null);
+    }
+
+    #endregion
+
+    #region IWorkflowModel implementation
+
+    public Guid ModelId
+    {
+      get { return new Guid(MODEL_ID_STR); }
+    }
+
+    public bool CanEnterState(NavigationContext oldContext, NavigationContext newContext)
+    {
+      return true;
+    }
+
+    public void EnterModelContext(NavigationContext oldContext, NavigationContext newContext)
+    {
+      // We could initialize some data here when entering the media navigation state
+    }
+
+    public void ExitModelContext(NavigationContext oldContext, NavigationContext newContext)
+    {
+      // We could dispose some data here when exiting media navigation context
+    }
+
+    public void ChangeModelContext(NavigationContext oldContext, NavigationContext newContext, bool push)
+    {
+      // We could initialize some data here when changing the media navigation state
+    }
+
+    public void Deactivate(NavigationContext oldContext, NavigationContext newContext)
+    {
+    }
+
+    public void Reactivate(NavigationContext oldContext, NavigationContext newContext)
+    {
+    }
+
+    public void UpdateMenuActions(NavigationContext context, IDictionary<Guid, WorkflowAction> actions)
+    {
+    }
+
+    public ScreenUpdateMode UpdateScreen(NavigationContext context, ref string screen)
+    {
+      return ScreenUpdateMode.AutoWorkflowManager;
+    }
+
+    #endregion
+  }
+}
---
+++ MediaPortal/Resources/CodeTestPlugin/RATestModel.cs
@@ -0,0 +1,84 @@
+#region Copyright (C) 2007-2012 Team MediaPortal
+
+/*
+    Copyright (C) 2007-2012 Team MediaPortal
+    http://www.team-mediaportal.com
+
+    This file is part of MediaPortal II
+
+    MediaPortal II is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    MediaPortal II is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with MediaPortal II.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#endregion
+
+using System;
+using System.Linq;
+using MediaPortal.Common;
+using MediaPortal.Common.General;
+using MediaPortal.Common.Logging;
+using MediaPortal.Common.MediaManagement;
+using MediaPortal.Common.ResourceAccess;
+using MediaPortal.UI.Shares;
+
+namespace MediaPortal.Test.CodeTest
+{
+  public class RATestModel
+  {
+    public readonly string STR_ID_MODEL = "81679E2D-C267-4E7E-97F6-792989123DAF";
+
+    protected AbstractProperty _resourcePathStrProperty = new WProperty(typeof(string), string.Empty);
+
+    public RATestModel()
+    {
+      ResourcePathStr = "{e88e64a8-0233-4fdf-ba27-0b44c6a39ae9}:///Path.To.iso>{112728b1-f71d-4284-9e5c-3462e8d3c74d}:///";
+    }
+
+    public AbstractProperty ResourcePathStrProperty
+    {
+      get { return _resourcePathStrProperty; }
+    }
+
+    public string ResourcePathStr
+    {
+      get { return (string) _resourcePathStrProperty.GetValue(); }
+      set { _resourcePathStrProperty.SetValue(value); }
+    }
+
+    public void DoImport()
+    {
+      ILocalSharesManagement lsm = ServiceRegistration.Get<ILocalSharesManagement>();
+      Share videoShare = lsm.Shares.Values.FirstOrDefault(share => share.Name.Contains("Video"));
+      if (videoShare == null)
+        return;
+      IImporterWorker importerWorker = ServiceRegistration.Get<IImporterWorker>();
+      importerWorker.ScheduleImport(videoShare.BaseResourcePath, videoShare.MediaCategories, true);
+    }
+
+    public void CreateResourceAccessor()
+    {
+      try
+      {
+        string resourcePathStr = ResourcePathStr;
+        ResourcePath path = ResourcePath.Deserialize(resourcePathStr);
+        IResourceAccessor ra;
+        if (path.TryCreateLocalResourceAccessor(out ra))
+          ra.Dispose();
+      }
+      catch (Exception e)
+      {
+        ServiceRegistration.Get<ILogger>().Error("Error creating resource accessor", e);
+      }
+    }
+  }
+}
