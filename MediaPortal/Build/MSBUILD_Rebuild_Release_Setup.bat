6c8a6a17498dc4c5c69f825d697f5eeb8aeeb78b
chefkoch <chefkoch@team-mediaportal.com>
2012-07-24 16:46:07 +0200

    commented out mergeMSI calls, since installer is English only currently

--- MediaPortal/Build/MSBUILD_Rebuild_Release_Setup.bat
+++ MediaPortal/Build/MSBUILD_Rebuild_Release_Setup.bat
@@ -1,9 +1,9 @@
 @ECHO OFF
 
-"%WINDIR%\Microsoft.NET\Framework\v4.0.30319\MSBUILD.exe" ..\Tools\MergeMSI\MergeMSI.sln /target:Rebuild  /property:Configuration=Release > build_setup.log
+rem "%WINDIR%\Microsoft.NET\Framework\v4.0.30319\MSBUILD.exe" ..\Tools\MergeMSI\MergeMSI.sln /target:Rebuild  /property:Configuration=Release > build_setup.log
 
 
 "%WINDIR%\Microsoft.NET\Framework\v4.0.30319\MSBUILD.exe" ..\Setup\MP2-Setup.sln /target:Rebuild  /property:Configuration=Release;Platform=x86 >> build_setup.log
 
 
-..\Tools\MergeMSI\bin\Release\MergeMSI.exe --TargetDir=..\Setup\MP2-Setup\bin\Release\ --TargetFileName=MP2-Setup.msi >> build_setup.log
+rem ..\Tools\MergeMSI\bin\Release\MergeMSI.exe --TargetDir=..\Setup\MP2-Setup\bin\Release\ --TargetFileName=MP2-Setup.msi >> build_setup.log
