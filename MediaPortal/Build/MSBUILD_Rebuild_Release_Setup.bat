3d769fe7dbd835ae99c63a8a2f7afe843c699b29
chefkoch <chefkoch@team-mediaportal.com>
2012-08-01 20:03:01 +0200

    using own log files for building server, slient and setup (avoid overwriting)

--- MediaPortal/Build/MSBUILD_Rebuild_Release_Setup.bat
+++ MediaPortal/Build/MSBUILD_Rebuild_Release_Setup.bat
@@ -1,9 +1,9 @@
 @ECHO OFF
 
-rem "%WINDIR%\Microsoft.NET\Framework\v4.0.30319\MSBUILD.exe" ..\Tools\MergeMSI\MergeMSI.sln /target:Rebuild  /property:Configuration=Release > build_setup.log
+rem "%WINDIR%\Microsoft.NET\Framework\v4.0.30319\MSBUILD.exe" ..\Tools\MergeMSI\MergeMSI.sln /target:Rebuild  /property:Configuration=Release > Build_Setup.log
 
 
-"%WINDIR%\Microsoft.NET\Framework\v4.0.30319\MSBUILD.exe" ..\Setup\MP2-Setup.sln /target:Rebuild  /property:Configuration=Release;Platform=x86 >> build_setup.log
+"%WINDIR%\Microsoft.NET\Framework\v4.0.30319\MSBUILD.exe" ..\Setup\MP2-Setup.sln /target:Rebuild  /property:Configuration=Release;Platform=x86 > Build_Setup.log
 
 
-rem ..\Tools\MergeMSI\bin\Release\MergeMSI.exe --TargetDir=..\Setup\MP2-Setup\bin\Release\ --TargetFileName=MP2-Setup.msi >> build_setup.log
+rem ..\Tools\MergeMSI\bin\Release\MergeMSI.exe --TargetDir=..\Setup\MP2-Setup\bin\Release\ --TargetFileName=MP2-Setup.msi >> Build_Setup.log
--- MediaPortal/Build/VS2010_Rebuild_Release_Setup.bat
+++ MediaPortal/Build/VS2010_Rebuild_Release_Setup.bat
@@ -5,7 +5,7 @@ if exist "%ProgramFiles(x86)%" set ProgramDir=%ProgramFiles(x86)%
 
 rem "%ProgramDir%\Microsoft Visual Studio 10.0\Common7\IDE\devenv.com" /rebuild Release ..\Tools\MergeMSI\MergeMSI.sln
 
-"%ProgramDir%\Microsoft Visual Studio 10.0\Common7\IDE\devenv.com" ..\Setup\MP2-Setup.sln /Rebuild "Release|x86" /Out build_setup.log
+"%ProgramDir%\Microsoft Visual Studio 10.0\Common7\IDE\devenv.com" ..\Setup\MP2-Setup.sln /Rebuild "Release|x86" /Out Build_Setup.log
 
 
-rem ..\Tools\MergeMSI\bin\Release\MergeMSI.exe --TargetDir=..\Setup\MP2-Setup\bin\Release\ --TargetFileName=MP2-Setup.msi >> build_setup.log
+rem ..\Tools\MergeMSI\bin\Release\MergeMSI.exe --TargetDir=..\Setup\MP2-Setup\bin\Release\ --TargetFileName=MP2-Setup.msi >> Build_Setup.log
