2fb978800a8343c9edb08d93e410656397049a80
chefkoch <chefkoch@team-mediaportal.com>
2012-08-18 12:59:55 +0200

    moved call of transifex scripts into it's own batch file

--- MediaPortal/Build/MSBUILD_Create_Full_Installer.bat
+++ MediaPortal/Build/MSBUILD_Create_Full_Installer.bat
@@ -3,12 +3,8 @@ echo Deleting binary folder > BuildInstaller.log
 rmdir /s /q ..\bin
 
 echo Updating language resources from transifex >> BuildInstaller.log
-cd ..\Tools\Transifex\
-call tx_2_ToCache.bat
-call tx_4_Pull_Translations.bat
-call tx_5_FromCache.bat
+call TRANSIFEX_Update_Translation_Files.bat
 
-cd ..\..\Build
 echo Rebuilding Server >> BuildInstaller.log
 call MSBUILD_Build_Report_Release_Server.bat
 
---
+++ MediaPortal/Build/TRANSIFEX_Update_Translation_Files.bat
@@ -0,0 +1,6 @@
+
+cd ..\Tools\Transifex\
+call tx_2_ToCache.bat
+call tx_4_Pull_Translations.bat
+call tx_5_FromCache.bat
+cd ..\..\Build
