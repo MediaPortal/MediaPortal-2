11e87395840d4e80efbcbb8ad3153ad9ed6f0847
chefkoch <chefkoch@team-mediaportal.com>
2012-09-07 18:00:22 +0200

    added Transifex script for fixing Translations

--- MediaPortal/Build/TRANSIFEX_Update_Translation_Files.bat
+++ MediaPortal/Build/TRANSIFEX_Update_Translation_Files.bat
@@ -2,5 +2,6 @@
 cd ..\Tools\Transifex\
 call tx_2_ToCache.bat
 call tx_4_Pull_Translations.bat
-call tx_5_FromCache.bat
+call tx_5_Fix_Translations.bat
+call tx_6_FromCache.bat
 cd ..\..\Build
---
+++ MediaPortal/Tools/Transifex/tx_5_Fix_Translations.bat
@@ -0,0 +1,3 @@
+
+@"%WINDIR%\Microsoft.NET\Framework\v4.0.30319\MSBUILD.exe" /target:Rebuild /property:Configuration=Release;Platform=x86 ..\TransifexHelper\TransifexHelper.sln > fix.log
+..\TransifexHelper\bin\x86\Release\TransifexHelper.exe --fix -t "%cd%\..\.." >> fix.log
