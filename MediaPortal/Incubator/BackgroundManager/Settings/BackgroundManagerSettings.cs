2522afdcbb5224b0d5801e324f4dec221c2de901
morpheusxx <morpheus_xx@team-mediaportal.com>
2012-10-14 11:59:07 +0200

    Implemented settings object, setup model, dialog and language

---
+++ MediaPortal/Incubator/BackgroundManager/Settings/BackgroundManagerSettings.cs
@@ -0,0 +1,43 @@
+﻿#region Copyright (C) 2007-2012 Team MediaPortal
+
+/*
+    Copyright (C) 2007-2012 Team MediaPortal
+    http://www.team-mediaportal.com
+
+    This file is part of MediaPortal 2
+
+    MediaPortal 2 is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    MediaPortal 2 is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with MediaPortal 2. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#endregion
+
+using MediaPortal.Common.Settings;
+
+namespace MediaPortal.UiComponents.BackgroundManager.Settings
+{
+  class BackgroundManagerSettings
+  {
+    /// <summary>
+    /// Gets or sets an indicator, if video background should be enabled. It requires a <see cref="VideoBackgroundFileName"/> to be defined as well.
+    /// </summary>
+    [Setting(SettingScope.User, false)]
+    public bool EnableVideoBackground { get; set; }
+
+    /// <summary>
+    /// Local filename of a valid video that will be used for background.
+    /// </summary>
+    [Setting(SettingScope.User, null)]
+    public string VideoBackgroundFileName { get; set; }
+  }
+}
