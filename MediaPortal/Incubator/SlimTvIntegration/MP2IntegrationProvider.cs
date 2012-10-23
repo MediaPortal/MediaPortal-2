b611fac9f8bd3474cc5f6455ffc1b332c8c6ac5d
morpheusxx <morpheus_xx@team-mediaportal.com>
2012-10-23 18:07:44 +0200

    Extended IntegrationProvider to work also outside MP2 scope (SetupTV)

--- MediaPortal/Incubator/SlimTvIntegration/MP2IntegrationProvider.cs
+++ MediaPortal/Incubator/SlimTvIntegration/MP2IntegrationProvider.cs
@@ -22,23 +22,68 @@
 
 #endregion
 
+using System;
+using MediaPortal.Common;
+using MediaPortal.Common.Logging;
+using MediaPortal.Common.Services.Logging;
 using Mediaportal.TV.Server.TVLibrary.Interfaces.Integration;
+using ILogger = Mediaportal.TV.Server.TVLibrary.Interfaces.Integration.ILogger;
 
 namespace MediaPortal.Plugins.SlimTv.Integration
 {
-  public class MP2IntegrationProvider: IIntegrationProvider
+  /// <summary>
+  /// Integration provider to run TVE35 components in MP2 context. This provider will be used for TvLibrary.Services and the SetupTV GUI.
+  /// For the SetupTV GUI (which runs outside of MP2 scope) it provides default PathManager and Logger.
+  /// </summary>
+  public class MP2IntegrationProvider : IIntegrationProvider
   {
-    readonly PathManagerWrapper _pathManagerWrapper = new PathManagerWrapper();
-    readonly LoggerWrapper _loggerWrapper = new LoggerWrapper();
+    private PathManagerWrapper _pathManagerWrapper;
+    private LoggerWrapper _loggerWrapper;
+
+    protected void InitDefaults()
+    {
+      // Check if the required service is already present, then no action is required
+      bool pathManagerAvailable = ServiceRegistration.Get<Common.PathManager.IPathManager>(false) != null;
+      if (pathManagerAvailable && _pathManagerWrapper != null && _loggerWrapper != null)
+        return;
+
+      if (_pathManagerWrapper != null && _loggerWrapper != null) 
+        return;
+
+      // If running outside of MP2 scope
+      if (!pathManagerAvailable)
+      {
+        ServiceRegistration.Set<Common.Logging.ILogger>(new NoLogger());
+
+        Common.Services.PathManager.PathManager pathManager = new Common.Services.PathManager.PathManager();
+        pathManager.InitializeDefaults();
+
+        ServiceRegistration.Set<Common.PathManager.IPathManager>(pathManager);
+
+        Common.Logging.ILogger logger = new Log4NetLogger(pathManager.GetPath(@"<LOG>"));
+        logger.Info("MP2IntegrationProvider: Launching in AppDomain {0}...", AppDomain.CurrentDomain.FriendlyName);
+      }
+
+      _pathManagerWrapper = new PathManagerWrapper();
+      _loggerWrapper = new LoggerWrapper();
+    }
 
     public IPathManager PathManager
     {
-      get { return _pathManagerWrapper; }
+      get
+      {
+        InitDefaults();
+        return _pathManagerWrapper;
+      }
     }
 
     public ILogger Logger
     {
-      get { return _loggerWrapper; }
+      get
+      {
+        InitDefaults();
+        return _loggerWrapper;
+      }
     }
   }
 }
