ec13deb9ce79ca1b4b929bffcd3a8ad5f51a3786
morpheusxx <morpheus_xx@team-mediaportal.com>
2012-10-25 19:44:56 +0200

    Added NativeMethods, COMHelper and FilterLoader to load DirectShow filters by filename only

---
+++ MediaPortal/Source/Core/MediaPortal.Common/Utils/NativeMethods.cs
@@ -0,0 +1,76 @@
+﻿#region Copyright (C) 2007-2012 Team MediaPortal
+
+/*
+    Copyright (C) 2007-2012 Team MediaPortal
+    http://www.team-mediaportal.com
+
+    This file is part of MediaPortal 2
+
+    MediaPortal 2 is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    MediaPortal 2 is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with MediaPortal 2. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#endregion
+using System;
+using System.Runtime.InteropServices;
+
+namespace MediaPortal.Common.Utils
+{
+  /// <summary>
+  /// This class is a static class containing definitions for system methods that are used in various places in MediaPortal.
+  /// </summary>
+  public static class NativeMethods
+  {
+    /// <summary>
+    /// Retrieves a module handle for the specified module. The module must have been loaded by the calling process.
+    /// </summary>
+    /// <param name="lpModuleName">The name of the loaded module (either a .dll or .exe file).</param>
+    /// <returns>If the function succeeds, the return value is a handle to the module.<br></br><br>If the function fails, the return value is NULL. To get extended error information, call Marshal.GetLastWin32Error.</br></returns>
+    [DllImport("kernel32.dll", EntryPoint = "GetModuleHandle", CharSet = CharSet.Ansi)]
+    public static extern IntPtr GetModuleHandle(string lpModuleName);
+
+    /// <summary>
+    /// The GetProcAddress function retrieves the address of an exported function or variable from the specified dynamic-link library (DLL).
+    /// </summary>
+    /// <param name="hModule">Handle to the DLL module that contains the function or variable. The LoadLibrary or GetModuleHandle function returns this handle.</param>
+    /// <param name="lpProcName">Pointer to a null-terminated string containing the function or variable name, or the function's ordinal value. If this parameter is an ordinal value, it must be in the low-order word; the high-order word must be zero.</param>
+    /// <returns>If the function succeeds, the return value is the address of the exported function or variable.<br></br><br>If the function fails, the return value is NULL. To get extended error information, call Marshal.GetLastWin32Error.</br></returns>
+    [DllImport("kernel32.dll", EntryPoint = "GetProcAddress", CharSet = CharSet.Ansi)]
+    public static extern IntPtr GetProcAddress(IntPtr hModule, string lpProcName);
+
+    /// <summary>
+    /// Determines whether the specified process is running under WOW64.
+    /// </summary>
+    /// <param name="hProcess">A handle to the process. The handle must have the PROCESS_QUERY_INFORMATION or PROCESS_QUERY_LIMITED_INFORMATION access right.</param>
+    /// <param name="isWow64Process">A pointer to a value that is set to TRUE if the process is running under WOW64. If the process is running under 32-bit Windows, the value is set to FALSE. If the process is a 64-bit application running under 64-bit Windows, the value is also set to FALSE.</param>
+    /// <returns>If the function succeeds, the return value is nonzero.<br></br><br>If the function fails, the return value is zero. To get extended error information, call Marshal.GetLastWin32Error.</br></returns>
+    [DllImport("kernel32.dll", EntryPoint = "IsWow64Process")]
+    public static extern bool IsWow64Process(IntPtr hProcess, out bool isWow64Process);
+
+    /// <summary>
+    /// The LoadLibrary function maps the specified executable module into the address space of the calling process.
+    /// </summary>
+    /// <param name="lpLibFileName">Pointer to a null-terminated string that names the executable module (either a .dll or .exe file). The name specified is the file name of the module and is not related to the name stored in the library module itself, as specified by the LIBRARY keyword in the module-definition (.def) file.</param>
+    /// <returns>If the function succeeds, the return value is a handle to the module.<br></br><br>If the function fails, the return value is NULL. To get extended error information, call Marshal.GetLastWin32Error.</br></returns>
+    [DllImport("kernel32.dll", EntryPoint = "LoadLibraryA", CharSet = CharSet.Ansi)]
+    public static extern IntPtr LoadLibrary(string lpLibFileName);
+
+    /// <summary>
+    /// The FreeLibrary function decrements the reference count of the loaded dynamic-link library (DLL). When the reference count reaches zero, the module is unmapped from the address space of the calling process and the handle is no longer valid.
+    /// </summary>
+    /// <param name="hLibModule">Handle to the loaded DLL module. The LoadLibrary or GetModuleHandle function returns this handle.</param>
+    /// <returns>If the function succeeds, the return value is nonzero.<br></br><br>If the function fails, the return value is zero. To get extended error information, call Marshal.GetLastWin32Error.</br></returns>
+    [DllImport("kernel32.dll", EntryPoint = "FreeLibrary", CharSet = CharSet.Ansi)]
+    public static extern int FreeLibrary(IntPtr hLibModule);
+  }
+}
