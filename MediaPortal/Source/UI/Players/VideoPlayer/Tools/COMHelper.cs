45154cc0453da9ca8c1949c690eb91b45271b41c
morpheusxx <morpheus_xx@team-mediaportal.com>
2012-10-25 19:44:56 +0200

    Added NativeMethods, COMHelper and FilterLoader to load DirectShow filters by filename only

---
+++ MediaPortal/Source/UI/Players/VideoPlayer/Tools/COMHelper.cs
@@ -0,0 +1,124 @@
+﻿#region Copyright (C) 2007-2012 Team MediaPortal
+
+/*
+    Copyright (C) 2007-2012 Team MediaPortal
+    http://www.team-mediaportal.com
+
+    This file is part of MediaPortal 2
+
+    MediaPortal 2 is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    MediaPortal 2 is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with MediaPortal 2. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#endregion
+
+//
+// This class is based on the source of http://www.codeproject.com/Articles/13391/Using-IFilter-in-C. 
+// Many thanks to the original author!
+//
+
+using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+namespace MediaPortal.UI.Players.Video.Tools
+{
+  [ComVisible(false)]
+  [ComImport, InterfaceType(ComInterfaceType.InterfaceIsIUnknown), Guid("00000001-0000-0000-C000-000000000046")]
+  internal interface IClassFactory
+  {
+    void CreateInstance([MarshalAs(UnmanagedType.Interface)] object pUnkOuter, ref Guid refiid, [MarshalAs(UnmanagedType.Interface)] out object ppunk);
+    void LockServer(bool fLock);
+  }
+
+  /// <summary>
+  /// Utility class to get a Class Factory for a certain Class ID 
+  /// by loading the dll that implements that class
+  /// </summary>
+  internal static class ComHelper
+  {
+    // DllGetClassObject fuction pointer signature
+    private delegate int DllGetClassObject(ref Guid classId, ref Guid interfaceId, [Out, MarshalAs(UnmanagedType.Interface)] out object ppunk);
+
+    /// <summary>
+    /// Holds a list of dll handles and unloads the dlls 
+    /// in the destructor
+    /// </summary>
+    private class DllList
+    {
+      private readonly List<IntPtr> _handleList = new List<IntPtr>();
+      public void AddDllHandle(IntPtr dllHandle)
+      {
+        lock (_handleList)
+        {
+          _handleList.Add(dllHandle);
+        }
+      }
+
+      ~DllList()
+      {
+        foreach (IntPtr dllHandle in _handleList)
+        {
+          try
+          {
+            Common.Utils.NativeMethods.FreeLibrary(dllHandle);
+          }
+          catch { }
+        }
+      }
+    }
+
+    static readonly DllList DLL_LIST = new DllList();
+
+    /// <summary>
+    /// Gets a class factory for a specific COM Class ID. 
+    /// </summary>
+    /// <param name="dllName">The dll where the COM class is implemented</param>
+    /// <param name="filterPersistClass">The requested Class ID</param>
+    /// <returns>IClassFactory instance used to create instances of that class</returns>
+    internal static IClassFactory GetClassFactory(string dllName, Guid filterPersistClass)
+    {
+      // Load the class factory from the dll.
+      IClassFactory classFactory = GetClassFactoryFromDll(dllName, filterPersistClass);
+      return classFactory;
+    }
+
+    private static IClassFactory GetClassFactoryFromDll(string dllName, Guid filterPersistClass)
+    {
+      // Load the dll.
+      IntPtr dllHandle = Common.Utils.NativeMethods.LoadLibrary(dllName);
+      if (dllHandle == IntPtr.Zero)
+        return null;
+
+      // Keep a reference to the dll until the process\AppDomain dies.
+      DLL_LIST.AddDllHandle(dllHandle);
+
+      //Get a pointer to the DllGetClassObject function
+      IntPtr dllGetClassObjectPtr = Common.Utils.NativeMethods.GetProcAddress(dllHandle, "DllGetClassObject");
+      if (dllGetClassObjectPtr == IntPtr.Zero)
+        return null;
+
+      // Convert the function pointer to a .net delegate.
+      DllGetClassObject dllGetClassObject = (DllGetClassObject) Marshal.GetDelegateForFunctionPointer(dllGetClassObjectPtr, typeof(DllGetClassObject));
+
+      // Call the DllGetClassObject to retreive a class factory for out Filter class.
+      Guid baseFilterGuid = filterPersistClass;
+      Guid classFactoryGuid = typeof(IClassFactory).GUID;
+      Object unk;
+      if (dllGetClassObject(ref baseFilterGuid, ref classFactoryGuid, out unk) != 0)
+        return null;
+
+      return (unk as IClassFactory);
+    }
+  }
+}
---
+++ MediaPortal/Source/UI/Players/VideoPlayer/Tools/FilterLoader.cs
@@ -0,0 +1,71 @@
+﻿#region Copyright (C) 2007-2012 Team MediaPortal
+
+/*
+    Copyright (C) 2007-2012 Team MediaPortal
+    http://www.team-mediaportal.com
+
+    This file is part of MediaPortal 2
+
+    MediaPortal 2 is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    MediaPortal 2 is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with MediaPortal 2. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#endregion
+
+using System;
+using System.IO;
+using System.Reflection;
+using DirectShowLib;
+
+namespace MediaPortal.UI.Players.Video.Tools
+{
+  /// <summary>
+  /// <see cref="FilterLoader"/> is a helper class to load <see cref="IBaseFilter"/>s from any .dll, for which is no registration of the filter needed.
+  /// </summary>
+  public static class FilterLoader
+  {
+    /// <summary>
+    /// Loads an COM .dll and creates an instance of the given Interface with IID <paramref name="interfaceId"/>.
+    /// </summary>
+    /// <param name="dllName">Filename of a .dll or .ax component</param>
+    /// <param name="interfaceId">Interface to create an object instance for</param>
+    /// <param name="useAssemblyRelativeLocation">Combine the given file name to a full path</param>
+    /// <returns>Instance or <c>null</c></returns>
+    public static IBaseFilter LoadFilterFromDll(string dllName, Guid interfaceId, bool useAssemblyRelativeLocation = false)
+    {
+      // Get a ClassFactory for our classID
+      string dllPath = useAssemblyRelativeLocation ? BuildAssemblyRelativePath(dllName) : dllName;
+      IClassFactory classFactory = ComHelper.GetClassFactory(dllPath, interfaceId);
+      if (classFactory == null)
+        return null;
+
+      // And create an IFilter instance using that class factory
+      Guid baseFilterGuid = typeof(IBaseFilter).GUID;
+      object obj;
+      classFactory.CreateInstance(null, ref baseFilterGuid, out obj);
+      return (obj as IBaseFilter);
+    }
+
+    /// <summary>
+    /// Builds a full path for a given <paramref name="fileName"/> that is located in the same folder as the <see cref="Assembly.GetCallingAssembly"/>.
+    /// </summary>
+    /// <param name="fileName">File name</param>
+    /// <returns>Combined path</returns>
+    public static string BuildAssemblyRelativePath(string fileName)
+    {
+      string executingPath = Assembly.GetCallingAssembly().Location;
+      return Path.Combine(Path.GetDirectoryName(executingPath), fileName);
+    }
+
+  }
+}
