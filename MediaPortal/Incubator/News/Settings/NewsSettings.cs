37d37fa916e09726143607ee6700e0af1142fe29
offbyoneBB <bborgsdorf@gmail.com>
2012-10-26 18:51:01 +0200

    added option to have a default list of feeds per language managing feeds in configuration now shows the default list of feeds refreshing of feeds is done instantly after configuration is saved refresh interval can now be configured by the user refactored the feed downloading to a separate class

--- MediaPortal/Incubator/News/Settings/NewsSettings.cs
+++ MediaPortal/Incubator/News/Settings/NewsSettings.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using MediaPortal.Common.Settings;
+using MediaPortal.Common.Configuration.ConfigurationClasses;
+using MediaPortal.Common;
+using MediaPortal.Common.Localization;
 
 namespace MediaPortal.UiComponents.News.Settings
 {
@@ -16,12 +19,37 @@ namespace MediaPortal.UiComponents.News.Settings
     [Setting(SettingScope.User, HasDefault=false)]
     public List<FeedBookmark> FeedsList { get; set; }
 
-    public readonly string[] SampleFeeds = new string[] 
+    [Setting(SettingScope.User, 15)]
+    public int RefreshInterval { get; set; }
+
+    static readonly Dictionary<string, FeedBookmark[]> SampleFeeds = new Dictionary<string, FeedBookmark[]>()
     { 
-      "http://www.team-mediaportal.com/rss-feeds",
-      "http://www.spiegel.de/schlagzeilen/tops/index.rss", 
-      "http://www.heise.de/newsticker/heise-atom.xml", 
-      "http://feeds.betanews.com/bn" 
+      { "en", new FeedBookmark[] 
+        { 
+          new FeedBookmark() { Name = "MediaPortal", Url = "http://www.team-mediaportal.com/rss-feeds"},
+          new FeedBookmark() { Name = "Cnet", Url = "http://feeds.feedburner.com/cnet/tcoc"},
+          new FeedBookmark() { Name = "BetaNews", Url = "http://feeds.betanews.com/bn"}
+        }
+      },
+      { "de", new FeedBookmark[] 
+        { 
+          new FeedBookmark() { Name = "MediaPortal", Url = "http://www.team-mediaportal.com/rss-feeds"},
+          new FeedBookmark() { Name = "Spiegel", Url = "http://www.spiegel.de/schlagzeilen/tops/index.rss"}, 
+          new FeedBookmark() { Name = "Heise", Url = "http://www.heise.de/newsticker/heise-atom.xml"}
+        }
+      }
     };
+
+    public static FeedBookmark[] GetDefaultRegionalFeeds()
+    {
+      FeedBookmark[] result = null;
+      var culture = ServiceRegistration.Get<ILocalization>().CurrentCulture;
+      string langCode = culture.Name;
+      int regionPartIndex = langCode.IndexOf("-");
+      if (regionPartIndex > 0) langCode = langCode.Substring(0, regionPartIndex);
+      if (!SampleFeeds.TryGetValue(langCode, out result))
+        result = SampleFeeds["en"];
+      return result;
+    }
   }
 }
