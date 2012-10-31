13e50bf1736b0688b873bf62884b598b0d19ed3c
morpheusxx <morpheus_xx@team-mediaportal.com>
2012-10-31 12:42:14 +0100

    Added new ffmpeg based movie thumbnail builder

---
+++ MediaPortal/Source/Extensions/MetadataExtractors/MovieThumbnailer/Properties/AssemblyInfo.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// Allgemeine Informationen über eine Assembly werden über die folgenden 
+// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+// die mit einer Assembly verknüpft sind.
+[assembly: AssemblyTitle("MovieThumbnailer")]
+[assembly: AssemblyDescription("MovieThumbnailer metada extractor")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Durch Festlegen von ComVisible auf "false" werden die Typen in dieser Assembly unsichtbar 
+// für COM-Komponenten. Wenn Sie auf einen Typ in dieser Assembly von 
+// COM zugreifen müssen, legen Sie das ComVisible-Attribut für diesen Typ auf "true" fest.
+[assembly: ComVisible(false)]
+
+// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+[assembly: Guid("57cb17cd-c15b-4c13-af89-ae9638e4a95a")]
+
+// Other attributes are included from VersionInfo.cs!
