86cc45c367ab7779dc02ecc0ebc87eba57eb1dea
chefkoch <chefkoch@team-mediaportal.com>
2012-12-13 03:13:33 +0100

    added SystemStateMenu plugin

---
+++ MediaPortal/Incubator/SystemStateMenu/Helper Classes/SystemStateItem.cs
@@ -0,0 +1,48 @@
+#region Copyright (C) 2007-2013 Team MediaPortal
+
+/*
+    Copyright (C) 2007-2013 Team MediaPortal
+    http://www.team-mediaportal.com
+
+    This file is part of MediaPortal 2
+
+    MediaPortal 2 is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    MediaPortal 2 is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with MediaPortal 2. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#endregion
+
+namespace MediaPortal.Plugins.SystemStateMenu
+{
+  /// <summary>
+  /// This is a basic class where the information for the provider are stored (usually the location name and unique id).
+  /// </summary>
+  public class SystemStateItem
+  {
+    internal SystemStateItem(SystemStateAction action, bool enabled)
+    {
+      Action = action;
+      Enabled = enabled;
+    }
+
+    public SystemStateItem() { }
+
+    public SystemStateAction Action { get; set; }
+    public bool Enabled { get; set; }
+
+    public override string ToString()
+    {
+      return Action.ToString();
+    }
+  }
+}
