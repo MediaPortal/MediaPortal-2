b913f172e788f187d53b36e50bf57a355cb78f5c
mcgoober <jason@intensepurple.co.uk>
2013-01-18 22:13:22 +0000

    New code for figuring out the DLNA profile by asking FFMPEG for the file details, and based on the guidelines in the DLNA profile document converting it in to the correct profile. This is then stored in a DLNA aspect which can then be recalled later when rendering container information and forming the DLNAForthField.

--- MediaPortal/Incubator/MediaServer/MetadataExtractors/DlnaMetadataExtractor.cs
+++ MediaPortal/Incubator/MediaServer/MetadataExtractors/DlnaMetadataExtractor.cs
@@ -24,12 +24,18 @@
 
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Text.RegularExpressions;
 using MediaPortal.Common;
 using MediaPortal.Common.Logging;
 using MediaPortal.Common.MediaManagement;
 using MediaPortal.Common.MediaManagement.DefaultItemAspects;
 using MediaPortal.Common.ResourceAccess;
+using MediaPortal.Common.Services.ResourceAccess.StreamedResourceToLocalFsAccessBridge;
 using MediaPortal.Extensions.MediaServer.Aspects;
+using MediaPortal.Utilities.FileSystem;
+using MediaPortal.Utilities.Process;
 
 namespace MediaPortal.Extensions.MediaServer.MetadataExtractors
 {
@@ -39,6 +45,11 @@ namespace MediaPortal.Extensions.MediaServer.MetadataExtractors
     /// Image metadata extractor GUID.
     /// </summary>
     public static Guid MetadataExtractorId = new Guid(MediaServerPlugin.DEVICE_UUID);
+
+    /// <summary>
+    /// Maximum duration for creating a single movie thumbnail.
+    /// </summary>
+    protected const int PROCESS_TIMEOUT_MS = 2500;
 
     protected static List<MediaCategory> MediaCategories = new List<MediaCategory>
       { DefaultMediaCategories.Audio, DefaultMediaCategories.Image, DefaultMediaCategories.Video };
@@ -75,40 +86,134 @@ namespace MediaPortal.Extensions.MediaServer.MetadataExtractors
     {
       try
       {
-        var fsra = mediaItemAccessor as IFileSystemResourceAccessor;
-        if (fsra == null || !fsra.IsFile)
-        {
+        if (!(mediaItemAccessor is IFileSystemResourceAccessor))
           return false;
-        }
-
-        MediaItemAspect.SetAttribute(extractedAspectData, DlnaItemAspect.ATTR_SIZE, fsra.Size);
-
-        var fileName = mediaItemAccessor.ResourceName;
-        var ext = DosPathHelper.GetExtension(fileName).ToLowerInvariant();
-        switch (ext)
+        using (var fsra = (IFileSystemResourceAccessor)mediaItemAccessor.Clone())
         {
-          case "mp3":
-            MediaItemAspect.SetAttribute(extractedAspectData, DlnaItemAspect.ATTR_MIME_TYPE, "audio/mpeg");
-            MediaItemAspect.SetAttribute(extractedAspectData, DlnaItemAspect.ATTR_PROFILE, "MP3");
-            break;
-          case "jpg":
-            MediaItemAspect.SetAttribute(extractedAspectData, DlnaItemAspect.ATTR_MIME_TYPE, "image/jpeg");
-            MediaItemAspect.SetAttribute(extractedAspectData, DlnaItemAspect.ATTR_PROFILE, "JPEG_LRG");
-            break;
-          default:
+          if (!fsra.IsFile)
             return false;
-        }  
-        
+          using (var lfsra = StreamedResourceToLocalFsAccessBridge.GetLocalFsResourceAccessor(fsra))
+          {
+            var info = ExtractFFMpegInfo(lfsra);
+            ConvertFFMPEGInfoToAspectData(extractedAspectData, info);
+          }
+        }       
         return true;
       }
       catch (Exception e)
       {
         // Only log at the info level here - And simply return false. This lets the caller know that we
         // couldn't perform our task here.
-        ServiceRegistration.Get<ILogger>().Info("DlnaMetadataExtractor: Exception reading resource '{0}' (Text: '{1}')", mediaItemAccessor.CanonicalLocalResourcePath, e.Message);
+        ServiceRegistration.Get<ILogger>().Info("DlnaMediaServer: Exception reading resource '{0}' (Text: '{1}')", mediaItemAccessor.CanonicalLocalResourcePath, e.Message);
       }
 
       return false;
+    }
+
+    private FFMPEGInfo ExtractFFMpegInfo(ILocalFsResourceAccessor lfsra)
+    {
+      var executable = FileUtils.BuildAssemblyRelativePath("ffmpeg.exe");
+      var arguments = string.Format("-i \"{0}\"",
+        lfsra.LocalFileSystemPath);
+
+      string result;
+      if (TryExecuteReadString(executable, arguments, out result, ProcessPriorityClass.BelowNormal, PROCESS_TIMEOUT_MS))
+      {        
+        ServiceRegistration.Get<ILogger>().Info("DlnaMediaServer: Successfully ran ffmpeg:\n {0}", result);
+        return ParseFFMpegOutput(result);
+      }
+      else
+        ServiceRegistration.Get<ILogger>().Warn("DlnaMediaServer: Failed to extract media type information for resource '{0}'", lfsra.LocalFileSystemPath);
+      return null;
+    }
+
+    public static bool TryExecuteReadString(string executable, string arguments, out string result, ProcessPriorityClass priorityClass = ProcessPriorityClass.Normal, int maxWaitMs = 1000)
+    {
+      using (Process process = new Process { StartInfo = new ProcessStartInfo(executable, arguments) { UseShellExecute = false, CreateNoWindow = true, RedirectStandardError = true } })
+      {
+        process.Start();
+        //process.PriorityClass = priorityClass;
+        using (process.StandardError)
+        {
+          result = process.StandardError.ReadToEnd();
+          if (process.WaitForExit(maxWaitMs))
+            return process.ExitCode == 1;
+        }
+        if (!process.HasExited)
+          process.Close();
+      }
+      return false;
+    }
+
+    private FFMPEGInfo ParseFFMpegOutput(string ffmpeg)
+    {
+      var input = ffmpeg.Split('\n');
+      if (!input[0].StartsWith("ffmpeg version"))
+        return null;
+      for(var i = 0; i < input.Length; i++)
+      {
+        if (input[i].StartsWith("Input"))
+        {
+          return ParseFFMpegInputBlock(input, i);
+        }
+      }
+      return null;            
+    }
+
+    private FFMPEGInfo ParseFFMpegInputBlock(string[] input, int i)
+    {
+      var result = new FFMPEGInfo();     
+      var match = Regex.Match(input[i++], @"Input #(\d), (\w*), from");
+      if (!match.Success)
+        return null;
+
+      result.ContainerType = match.Groups[2].Value;
+
+      while (i < input.Length)
+      {
+        match = Regex.Match(input[i++], @"  Duration: ([\d\.:]+), start: [\d\.]+, bitrate: (\d+) kb/s");
+        if (match.Success)
+        {
+          result.ContainerBitRate = match.Groups[2].Value;
+          return result;
+        }
+      }
+      return null;
+    }
+
+    private static bool ConvertFFMPEGInfoToAspectData(IDictionary<Guid, MediaItemAspect> extractedAspectData, FFMPEGInfo info)
+    {
+      switch (info.ContainerType)
+      {
+        case "mp3":
+          MediaItemAspect.SetAttribute(extractedAspectData, DlnaItemAspect.ATTR_MIME_TYPE, "audio/mpeg");
+          MediaItemAspect.SetAttribute(extractedAspectData, DlnaItemAspect.ATTR_PROFILE, "MP3");
+          break;
+        case "jpg":
+          MediaItemAspect.SetAttribute(extractedAspectData, DlnaItemAspect.ATTR_MIME_TYPE, "image/jpeg");
+          MediaItemAspect.SetAttribute(extractedAspectData, DlnaItemAspect.ATTR_PROFILE, "JPEG_LRG");
+          break;
+        default:
+          return false;
+      }
+      return true;
+    }
+
+    private class FFMPEGInfo
+    {
+      public string ContainerType;
+      public string ContainerBitRate;
+
+      public string ImageType;
+      public string ImageResolutionX;
+      public string ImageResolutionY;
+      public string ImageBitRate;
+
+      public string AudioType;
+      public string AudioFrequency;
+      public string AudioChannels;
+      public string AudioBits;
+      public string AudioBitRate;
     }
 
     #endregion
