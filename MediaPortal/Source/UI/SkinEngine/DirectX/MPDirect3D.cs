eb86028e0b91c1cdafd652323babb3fc72761905
morpheusxx <morpheus_xx@team-mediaportal.com>
2013-02-10 15:36:49 +0100

    Changed copyright headers to 2013 (mantis-4177)

--- MediaPortal/Source/UI/SkinEngine/DirectX/MPDirect3D.cs
+++ MediaPortal/Source/UI/SkinEngine/DirectX/MPDirect3D.cs
@@ -1,7 +1,7 @@
-﻿#region #region Copyright (C) 2007-2012 Team MediaPortal
+﻿#region #region Copyright (C) 2007-2013 Team MediaPortal
 
 /*
-    #region Copyright (C) 2007-2012 Team MediaPortal
+    #region Copyright (C) 2007-2013 Team MediaPortal
     http://www.team-mediaportal.com
 
     This file is part of MediaPortal 2
