6ac2211a9768f85cd07644e97c420b4fbce6d82d
chefkoch <chefkoch@team-mediaportal.com>
2013-02-18 22:38:32 +0100

    linked to common VersionInfo & project file cleanup

--- MediaPortal/Source/Main/MediaPortal.ServiceMonitor/Properties/AssemblyInfo.cs
+++ MediaPortal/Source/Main/MediaPortal.ServiceMonitor/Properties/AssemblyInfo.cs
@@ -1,18 +1,13 @@
 ﻿using System.Reflection;
-using System.Resources;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Windows;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MediaPortal.ServiceMonitor")]
+[assembly: AssemblyTitle("MediaPortal 2 ServiceMonitor")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MediaPortal.ServiceMonitor")]
-[assembly: AssemblyCopyright("Copyright ©  2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -30,7 +25,6 @@ using System.Windows;
 
 //[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
 
-
 [assembly: ThemeInfo(
     ResourceDictionaryLocation.None, //where theme specific resource dictionaries are located
   //(used if a resource is not found in the page, 
@@ -40,16 +34,4 @@ using System.Windows;
   // app, or any theme specific resource dictionaries)
 )]
 
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+// Other attributes are included from VersionInfo.cs!
