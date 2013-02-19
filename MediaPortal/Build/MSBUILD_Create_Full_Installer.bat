16b791e4956b943b95bce81c3bc7b915996e8be9
chefkoch <chefkoch@team-mediaportal.com>
2013-02-19 12:33:40 +0100

    added new BuildReport submodule adapted Build scripts removed obsolete BuildReport scripts

--- MediaPortal/Build/MSBUILD_Create_Full_Installer.bat
+++ MediaPortal/Build/MSBUILD_Create_Full_Installer.bat
@@ -6,13 +6,13 @@ echo Updating language resources from transifex >> BuildInstaller.log
 call TRANSIFEX_Update_Translation_Files.bat
 
 echo Rebuilding Server >> BuildInstaller.log
-call MSBUILD_Build_Report_Release_Server.bat
+call MSBUILD_Rebuild_Release_Server.bat
 
 echo Rebuilding Client >> BuildInstaller.log
-call MSBUILD_Build_Report_Release_Client.bat
+call MSBUILD_Rebuild_Release_Client.bat
 
 echo Rebuilding ServiceMonitor >> BuildInstaller.log
-call MSBUILD_Build_Report_Release_ServiceMonitor.bat
+call MSBUILD_Rebuild_Release_ServiceMonitor.bat
 
 echo Rebuilding Setup >> BuildInstaller.log
 call MSBUILD_Rebuild_Release_Setup.bat
--- MediaPortal/Build/MSBUILD_Rebuild_Release_Server.bat
+++ MediaPortal/Build/MSBUILD_Rebuild_Release_Server.bat
@@ -1,1 +1,10 @@
-@"%WINDIR%\Microsoft.NET\Framework\v4.0.30319\MSBUILD.exe" ..\Source\MP2-Server.sln /target:Rebuild  /property:Configuration=Release;Platform=x86 > Build_Server.log
+
+xcopy /I /Y .\BuildReport\_BuildReport_Files .\_BuildReport_Files
+
+set xml=Build_Report_Release_Server.xml
+set html=Build_Report_Release_Server.html
+
+set logger=/l:XmlFileLogger,"BuildReport\MSBuild.ExtensionPack.Loggers.dll";logfile=%xml%
+"%WINDIR%\Microsoft.NET\Framework\v4.0.30319\MSBUILD.exe" ..\Source\MP2-Server.sln %logger% /target:Rebuild /property:Configuration=Release;Platform=x86
+
+BuildReport\msxsl %xml% _BuildReport_Files\BuildReport.xslt -o %html%
