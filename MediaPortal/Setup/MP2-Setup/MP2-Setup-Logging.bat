edf1f0864f1394a29ffe78529d0a9d2b363fa41b
chefkoch <chefkoch@team-mediaportal.com>
2013-04-10 19:49:38 +0200

    fixed issue #4329: Include batch files next to the MP2-Setup.msi to allow running the installation with logging enabled easily

---
+++ MediaPortal/Setup/MP2-Setup/MP2-Setup-Logging.bat
@@ -0,0 +1,8 @@
+
+REM =========================================================================
+REM This file starts the MP2-Setup.msi and logs all messages to MP2-Setup.log
+REM =========================================================================
+
+REM For verbose logging uncomment the next line and comment the second next.
+REM msiexec /i MP2-Setup.msi /l*v MP2-Setup.log
+msiexec /i MP2-Setup.msi /l MP2-Setup.log
