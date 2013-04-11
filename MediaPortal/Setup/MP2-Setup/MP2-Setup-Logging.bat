ed7e0d05655759ee2025e4be13c91a58cb5c2850
chefkoch <chefkoch@team-mediaportal.com>
2013-04-11 08:54:12 +0200

    improved text for cmd when using batch file

--- MediaPortal/Setup/MP2-Setup/MP2-Setup-Logging.bat
+++ MediaPortal/Setup/MP2-Setup/MP2-Setup-Logging.bat
@@ -1,8 +1,11 @@
-
+@echo off
 REM =========================================================================
 REM This file starts the MP2-Setup.msi and logs all messages to MP2-Setup.log
 REM =========================================================================
 
+echo.
+echo MP2-Setup.msi started.
+echo Logging to MP2-Setup.log...
 REM For verbose logging uncomment the next line and comment the second next.
 REM msiexec /i MP2-Setup.msi /l*v MP2-Setup.log
 msiexec /i MP2-Setup.msi /l MP2-Setup.log
