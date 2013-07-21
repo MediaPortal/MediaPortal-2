25a74db29a7464bdbda4b0845298b4dca47b7f99
chefkoch <chefkoch@team-mediaportal.com>
2013-07-21 17:40:49 +0200

    removed VS2010 build scripts (MP2-380)

--- MediaPortal/Build/VS2010_Rebuild_Release_ServiceMonitor.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-
-set ProgramDir=%ProgramFiles%
-if exist "%ProgramFiles(x86)%" set ProgramDir=%ProgramFiles(x86)%
-
-"%ProgramDir%\Microsoft Visual Studio 10.0\Common7\IDE\devenv.com" ..\Source\MP2-ServiceMonitor.sln /Rebuild "Release|x86" /Out build.log
