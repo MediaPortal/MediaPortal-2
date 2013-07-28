382ab9f3dd78484176ee7fded8d396677655b991
morpheusxx <morpheus_xx@team-mediaportal.com>
2013-07-28 16:40:15 +0200

    Added "cache" argument to force reloading on each change

--- MediaPortal/Incubator/FanArtService.Client/FanArtImageSourceConverter.cs
+++ MediaPortal/Incubator/FanArtService.Client/FanArtImageSourceConverter.cs
@@ -1,4 +1,28 @@
-﻿using System;
+﻿#region Copyright (C) 2007-2013 Team MediaPortal
+
+/*
+    Copyright (C) 2007-2013 Team MediaPortal
+    http://www.team-mediaportal.com
+
+    This file is part of MediaPortal 2
+
+    MediaPortal 2 is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    MediaPortal 2 is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with MediaPortal 2. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#endregion
+
+using System;
 using System.Globalization;
 using MediaPortal.Extensions.UserServices.FanArtService.Interfaces;
 using MediaPortal.UI.SkinEngine.MpfElements.Converters;
@@ -31,13 +55,18 @@ namespace MediaPortal.Extensions.UserServices.FanArtService.Client
       int.TryParse(args[1], out maxWidth);
       int.TryParse(args[2], out maxHeight);
 
+      bool useCache = true;
+      if (args.Length == 4 && !bool.TryParse(args[3], out useCache))
+        useCache = true;
+
       result = new FanArtImageSource
         {
           FanArtMediaType = imageSource.FanArtMediaType,
           FanArtName = imageSource.FanArtName,
           FanArtType = fanartType,
           MaxWidth = maxWidth,
-          MaxHeight = maxHeight
+          MaxHeight = maxHeight,
+          Cache = useCache
         };
 
       return true;
