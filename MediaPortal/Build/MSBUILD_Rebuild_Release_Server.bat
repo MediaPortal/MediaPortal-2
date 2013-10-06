6ebe479845f8d2dd1a6950dae3fc31898b8f8e1d
chefkoch <chefkoch@team-mediaportal.com>
2013-10-06 15:32:28 +0200

    replaced BuildReport submodule by a NuGet package (MP2-379)

--- MediaPortal/Build/MSBUILD_Rebuild_Release_Server.bat
+++ MediaPortal/Build/MSBUILD_Rebuild_Release_Server.bat
@@ -1,10 +1,13 @@
 
-xcopy /I /Y .\BuildReport\_BuildReport_Files .\_BuildReport_Files
+@"%WINDIR%\Microsoft.NET\Framework\v4.0.30319\MSBUILD.exe" RestorePackages.targets /target:RestoreBuildPackages
+
+set PathToBuildReport=.\..\Packages\BuildReport.1.0.0
+xcopy /I /Y %PathToBuildReport%\_BuildReport_Files .\_BuildReport_Files
 
 set xml=Build_Report_Release_Server.xml
 set html=Build_Report_Release_Server.html
 
-set logger=/l:XmlFileLogger,"BuildReport\MSBuild.ExtensionPack.Loggers.dll";logfile=%xml%
+set logger=/l:XmlFileLogger,"%PathToBuildReport%\MSBuild.ExtensionPack.Loggers.dll";logfile=%xml%
 "%WINDIR%\Microsoft.NET\Framework\v4.0.30319\MSBUILD.exe" Build.proj %logger% /property:OneStepOnly=true;BuildServer=true
 
-BuildReport\msxsl %xml% _BuildReport_Files\BuildReport.xslt -o %html%
+%PathToBuildReport%\msxsl %xml% _BuildReport_Files\BuildReport.xslt -o %html%
