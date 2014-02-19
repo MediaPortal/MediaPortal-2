b88e7d740342c9b5a87d7d9789b7ef6a082a3024
morpheusxx <morpheus_xx@team-mediaportal.com>
2014-02-19 20:07:17 +0100

    Changed assembly info copyright (MP2-421), setting build to 1402 (2014-02)

--- MediaPortal/Source/Core/MediaPortal.Common/VersionInfo/VersionInfo.cs
+++ MediaPortal/Source/Core/MediaPortal.Common/VersionInfo/VersionInfo.cs
@@ -5,6 +5,6 @@ using System.Reflection;
 // be deleted.
 [assembly: AssemblyCompany("Team MediaPortal")]
 [assembly: AssemblyProduct("MediaPortal 2")]
-[assembly: AssemblyCopyright("Copyright © Team MediaPortal 2007 - 2012")]
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyCopyright("Copyright © Team MediaPortal 2007 - 2014")]
+[assembly: AssemblyVersion("2.0.0.1402")]
+[assembly: AssemblyFileVersion("2.0.0.1402")]
