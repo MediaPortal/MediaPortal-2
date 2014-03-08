2c230efe57b0189d6e21168d6f3adaee4c873e07
morpheusxx <morpheus_xx@team-mediaportal.com>
2014-03-08 11:41:31 +0100

    Added interval based updating tasks and percent based "watched" check

--- MediaPortal/Incubator/Trakt/Service/TraktHandler.cs
+++ MediaPortal/Incubator/Trakt/Service/TraktHandler.cs
@@ -2,21 +2,40 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using System.Threading;
+using MediaPortal.Common;
+using MediaPortal.Common.Logging;
 using MediaPortal.Common.MediaManagement;
 using MediaPortal.Common.MediaManagement.DefaultItemAspects;
 using MediaPortal.Common.Messaging;
 using MediaPortal.Common.Services.Settings;
+using MediaPortal.Common.Threading;
 using MediaPortal.Extensions.OnlineLibraries.Libraries.Trakt;
 using MediaPortal.Extensions.OnlineLibraries.Libraries.Trakt.DataStructures;
 using MediaPortal.UI.Presentation.Players;
+using MediaPortal.UI.Presentation.Players.ResumeState;
 using MediaPortal.UI.Services.Players;
 
 namespace MediaPortal.UiComponents.Trakt.Service
 {
   public class TraktHandler : IDisposable
   {
+    // Defines the minimum playback progress in percent to consider a video as fully watched.
+    private const int WATCHED_PERCENT = 85;
+    private static readonly TimeSpan UPDATE_INTERVAL = TimeSpan.FromMinutes(10);
+
+    private class PositionWatcher
+    {
+      public IIntervalWork Work { get; set; }
+      public TimeSpan Duration { get; set; }
+      public TimeSpan ResumePosition { get; set; }
+    }
+
     private AsynchronousMessageQueue _messageQueue;
+    private readonly object _syncObj = new object();
     private readonly SettingsChangeWatcher<Settings.TraktSettings> _settings = new SettingsChangeWatcher<Settings.TraktSettings>();
+    private readonly Dictionary<IPlayerSlotController, PositionWatcher> _progressUpdateWorks = new Dictionary<IPlayerSlotController, PositionWatcher>();
+
     public TraktHandler()
     {
       _settings.SettingsChanged += ConfigureHandler;
@@ -67,61 +86,116 @@ namespace MediaPortal.UiComponents.Trakt.Service
         // React to player changes
         PlayerManagerMessaging.MessageType messageType = (PlayerManagerMessaging.MessageType)message.MessageType;
         IPlayerSlotController psc;
+        // ServiceRegistration.Get<ILogger>().Debug("Trakt.tv: PlayerManagerMessage: {0}", message.MessageType);
         switch (messageType)
         {
+          case PlayerManagerMessaging.MessageType.PlayerResumeState:
+            psc = (IPlayerSlotController)message.MessageData[PlayerManagerMessaging.PLAYER_SLOT_CONTROLLER];
+            IResumeState resumeState = (IResumeState)message.MessageData[PlayerManagerMessaging.KEY_RESUME_STATE];
+            Guid mediaItemId = (Guid)message.MessageData[PlayerManagerMessaging.KEY_MEDIAITEM_ID];
+            HandleResumeInfo(psc, mediaItemId, resumeState);
+            break;
           case PlayerManagerMessaging.MessageType.PlayerError:
           case PlayerManagerMessaging.MessageType.PlayerEnded:
           case PlayerManagerMessaging.MessageType.PlayerStopped:
             psc = (IPlayerSlotController)message.MessageData[PlayerManagerMessaging.PLAYER_SLOT_CONTROLLER];
-            HandlePlayerStopped(psc);
+            HandleScrobble(psc, false);
             break;
           case PlayerManagerMessaging.MessageType.PlayerStarted:
             psc = (IPlayerSlotController)message.MessageData[PlayerManagerMessaging.PLAYER_SLOT_CONTROLLER];
-            HandlePlayerStarted(psc);
+            HandleScrobble(psc, true);
             break;
         }
       }
     }
 
-    private void HandlePlayerStopped(IPlayerSlotController psc)
+    private void HandleResumeInfo(IPlayerSlotController psc, Guid mediaItemId, IResumeState resumeState)
     {
-      HandleScrobble(psc, false);
-    }
-
-    private void HandlePlayerStarted(IPlayerSlotController psc)
-    {
-      HandleScrobble(psc, true);
+      PositionResumeState pos = resumeState as PositionResumeState;
+      lock (_syncObj)
+        if (_progressUpdateWorks.ContainsKey(psc))
+          _progressUpdateWorks[psc].ResumePosition = pos != null ? pos.ResumePosition : _progressUpdateWorks[psc].Duration;
     }
 
     private void HandleScrobble(IPlayerSlotController psc, bool starting)
     {
-      IPlayerContext pc = PlayerContext.GetPlayerContext(psc);
-      if (pc == null || pc.CurrentMediaItem == null)
-        return;
-
-      AbstractScrobble scrobbleData;
-      TraktScrobbleStates state;
-      if (TryCreateScrobbleData(pc, starting, out scrobbleData, out state))
+      try
       {
-        TraktMovieScrobble movie = scrobbleData as TraktMovieScrobble;
-        if (movie != null)
-          TraktAPI.ScrobbleMovieState(movie, state);
+        IPlayerContext pc = PlayerContext.GetPlayerContext(psc);
+        if (pc == null)
+          return;
 
-        TraktEpisodeScrobble episode = scrobbleData as TraktEpisodeScrobble;
-        if (episode != null)
-          TraktAPI.ScrobbleEpisodeState(episode, state);
+        bool removePsc = HandleTasks(psc, starting);
+
+        // Asynchronous execution or aborting
+        AbstractScrobble scrobbleData;
+        TraktScrobbleStates state;
+        if (TryCreateScrobbleData(psc, pc, starting, out scrobbleData, out state))
+        {
+          ServiceRegistration.Get<ILogger>().Debug("Trakt.tv: [{5}] {0}, Duration {1}, Percent {2}, PSC.Duration {3}, PSC.ResumePosition {4}",
+            scrobbleData.Title, scrobbleData.Duration, scrobbleData.Progress, _progressUpdateWorks[psc].Duration, _progressUpdateWorks[psc].ResumePosition, state);
+
+          TraktMovieScrobble movie = scrobbleData as TraktMovieScrobble;
+          if (movie != null)
+            TraktAPI.ScrobbleMovieState(movie, state);
+
+          TraktEpisodeScrobble episode = scrobbleData as TraktEpisodeScrobble;
+          if (episode != null)
+            TraktAPI.ScrobbleEpisodeState(episode, state);
+        }
+
+        if (removePsc)
+          lock (_syncObj)
+            _progressUpdateWorks.Remove(psc);
+
       }
+      catch (ThreadAbortException)
+      { }
+      catch (Exception ex)
+      {
+        ServiceRegistration.Get<ILogger>().Error("Trakt.tv: Exception while scrobbling", ex);
+      }
+    }
+
+    /// <summary>
+    /// Creates or removes <see cref="IIntervalWork"/> from <see cref="IThreadPool"/>.
+    /// </summary>
+    /// <param name="psc">IPlayerSlotController</param>
+    /// <param name="starting"><c>true</c> if starting, <c>false</c> if stopping.</param>
+    /// <returns><c>true</c> if work should be removed when done.</returns>
+    private bool HandleTasks(IPlayerSlotController psc, bool starting)
+    {
+      IThreadPool threadPool = ServiceRegistration.Get<IThreadPool>();
+      lock (_syncObj)
+      {
+        // On stop, abort background interval work
+        if (!starting && _progressUpdateWorks.ContainsKey(psc))
+        {
+          threadPool.RemoveIntervalWork(_progressUpdateWorks[psc].Work);
+          return true;
+        }
+
+        // When starting, create an asynchronous work and exit here
+        if (!_progressUpdateWorks.ContainsKey(psc))
+        {
+          IntervalWork work = new IntervalWork(() => HandleScrobble(psc, true), UPDATE_INTERVAL);
+          threadPool.AddIntervalWork(work, false);
+          _progressUpdateWorks[psc] = new PositionWatcher { Work = work };
+        }
+      }
+      return false;
     }
 
     /// <summary>
     /// Creates Scrobble data based on a DBMovieInfo object
     /// </summary>
+    /// <param name="psc"></param>
     /// <param name="pc">PlayerContext</param>
     /// <param name="starting"></param>
     /// <param name="scrobbleData"></param>
     /// <param name="state"></param>
     /// <returns>The Trakt scrobble data to send</returns>
-    public bool TryCreateScrobbleData(IPlayerContext pc, bool starting, out AbstractScrobble scrobbleData, out TraktScrobbleStates state)
+    private bool TryCreateScrobbleData(IPlayerSlotController psc, IPlayerContext pc, bool starting, out AbstractScrobble scrobbleData, out TraktScrobbleStates state)
     {
       scrobbleData = null;
       state = starting ? TraktScrobbleStates.watching : TraktScrobbleStates.scrobble;
@@ -149,6 +223,21 @@ namespace MediaPortal.UiComponents.Trakt.Service
         return false;
 
       string title = pc.CurrentPlayer != null ? pc.CurrentPlayer.MediaItemTitle : null;
+      IMediaPlaybackControl pmc = pc.CurrentPlayer as IMediaPlaybackControl;
+      TimeSpan currentPosition;
+      if (pmc != null)
+      {
+        _progressUpdateWorks[psc].Duration = pmc.Duration;
+        currentPosition = pmc.CurrentTime;
+      }
+      else
+      {
+        // Player is already removed on stopping, so take the resume position if available
+        currentPosition = _progressUpdateWorks[psc].ResumePosition;
+      }
+
+      int progress = currentPosition == TimeSpan.Zero ? (starting ? 0 : 100) : Math.Min((int)(currentPosition.TotalSeconds * 100 / _progressUpdateWorks[psc].Duration.TotalSeconds), 100);
+
       string value;
       int iValue;
       DateTime dtValue;
@@ -195,6 +284,10 @@ namespace MediaPortal.UiComponents.Trakt.Service
 
       if (string.IsNullOrWhiteSpace(scrobbleData.Title))
         scrobbleData.Title = title;
+
+      scrobbleData.Progress = progress.ToString();
+      if (!starting && progress < WATCHED_PERCENT)
+        state = TraktScrobbleStates.cancelwatching;
 
       scrobbleData.PluginVersion = TraktSettings.Version;
       scrobbleData.MediaCenter = "MediaPortal 2";
