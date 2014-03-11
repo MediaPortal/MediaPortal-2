553e3e77f45c112fb8ca24ee45a693e6194c240b
morpheusxx <morpheus_xx@team-mediaportal.com>
2014-03-11 21:36:41 +0100

    Added "sync to trakt" for movies

--- MediaPortal/Incubator/Trakt/Models/TraktSetupModel.cs
+++ MediaPortal/Incubator/Trakt/Models/TraktSetupModel.cs
@@ -23,15 +23,21 @@
 #endregion
 
 using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using MediaPortal.Common;
 using MediaPortal.Common.General;
 using MediaPortal.Common.Logging;
+using MediaPortal.Common.MediaManagement;
+using MediaPortal.Common.MediaManagement.DefaultItemAspects;
+using MediaPortal.Common.MediaManagement.MLQueries;
 using MediaPortal.Common.Settings;
 using MediaPortal.Extensions.OnlineLibraries.Libraries.Trakt;
 using MediaPortal.Extensions.OnlineLibraries.Libraries.Trakt.DataStructures;
 using MediaPortal.UI.Presentation.Models;
 using MediaPortal.UI.Presentation.Workflow;
+using MediaPortal.UI.ServerCommunication;
 using TraktSettings = MediaPortal.UiComponents.Trakt.Settings.TraktSettings;
 
 namespace MediaPortal.UiComponents.Trakt.Models
@@ -138,6 +144,91 @@ namespace MediaPortal.UiComponents.Trakt.Models
         TestStatus = "Error";
         ServiceRegistration.Get<ILogger>().Error("Trakt.tv: Exception while testing account.", ex);
       }
+    }
+
+    public void SyncMediaToTrakt()
+    {
+      try
+      {
+        Guid[] types = { MediaAspect.ASPECT_ID, MovieAspect.ASPECT_ID };
+
+        var movies = ServiceRegistration.Get<IServerConnectionManager>().ContentDirectory.Search(new MediaItemQuery(types, null, null), true);
+        TraktMovieSync syncData = new TraktMovieSync { UserName = Username, Password = Password, MovieList = new List<TraktMovieSync.Movie>() };
+        // First send all movies to Trakt that we have so they appear in library
+        foreach (var movie in movies)
+          syncData.MovieList.Add(ToMovie(movie));
+
+        TraktSyncModes traktSyncMode = TraktSyncModes.library;
+        var response = TraktAPI.SyncMovieLibrary(syncData, traktSyncMode);
+        ServiceRegistration.Get<ILogger>().Info("Trakt.tv: Synchronized '{0}': {1} inserted, {2} existing, {3} skipped movies.", traktSyncMode, response.Inserted, SafeCount(response.AlreadyExistMovies), SafeCount(response.SkippedMovies));
+
+        syncData.MovieList.Clear();
+        // Then send only the watched movies as "seen"
+        foreach (var movie in movies.Where(IsWatched))
+          syncData.MovieList.Add(ToMovie(movie));
+
+        traktSyncMode = TraktSyncModes.seen;
+        response = TraktAPI.SyncMovieLibrary(syncData, traktSyncMode);
+        ServiceRegistration.Get<ILogger>().Info("Trakt.tv: Synchronized '{0}': {1} inserted, {2} existing, {3} skipped movies.", traktSyncMode, response.Inserted, SafeCount(response.AlreadyExistMovies), SafeCount(response.SkippedMovies));
+      }
+      catch (Exception ex)
+      {
+        ServiceRegistration.Get<ILogger>().Error("Trakt.tv: Exception while synchronizing media library.", ex);
+      }
+    }
+
+    private static bool IsWatched(MediaItem mediaItem)
+    {
+      int playCount;
+      return (MediaItemAspect.TryGetAttribute(mediaItem.Aspects, MediaAspect.ATTR_PLAYCOUNT, out playCount) && playCount > 0);
+    }
+
+    private static int SafeCount(IList list)
+    {
+      return list != null ? list.Count : 0;
+    }
+
+    private TraktMovieSync.Movie ToMovie(MediaItem mediaItem)
+    {
+      string value;
+      int iValue;
+      DateTime dtValue;
+
+      TraktMovieSync.Movie movie = new TraktMovieSync.Movie();
+      if (MediaItemAspect.TryGetAttribute(mediaItem.Aspects, MovieAspect.ATTR_MOVIE_NAME, out value) && !string.IsNullOrWhiteSpace(value))
+        movie.Title = value;
+
+      if (MediaItemAspect.TryGetAttribute(mediaItem.Aspects, MovieAspect.ATTR_IMDB_ID, out value) && !string.IsNullOrWhiteSpace(value))
+        movie.IMDBID = value;
+
+      if (MediaItemAspect.TryGetAttribute(mediaItem.Aspects, MovieAspect.ATTR_TMDB_ID, out iValue) && iValue > 0)
+        movie.TMDBID = iValue.ToString();
+
+      if (MediaItemAspect.TryGetAttribute(mediaItem.Aspects, MediaAspect.ATTR_RECORDINGTIME, out dtValue))
+        movie.Year = dtValue.Year.ToString();
+
+      return movie;
+    }
+
+    private string ToSeries(MediaItem mediaItem)
+    {
+      string value;
+      int iValue;
+      DateTime dtValue;
+
+      TraktEpisodeScrobble series = new TraktEpisodeScrobble();
+      if (MediaItemAspect.TryGetAttribute(mediaItem.Aspects, SeriesAspect.ATTR_SERIESNAME, out value) && !string.IsNullOrWhiteSpace(value))
+        series.Title = value;
+
+      if (MediaItemAspect.TryGetAttribute(mediaItem.Aspects, SeriesAspect.ATTR_FIRSTAIRED, out dtValue))
+        series.Year = dtValue.Year.ToString();
+
+      if (MediaItemAspect.TryGetAttribute(mediaItem.Aspects, SeriesAspect.ATTR_SEASON, out iValue))
+        series.Season = iValue.ToString();
+      List<int> intList;
+      if (MediaItemAspect.TryGetAttribute(mediaItem.Aspects, SeriesAspect.ATTR_EPISODE, out intList) && intList.Any())
+        series.Episode = intList.First().ToString(); // TODO: multi episode files?!
+      return null;
     }
 
     #endregion
