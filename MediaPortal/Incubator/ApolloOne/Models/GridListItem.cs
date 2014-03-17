367ca1bce86196e0652745600750d358a9e437db
morpheusxx <morpheus_xx@team-mediaportal.com>
2014-03-17 06:29:39 +0100

    WiP: custom menu model to allow free positioning of items

---
+++ MediaPortal/Incubator/ApolloOne/Models/GridListItem.cs
@@ -0,0 +1,77 @@
+﻿using MediaPortal.Common.General;
+using MediaPortal.UI.Presentation.DataObjects;
+
+namespace MediaPortal.UiComponents.ApolloOne.Models
+{
+  /// <summary>
+  /// Extends a generic <see cref="ListItem"/> with position and size information.
+  /// </summary>
+  public class GridListItem : ListItem
+  {
+    readonly AbstractProperty _gridRowProperty = new WProperty(typeof(int), 0);
+    readonly AbstractProperty _gridRowSpanProperty = new WProperty(typeof(int), 1);
+    readonly AbstractProperty _gridColumnProperty = new WProperty(typeof(int), 0);
+    readonly AbstractProperty _gridColumnSpanProperty = new WProperty(typeof(int), 1);
+
+    public int GridRow
+    {
+      get { return (int)_gridRowProperty.GetValue(); }
+      set { _gridRowProperty.SetValue(value); }
+    }
+
+    public AbstractProperty GridRowProperty
+    {
+      get { return _gridRowProperty; }
+    }
+
+    public int GridRowSpan
+    {
+      get { return (int)_gridRowSpanProperty.GetValue(); }
+      set { _gridRowSpanProperty.SetValue(value); }
+    }
+    public AbstractProperty GridRowSpanProperty
+    {
+      get { return _gridRowSpanProperty; }
+    }
+
+    public int GridColumn
+    {
+      get { return (int)_gridColumnProperty.GetValue(); }
+      set { _gridColumnProperty.SetValue(value); }
+    }
+
+    public AbstractProperty GridColumnProperty
+    {
+      get { return _gridColumnProperty; }
+    }
+
+    public int GridColumnSpan
+    {
+      get { return (int)_gridColumnSpanProperty.GetValue(); }
+      set { _gridColumnSpanProperty.SetValue(value); }
+    }
+
+    public AbstractProperty GridColumnSpanProperty
+    {
+      get { return _gridColumnSpanProperty; }
+    }
+
+    #region Constructor
+
+    public GridListItem()
+    {
+    }
+
+    public GridListItem(ListItem origItem)
+    {
+      AdditionalProperties = origItem.AdditionalProperties;
+      Enabled = origItem.Enabled;
+      Command = origItem.Command;
+      IsVisible = origItem.IsVisible;
+      Labels = origItem.Labels;
+      Selected = origItem.Selected;
+    }
+
+    #endregion
+  }
+}
---
+++ MediaPortal/Incubator/ApolloOne/Models/HomeMenuModel.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using MediaPortal.Common.General;
+using MediaPortal.UI.Presentation.DataObjects;
+using MediaPortal.UI.Presentation.Workflow;
+using MediaPortal.UiComponents.SkinBase.General;
+using MediaPortal.UiComponents.SkinBase.Models;
+
+namespace MediaPortal.UiComponents.ApolloOne.Models
+{
+  public class HomeMenuModel : MenuModel
+  {
+    #region Consts
+
+    public const string STR_HOMEMENU_MODEL_ID = "EBA16B93-B669-4162-9CA2-CB1D5E267EC3";
+    public static readonly Guid HOMEMENU_MODEL_ID = new Guid(STR_HOMEMENU_MODEL_ID);
+
+    #endregion
+
+    #region Fields
+
+    readonly Dictionary<Guid, GridPosition> _positions = new Dictionary<Guid, GridPosition>();
+    readonly ItemsList _positionedItems = new ItemsList();
+
+    #endregion
+
+    #region Internal class
+
+    public class GridPosition
+    {
+      public int Row { get; set; }
+      public int RowSpan { get; set; }
+      public int Column { get; set; }
+      public int ColumnSpan { get; set; }
+
+      public GridPosition()
+      {
+        RowSpan = 1;
+        ColumnSpan = 1;
+      }
+    }
+
+    #endregion
+
+    public HomeMenuModel()
+    {
+      ReadPositions();
+      CreatePositionedItems();
+      MenuItems.ObjectChanged += MenuItemsOnObjectChanged;
+    }
+
+    private void MenuItemsOnObjectChanged(IObservable observable)
+    {
+      CreatePositionedItems();
+    }
+
+    public void CreatePositionedItems()
+    {
+      _positionedItems.Clear();
+      foreach (var menuItem in MenuItems)
+      {
+        object action;
+        if (!menuItem.AdditionalProperties.TryGetValue(Consts.KEY_ITEM_ACTION, out action))
+          continue;
+        WorkflowAction wfAction = action as WorkflowAction;
+        if (wfAction == null)
+          continue;
+
+        GridPosition gridPosition;
+        if (_positions.TryGetValue(wfAction.ActionId, out gridPosition))
+        {
+          GridListItem gridItem = new GridListItem(menuItem)
+          {
+            GridRow = gridPosition.Row,
+            GridColumn = gridPosition.Column,
+            GridRowSpan = gridPosition.RowSpan,
+            GridColumnSpan = gridPosition.ColumnSpan,
+          };
+          _positionedItems.Add(gridItem);
+        }
+      }
+      _positionedItems.FireChange();
+    }
+
+    public ItemsList PositionedMenuItems
+    {
+      get
+      {
+        return _positionedItems;
+      }
+    }
+
+    /// <summary>
+    /// Reads actions/positon from settings.
+    /// </summary>
+    private void ReadPositions()
+    {
+      _positions[new Guid("A4DF2DF6-8D66-479a-9930-D7106525EB07")] = new GridPosition { Column = 0, ColumnSpan = 5, Row = 0, RowSpan = 3 }; // Videos
+      _positions[new Guid("80D2E2CC-BAAA-4750-807B-F37714153751")] = new GridPosition { Column = 0, ColumnSpan = 5, Row = 3, RowSpan = 3 }; // Movies
+      _positions[new Guid("30F57CBA-459C-4202-A587-09FFF5098251")] = new GridPosition { Column = 5, ColumnSpan = 5, Row = 0, RowSpan = 3 }; // Series
+
+      _positions[new Guid("93442DF7-186D-42e5-A0F5-CF1493E68F49")] = new GridPosition { Column = 10, ColumnSpan = 6, Row = 0, RowSpan = 6 }; // Browse Media
+
+      _positions[new Guid("BB49A591-7705-408F-8177-45D633FDFAD0")] = new GridPosition { Column = 5, ColumnSpan = 2, Row = 3, RowSpan = 2 };
+
+      _positions[new Guid("17D2390E-5B05-4fbd-89F6-24D60CEB427F")] = new GridPosition { Column = 5, ColumnSpan = 5, Row = 0, RowSpan = 3 };
+    }
+  }
+}
