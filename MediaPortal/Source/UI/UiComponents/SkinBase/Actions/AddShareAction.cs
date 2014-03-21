5906cacd670c1ef5bad7e4ebd126b34fdaa2bca9
morpheusxx <morpheus_xx@team-mediaportal.com>
2014-03-21 20:07:20 +0100

    Changed action back to always use system selection

--- MediaPortal/Source/UI/UiComponents/SkinBase/Actions/AddShareAction.cs
+++ MediaPortal/Source/UI/UiComponents/SkinBase/Actions/AddShareAction.cs
@@ -92,12 +92,9 @@ namespace MediaPortal.UiComponents.SkinBase.Actions
             DialogButtonType.Ok);
         return;
       }
-      bool isSingleOption = localHomeServer || !homeServerConnected;
+
       IWorkflowManager workflowManager = ServiceRegistration.Get<IWorkflowManager>();
-      workflowManager.NavigatePush(
-        isSingleOption ?
-        Consts.WF_STATE_ID_SHARE_ADD_CHOOSE_RESOURCE_PROVIDER :
-        Consts.WF_STATE_ID_SHARE_ADD_CHOOSE_SYSTEM);
+      workflowManager.NavigatePush(Consts.WF_STATE_ID_SHARE_ADD_CHOOSE_SYSTEM);
     }
 
     #endregion
