7ca52e96f541e182862e1f53d2d5b8317ddc4662
morpheusxx <morpheus_xx@team-mediaportal.com>
2014-07-27 16:22:48 +0200

    Updated helper tool to Net 4.5.1

--- MediaPortal/Resources/MediaItemAspectModelBuilder/MediaItemAspectModelBuilder/AspectModelBuilder.cs
+++ MediaPortal/Resources/MediaItemAspectModelBuilder/MediaItemAspectModelBuilder/AspectModelBuilder.cs
@@ -111,7 +111,7 @@ namespace MediaItemAspectModelBuilder
 
       // Copyright
       _copyright.Add(@"/*
-    Copyright (C) 2007-2013 Team MediaPortal
+    Copyright (C) 2007-2014 Team MediaPortal
     http://www.team-mediaportal.com
 
     This file is part of MediaPortal 2
@@ -154,7 +154,7 @@ namespace MediaItemAspectModelBuilder
       // Construct source file
       StringBuilder result = new StringBuilder();
 
-      AppendRegion(result, "Copyright (C) 2007-2013 Team MediaPortal", _copyright, false);
+      AppendRegion(result, "Copyright (C) 2007-2014 Team MediaPortal", _copyright, false);
 
       AppendRegion(result, null, _usings, false);
 
