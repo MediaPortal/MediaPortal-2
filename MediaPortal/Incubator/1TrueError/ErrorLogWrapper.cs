4a22e0bfb04b587be71b7323e1687406c27bd527
morpheusxx <morpheus_xx@team-mediaportal.com>
2014-08-24 13:23:06 +0200

    First version of error tracking based on https://onetrueerror.com/

---
+++ MediaPortal/Incubator/1TrueError/ErrorLogWrapper.cs
@@ -0,0 +1,116 @@
+﻿#region Copyright (C) 2007-2014 Team MediaPortal
+
+/*
+    Copyright (C) 2007-2014 Team MediaPortal
+    http://www.team-mediaportal.com
+
+    This file is part of MediaPortal 2
+
+    MediaPortal 2 is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    MediaPortal 2 is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with MediaPortal 2. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#endregion
+
+using System;
+using MediaPortal.Common.Logging;
+using OneTrueError.Reporting;
+
+namespace MediaPortal.Plugins.OneTrueError
+{
+  public class ErrorLogWrapper : ILogger
+  {
+    private readonly ILogger _logger;
+
+    /// <summary>
+    /// Creates a new <see cref="ErrorLogWrapper"/> instance and initializes it with the given <paramref name="parentLogger"/>.
+    /// All logging calls that contain an exception will be reported.
+    /// </summary>
+    /// <param name="parentLogger">Current logger to be wrapped around.</param>
+    public ErrorLogWrapper(ILogger parentLogger)
+    {
+      _logger = parentLogger;
+    }
+
+    #region ILogger implementation
+
+    public void Debug(string format, params object[] args)
+    {
+      _logger.Debug(format, args);
+    }
+
+    public void Debug(string format, Exception ex, params object[] args)
+    {
+      _logger.Debug(string.Format(format, args), ex);
+      OneTrue.Report(ex);
+    }
+
+    public void Info(string format, params object[] args)
+    {
+      _logger.Info(format, args);
+    }
+
+    public void Info(string format, Exception ex, params object[] args)
+    {
+      _logger.Info(string.Format(format, args), ex);
+      OneTrue.Report(ex);
+    }
+
+    public void Warn(string format, params object[] args)
+    {
+      _logger.Warn(format, args);
+    }
+
+    public void Warn(string format, Exception ex, params object[] args)
+    {
+      _logger.Warn(string.Format(format, args), ex);
+      OneTrue.Report(ex);
+    }
+
+    public void Error(string format, params object[] args)
+    {
+      _logger.Error(format, args);
+    }
+
+    public void Error(string format, Exception ex, params object[] args)
+    {
+      _logger.Error(string.Format(format, args), ex);
+      OneTrue.Report(ex);
+    }
+
+    public void Error(Exception ex)
+    {
+      _logger.Error("", ex);
+      OneTrue.Report(ex);
+    }
+
+    public void Critical(string format, params object[] args)
+    {
+      _logger.Critical(format, args);
+    }
+
+    public void Critical(string format, Exception ex, params object[] args)
+    {
+      _logger.Critical(string.Format(format, args), ex);
+      OneTrue.Report(ex);
+    }
+
+    public void Critical(Exception ex)
+    {
+      _logger.Critical("", ex);
+      OneTrue.Report(ex);
+    }
+
+    #endregion
+  }
+}
---
+++ MediaPortal/Incubator/1TrueError/ErrorReportingService.cs
@@ -0,0 +1,71 @@
+﻿#region Copyright (C) 2007-2014 Team MediaPortal
+
+/*
+    Copyright (C) 2007-2014 Team MediaPortal
+    http://www.team-mediaportal.com
+
+    This file is part of MediaPortal 2
+
+    MediaPortal 2 is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    MediaPortal 2 is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with MediaPortal 2. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#endregion
+
+using System;
+using MediaPortal.Common;
+using MediaPortal.Common.Logging;
+using MediaPortal.Common.PluginManager;
+using MediaPortal.Common.SystemResolver;
+using OneTrueError.Reporting;
+
+namespace MediaPortal.Plugins.OneTrueError
+{
+  public class ErrorReportingService : IPluginStateTracker
+  {
+    private static Tuple<string, string> KEY_SERVER = new Tuple<string, string>("f2f6310b-1714-4112-bd6d-ec9df98ade37", "c6965f14-db18-41d1-8d1b-ffef6586be92");
+    private static Tuple<string, string> KEY_CLIENT = new Tuple<string, string>("9f39363e-e7c7-4e42-acc7-914ec41a52eb", "93dec981-b867-4adb-8e80-6cb86f52c034");
+
+    public void Activated(PluginRuntime pluginRuntime)
+    {
+      ISystemResolver systemResolver = ServiceRegistration.Get<ISystemResolver>();
+      var appKey = systemResolver.SystemType == SystemType.Server ? KEY_SERVER : KEY_CLIENT;
+
+      // The appkey and shared key can be found in onetrueeror.com
+      OneTrue.Configuration.Credentials(appKey.Item1, appKey.Item2);
+      OneTrue.Configuration.CatchWinFormsExeptions();
+
+      // Exchange the logger by the error reporting wrapper
+      var currentLogger = ServiceRegistration.Get<ILogger>();
+      var errorLogger = new ErrorLogWrapper(currentLogger);
+      ServiceRegistration.Set<ILogger>(errorLogger);
+    }
+
+    public bool RequestEnd()
+    {
+      return true;
+    }
+
+    public void Stop()
+    {
+    }
+
+    public void Continue()
+    {
+    }
+
+    public void Shutdown()
+    {
+    }
+  }
+}
