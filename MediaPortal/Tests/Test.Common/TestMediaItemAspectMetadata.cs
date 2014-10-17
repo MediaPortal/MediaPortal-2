5df03fcc5c432e6a5d912c269ce2a18e4d3e1055
MrTechno <MrTechno@users.noreply.github.com>
2014-10-17 22:45:42 +0100

    Tests
    Added Test.Common project

---
+++ MediaPortal/Tests/Test.Common/TestMediaItemAspectMetadata.cs
@@ -0,0 +1,44 @@
+﻿using MediaPortal.Common;
+using MediaPortal.Common.MediaManagement;
+using MediaPortal.Common.MediaManagement.DefaultItemAspects;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml;
+
+namespace Test.Common
+{
+  [TestClass]
+  public class TestMediaItemAspectMetadata
+  {
+    [TestMethod]
+    public void TestMetadata()
+    {
+      TextWriter writer = new StringWriter();
+      XmlWriter serialiser = new XmlTextWriter(writer);
+      serialiser.WriteStartElement("Test"); // Wrapper around the albums
+      MediaAspect.Metadata.Serialize(serialiser);
+      RelationshipAspect.Metadata.Serialize(serialiser);
+      serialiser.WriteEndElement();
+
+      Console.WriteLine("XML: {0}", writer.ToString());
+      //Assert.AreEqual("<MI Id=\"" + trackId + "\"><Relationship ItemType=\"" + AudioAspect.RELATIONSHIP_TRACK + "\" RelationshipType=\"" + AlbumAspect.RELATIONSHIP_ALBUM + "\" RelationshipId=\"" + albumId + "\" /></MI>", trackText.ToString(), "Track XML");
+
+      XmlReader reader = XmlReader.Create(new StringReader(writer.ToString()));
+      reader.Read(); // Test
+      Console.WriteLine("Reader state Test, {0} {1}", reader.NodeType, reader.Name);
+
+      // Media metadata
+      reader.Read(); // MI
+      Console.WriteLine("Reader state metadata1, {0} {1}", reader.NodeType, reader.Name);
+      MediaItemAspectMetadata metadata1 = MediaItemAspectMetadata.Deserialize(reader);
+
+      // Relationship metadata
+      Console.WriteLine("Reader state metadata2, {0} {1}", reader.NodeType, reader.Name);
+      MediaItemAspectMetadata metadata2 = MediaItemAspectMetadata.Deserialize(reader);
+
+      reader.Read(); // Test
+    }
+  }
+}
---
+++ MediaPortal/Tests/Test.Common/TestMediaItemQuery.cs
@@ -0,0 +1,42 @@
+﻿using MediaPortal.Common.MediaManagement.DefaultItemAspects;
+using MediaPortal.Common.MediaManagement.MLQueries;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Test.Common
+{
+  [TestClass]
+  public class TestMediaItemQuery
+  {
+    [TestMethod]
+    public void TestFilter()
+    {
+      Guid itemId = new Guid("11111111-aaaa-aaaa-aaaa-111111111111");
+      Guid itemType = new Guid("22222222-bbbb-bbbb-bbbb-222222222222");
+      Guid relationshipType = new Guid("33333333-cccc-cccc-cccc-333333333333");
+
+      MediaItemQuery query1 = new MediaItemQuery(new Guid[] { MediaAspect.ASPECT_ID }, new RelationshipFilter(itemId, itemType, relationshipType));
+
+      TextWriter writer = new StringWriter();
+      XmlWriter serialiser = new XmlTextWriter(writer);
+      query1.Serialize(serialiser);
+
+      Console.WriteLine("XML: {0}", writer.ToString());
+
+      XmlReader reader = XmlReader.Create(new StringReader(writer.ToString()));
+      MediaItemQuery query2 = MediaItemQuery.Deserialize(reader);
+
+      Assert.IsTrue(query2.Filter is RelationshipFilter, "Query filter type");
+      RelationshipFilter filter = (RelationshipFilter)query2.Filter;
+      Assert.AreEqual(filter.ItemId, itemId, "Filter item ID");
+      Assert.AreEqual(filter.Role, itemType, "Filter item type");
+      Assert.AreEqual(filter.LinkedRole, relationshipType, "Filter item linked role");
+    }
+  }
+}
