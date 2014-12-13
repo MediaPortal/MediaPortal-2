37063b8690bc6a37b3a9e11bfb386defc1d854fe
MJGraf <mgraf@igros.de>
2014-12-13 13:16:16 +0100

    MP2-459: Added NfoMetadataExtractors plugin (no functionality, yet)
    Conflicts:
    	MediaPortal/Source/MP2-Server.sln

---
+++ MediaPortal/Source/Extensions/MetadataExtractors/NfoMetadataExtractors/Properties/AssemblyInfo.cs
@@ -0,0 +1,21 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// Allgemeine Informationen über eine Assembly werden über die folgenden 
+// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+// die mit einer Assembly verknüpft sind.
+[assembly: AssemblyTitle("NfoMetadataExtractors")]
+[assembly: AssemblyDescription("Metadata extractors reading from nfo-files")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Durch Festlegen von ComVisible auf "false" werden die Typen in dieser Assembly unsichtbar 
+// für COM-Komponenten. Wenn Sie auf einen Typ in dieser Assembly von 
+// COM zugreifen müssen, legen Sie das ComVisible-Attribut für diesen Typ auf "true" fest.
+[assembly: ComVisible(false)]
+
+// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+[assembly: Guid("2505C495-28AA-4D1C-BDEE-CA4A3A89B0D5")]
+
+// Other attributes are included from VersionInfo.cs!
