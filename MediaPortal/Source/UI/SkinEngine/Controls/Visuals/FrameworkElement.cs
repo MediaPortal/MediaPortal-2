e44c40bf07abcf130720bf6f8a57811f7ad99c70
morpheusxx <morpheus_xx@team-mediaportal.com>
2015-01-01 22:16:43 +0100

    Fix for layer imbalance

--- MediaPortal/Source/UI/SkinEngine/Controls/Visuals/FrameworkElement.cs
+++ MediaPortal/Source/UI/SkinEngine/Controls/Visuals/FrameworkElement.cs
@@ -2013,6 +2013,7 @@ namespace MediaPortal.UI.SkinEngine.Controls.Visuals
         _renderedBoundingBox = CalculateBoundingBox(_innerRect, finalTransform);
       }
 
+      bool layerPushed = false;
       Brushes.Brush opacityMask = OpacityMask;
       if (opacityMask != null && opacityMask.TryAllocate())
       {
@@ -2036,12 +2037,11 @@ namespace MediaPortal.UI.SkinEngine.Controls.Visuals
         };
 
         GraphicsDevice11.Instance.Context2D1.PushLayer(ref layerParameters, null);
+        layerPushed = true;
       }
-      // TODO
-      //if (opacityMask == null && Effect == null)
-      // Simply render without opacity mask
+
       RenderOverride(localRenderContext);
-      if (opacityMask != null)
+      if (layerPushed)
       {
         GraphicsDevice11.Instance.Context2D1.PopLayer();
       }
