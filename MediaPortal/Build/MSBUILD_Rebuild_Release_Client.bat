6076f549a3c2ddbf753e81340c86a4a6f968ccc1
morpheusxx <morpheus_xx@team-mediaportal.com>
2015-03-06 19:19:40 +0100

    Include EVRPresenter into build process, requires MSBuild 12 to be available

--- MediaPortal/Build/MSBUILD_Rebuild_Release_Client.bat
+++ MediaPortal/Build/MSBUILD_Rebuild_Release_Client.bat
@@ -8,6 +8,8 @@ set xml=Build_Report_Release_Client.xml
 set html=Build_Report_Release_Client.html
 
 set logger=/l:XmlFileLogger,"%PathToBuildReport%\MSBuild.ExtensionPack.Loggers.dll";logfile=%xml%
-"%WINDIR%\Microsoft.NET\Framework\v4.0.30319\MSBUILD.exe" /m Build.proj %logger% /property:OneStepOnly=true;BuildClient=true
+rem Tools version 4 is not able to build the VS2013 C++ project :-(
+rem "%WINDIR%\Microsoft.NET\Framework\v4.0.30319\MSBUILD.exe" /m Build.proj %logger% /property:OneStepOnly=true;BuildClient=true
+"C:\Program Files (x86)\MSBuild\12.0\bin\MSBUILD.exe" /m Build.proj %logger% /property:OneStepOnly=true;BuildClient=true
 
 %PathToBuildReport%\msxsl %xml% _BuildReport_Files\BuildReport.xslt -o %html%
