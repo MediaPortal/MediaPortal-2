523acff0e667bfdcfaf66772be8bebb90873b139
MrTechno <MrTechno@users.noreply.github.com>
2015-04-07 22:12:41 +0100

    Added new MIAs
    AudioAlbumAspect, SeasonAspect, SeriesAspect

---
+++ MediaPortal/Source/Core/MediaPortal.Common/MediaManagement/DefaultItemAspects/SeriesAspect.cs
@@ -0,0 +1,50 @@
+#region Copyright (C) 2007-2015 Team MediaPortal
+
+/*
+    Copyright (C) 2007-2015 Team MediaPortal
+    http://www.team-mediaportal.com
+
+    This file is part of MediaPortal 2
+
+    MediaPortal 2 is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    MediaPortal 2 is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with MediaPortal 2. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#endregion
+
+using System;
+
+namespace MediaPortal.Common.MediaManagement.DefaultItemAspects
+{
+  /// <summary>
+  /// Contains the metadata specification of the "Series" media item aspect which is assigned to series media items.
+  /// </summary>
+  public static class SeriesAspect
+  {
+    /// <summary>
+    /// Media item aspect id of the series aspect.
+    /// </summary>
+    public static readonly Guid ASPECT_ID = new Guid("2C3D5A0C-B7E5-45B3-85C6-40FFE6766FC0");
+
+    /// <summary>
+    /// Series name.
+    /// </summary>
+    public static readonly MediaItemAspectMetadata.SingleAttributeSpecification ATTR_SERIESNAME =
+        MediaItemAspectMetadata.CreateSingleStringAttributeSpecification("SeriesName", 200, Cardinality.Inline, false);
+
+    public static readonly SingleMediaItemAspectMetadata Metadata = new SingleMediaItemAspectMetadata(
+        ASPECT_ID, "SeriesItem", new[] {
+            ATTR_SERIESNAME,
+        });
+  }
+}
