22c1aaf9f5b3905465f92d6f72f535cf82b28d25
MrTechno <MrTechno@users.noreply.github.com>
2015-04-15 10:34:04 +0100

    Added DevTools program
    Server side developer tool for administering media sources, checking
    media items

---
+++ MediaPortal/Tools/DevTools/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DevTools")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("192a3848-e11d-4919-a848-40be5f7b0eaf")]
