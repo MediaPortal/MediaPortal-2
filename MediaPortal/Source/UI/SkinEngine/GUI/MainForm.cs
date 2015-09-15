04571a3d67364647fb6fa8f7376da15c0a22d894
morpheusxx <morpheus_xx@team-mediaportal.com>
2015-09-15 22:08:07 +0200

    Log screen resolution changes and reset DX device (try to fix scaling issues after suspend/resume)

--- MediaPortal/Source/UI/SkinEngine/GUI/MainForm.cs
+++ MediaPortal/Source/UI/SkinEngine/GUI/MainForm.cs
@@ -890,6 +890,15 @@ namespace MediaPortal.UI.SkinEngine.GUI
         return;
       }
 
+      if (m.Msg == WM_DISPLAYCHANGE)
+      {
+        int bitDepth = m.WParam.ToInt32();
+        int screenWidth = m.LParam.ToInt32() & 0xFFFF;
+        int screenHeight = m.LParam.ToInt32() >> 16;
+        ServiceRegistration.Get<ILogger>().Info("SkinEngine MainForm: Display changed to {0}x{1}@{2}.", screenWidth, screenHeight, bitDepth);
+        GraphicsDevice.Reset();
+      }
+
       // Albert, 2010-03-13: The following code can be used to make the window always maintain a fixed aspect ratio.
       // It was commented out because it doesn't really help. At least in the fullscreen mode, the aspect ratio is determined
       // by the screen and not by any other desired aspect ratio. I think we should not use the code, that's why I comment
