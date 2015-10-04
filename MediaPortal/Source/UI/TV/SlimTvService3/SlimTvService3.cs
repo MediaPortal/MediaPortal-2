30f99cd18be412bcc669823c2d86b4f59acc4048
morpheusxx <morpheus_xx@team-mediaportal.com>
2015-10-04 14:40:19 +0200

    Fixed loading of radio epg

--- MediaPortal/Source/UI/TV/SlimTvService3/SlimTvService3.cs
+++ MediaPortal/Source/UI/TV/SlimTvService3/SlimTvService3.cs
@@ -338,8 +338,16 @@ namespace MediaPortal.Plugins.SlimTv.Service
     public override bool GetProgramsGroup(IChannelGroup channelGroup, DateTime from, DateTime to, out IList<IProgram> programs)
     {
       programs = new List<IProgram>();
+      if (channelGroup.ChannelGroupId < 0)
+      {
+        foreach (var channel in _tvBusiness.GetRadioGuideChannelsForGroup(-channelGroup.ChannelGroupId))
+          CollectionUtils.AddAll(programs, _tvBusiness.GetPrograms(TvDatabase.Channel.Retrieve(channel.IdChannel), from, to).Select(p => p.ToProgram()));
+      }
+      else
+      {
       foreach (var channel in _tvBusiness.GetTVGuideChannelsForGroup(channelGroup.ChannelGroupId))
         CollectionUtils.AddAll(programs, _tvBusiness.GetPrograms(TvDatabase.Channel.Retrieve(channel.IdChannel), from, to).Select(p => p.ToProgram()));
+      }
       return programs.Count > 0;
     }
 
