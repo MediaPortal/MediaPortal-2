c6f0b086c06215b2653effa1d6d9f804ec0df6d3
FreakyJ <JensMarkwart@gmx.de>
2015-10-29 18:01:59 +0100

    wip commit

--- MediaPortal/Source/UI/TV/SlimTvInterfaces/Items/IProgramSeries.cs
+++ MediaPortal/Source/UI/TV/SlimTvInterfaces/Items/IProgramSeries.cs
@@ -34,9 +34,19 @@ namespace MediaPortal.Plugins.SlimTv.Interfaces.Items
     String SeasonNumber { get; set; }
 
     /// <summary>
-    /// Gets or Sets the Number of an Episode.
+    /// Gets or Sets the Number of an Episode. Format: [Episode]/[Total Episodes]
     /// </summary>
     String EpisodeNumber { get; set; }
+
+    /// <summary>
+    /// Gets or Sets the Number of an Episode. Format: [Season].[Episode]/[Total Episodes].[Part]/[Total Parts]
+    /// </summary>
+    String EpisodeNumberDetailed { get; set; }
+
+    /// <summary>
+    /// Gets or Sets the Part of an Episode.
+    /// </summary>
+    String EpisodePart { get; set; }
 
     /// <summary>
     /// Gets or Sets the Title of an Episode.
