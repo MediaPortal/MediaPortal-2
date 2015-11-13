71f1070752656d5426f32f387429f30212d6daa1
morpheusxx <morpheus_xx@team-mediaportal.com>
2015-11-13 17:25:55 +0100

    Added copyright headers

--- MediaPortal/Incubator/UPnPRenderer/OnlineVideos/IFilterState.cs
+++ MediaPortal/Incubator/UPnPRenderer/OnlineVideos/IFilterState.cs
@@ -1,4 +1,28 @@
-﻿// Type: OnlineVideos.MPUrlSourceFilter.IFilterState
+﻿#region Copyright (C) 2007-2015 Team MediaPortal
+
+/*
+    Copyright (C) 2007-2015 Team MediaPortal
+    http://www.team-mediaportal.com
+
+    This file is part of MediaPortal 2
+
+    MediaPortal 2 is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    MediaPortal 2 is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with MediaPortal 2. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#endregion
+
+// Type: OnlineVideos.MPUrlSourceFilter.IFilterState
 // Assembly: OnlineVideos, Version=1.10.0.1, Culture=neutral, PublicKeyToken=null
 // MVID: 8F27759C-CFBE-47CB-A39F-F16055EE5D07
 // Assembly location: M:\Programmieren\C#\MediaPortal 2\MediaPortal\Incubator\UPnPRenderer\references\OnlineVideos.dll
