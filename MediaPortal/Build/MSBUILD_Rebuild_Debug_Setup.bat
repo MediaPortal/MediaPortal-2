aff766a109f4fee09ae5baf80565201071590405
MrTechno <MrTechno@users.noreply.github.com>
2015-11-20 23:31:08 +0000

    NuGet package, batch file fixes

--- MediaPortal/Build/MSBUILD_Rebuild_Debug_Setup.bat
+++ MediaPortal/Build/MSBUILD_Rebuild_Debug_Setup.bat
@@ -1,13 +1,22 @@
+@echo off
+cls
 
 @"%WINDIR%\Microsoft.NET\Framework\v4.0.30319\MSBUILD.exe" RestorePackages.targets /target:RestoreBuildPackages
 
 set PathToBuildReport=.\..\Packages\BuildReport.1.0.0
 xcopy /I /Y %PathToBuildReport%\_BuildReport_Files .\_BuildReport_Files
 
-set xml=Build_Report_Debug_Setup.xml
-set html=Build_Report_Debug_Setup.html
+set CONFIGURATION=Debug
+
+set xml=Build_Report_%CONFIGURATION%_Setup.xml
+set html=Build_Report_%CONFIGURATION%_Setup.html
 
 set logger=/l:XmlFileLogger,"%PathToBuildReport%\MSBuild.ExtensionPack.Loggers.dll";logfile=%xml%
-"%WINDIR%\Microsoft.NET\Framework\v4.0.30319\MSBUILD.exe" /m Build.proj %logger% /property:OneStepOnly=true;BuildSetup=true;Configuration=Debug 
+"%WINDIR%\Microsoft.NET\Framework\v4.0.30319\MSBUILD.exe" /m Build.proj %logger% /property:OneStepOnly=true;BuildSetup=true;Configuration=%CONFIGURATION% 
+echo Component build exit code %errorlevel%
+if errorlevel 1 (
+   echo Component build failed
+   exit /b %errorlevel%
+)
 
 %PathToBuildReport%\msxsl %xml% _BuildReport_Files\BuildReport.xslt -o %html%
