b3c243cba0687e8524ff3b1b5de92086b5942f2d
MrTechno <MrTechno@users.noreply.github.com>
2015-11-20 23:32:00 +0000

    Batch script fixes

--- MediaPortal/Build/MSBUILD_Rebuild_Debug_Components.bat
+++ MediaPortal/Build/MSBUILD_Rebuild_Debug_Components.bat
@@ -4,10 +4,13 @@
 set PathToBuildReport=.\..\Packages\BuildReport.1.0.0
 xcopy /I /Y %PathToBuildReport%\_BuildReport_Files .\_BuildReport_Files
 
-set xml=Build_Report_Release_Server.xml
-set html=Build_Report_Release_Server.html
+set CONFIGURATION=Debug
+
+set xml=Build_Report_%CONFIGURATION%_Components.xml
+set html=Build_Report_%CONFIGURATION%_Components.html
 
 set logger=/l:XmlFileLogger,"%PathToBuildReport%\MSBuild.ExtensionPack.Loggers.dll";logfile=%xml%
-"%WINDIR%\Microsoft.NET\Framework\v4.0.30319\MSBUILD.exe" /m Build.proj %logger% /property:BuildClient=true;BuildServer=true;BuildServiceMonitor=true;BuildSetup=false;Configuration=Debug
+"%WINDIR%\Microsoft.NET\Framework\v4.0.30319\MSBUILD.exe" /m Build.proj %logger% /property:BuildClient=true;BuildServer=true;BuildServiceMonitor=true;BuildSetup=false;Configuration=%CONFIGURATION%
+"%WINDIR%\Microsoft.NET\Framework\v4.0.30319\MSBUILD.exe" /m Build.proj %logger% /property:OneStepOnly=true;ClearBinDirectory=false;BuildClient=false;BuildServer=false;BuildServiceMonitor=false;BuildSetup=true;Configuration=%CONFIGURATION%
 
 %PathToBuildReport%\msxsl %xml% _BuildReport_Files\BuildReport.xslt -o %html%
