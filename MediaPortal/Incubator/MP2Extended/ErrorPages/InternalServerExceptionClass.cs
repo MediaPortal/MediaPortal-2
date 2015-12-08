3bf0ba3401afcb86c10349c979c4e1c7de5b2e4e
FreakyJ <JensMarkwart@gmx.de>
2015-12-08 20:21:45 +0100

    make the error page more detailed

--- MediaPortal/Incubator/MP2Extended/ErrorPages/InternalServerExceptionClass.cs
+++ MediaPortal/Incubator/MP2Extended/ErrorPages/InternalServerExceptionClass.cs
@@ -2,13 +2,27 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using HttpServer.Exceptions;
 
 namespace MediaPortal.Plugins.MP2Extended.ErrorPages
 {
   partial class InternalServerExceptionTemplate
   {
     private Exception ex;
-    public InternalServerExceptionTemplate(Exception exception) { this.ex = exception; }
+    private int errorCode = 500;
+    private string errorName = "Internal Server Error";
+
+    public InternalServerExceptionTemplate(Exception exception)
+    {
+      ex = exception;
+      HttpException httpEx = exception as HttpException;
+      if (httpEx != null)
+      {
+        errorCode = (int)httpEx.HttpStatusCode;
+        errorName = Regex.Replace(httpEx.HttpStatusCode.ToString(), "([a-z])([A-Z])", "$1 $2");
+      }
+    }
   }
 }
