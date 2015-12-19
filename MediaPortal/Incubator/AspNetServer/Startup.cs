59343f01636748e8d5f6d74ce3fcffb5bca725c1
MJGraf <mgraf@igros.de>
2015-12-19 17:28:51 +0100

    MP2-500: First test implementation of an AspNetServer plugin using ASP.Net 5

---
+++ MediaPortal/Incubator/AspNetServer/Startup.cs
@@ -0,0 +1,53 @@
+﻿#region Copyright (C) 2007-2015 Team MediaPortal
+
+/*
+    Copyright (C) 2007-2015 Team MediaPortal
+    http://www.team-mediaportal.com
+
+    This file is part of MediaPortal 2
+
+    MediaPortal 2 is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    MediaPortal 2 is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with MediaPortal 2. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#endregion
+
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.FileProviders;
+using Microsoft.AspNet.Http;
+using Microsoft.AspNet.StaticFiles;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.PlatformAbstractions;
+
+namespace MediaPortal.Plugins.AspNetServer
+{
+  public class Startup
+  {
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddSingleton<IApplicationEnvironment>(new MP2ApplicationEnvironment());
+    }
+
+    public void Configure(IApplicationBuilder app)
+    {
+      app.UseFileServer(new FileServerOptions
+      {
+        FileProvider = new PhysicalFileProvider(@"C:\"),
+        RequestPath = new PathString("/StaticFiles"),
+        EnableDirectoryBrowsing = true,
+      });
+      app.Run(context => context.Response.WriteAsync("Hello World"));
+    }
+  }
+
+}
