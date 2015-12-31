e0f739ee098f01a9377e19456afdfc5433ede9f6
MrTechno <MrTechno@users.noreply.github.com>
2015-12-31 10:37:05 +0000

    VersionInfo change

--- MediaPortal/Source/Core/MediaPortal.Common/VersionInfo/VersionInfo.cs
+++ MediaPortal/Source/Core/MediaPortal.Common/VersionInfo/VersionInfo.cs
@@ -7,7 +7,7 @@ using System.Reflection;
 [assembly: AssemblyProduct("MediaPortal 2")]
 [assembly: AssemblyCopyright("Copyright © Team MediaPortal 2007 - 2015")]
 // Note: Following two line will be modified by AssemblyInfoHelper in build process. The last 4 digits will be replaced by "YYMM" (year-month)
-[assembly: AssemblyVersion("2.0.0.1509")]
-[assembly: AssemblyFileVersion("2.0.0.1509")]
+[assembly: AssemblyVersion("2.0.0.1511")]
+[assembly: AssemblyFileVersion("2.0.0.1511")]
 // Note: This line will be filled by AssemblyInfoHelper in build process.
-[assembly: AssemblyInformationalVersion("origin/EXP_MIA_Rework_SR15U1-547103")]
+[assembly: AssemblyInformationalVersion("origin/FEAT_MIA_Rework-43c1f0")]
