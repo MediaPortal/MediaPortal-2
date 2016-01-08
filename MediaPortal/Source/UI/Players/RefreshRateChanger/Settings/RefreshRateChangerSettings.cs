ce1a95f32bb74a0527c1e54dbba17384a9b5a669
morpheusxx <morpheus_xx@team-mediaportal.com>
2016-01-08 11:15:00 +0100

    Added setting to override fps/hz mappings

--- MediaPortal/Source/UI/Players/RefreshRateChanger/Settings/RefreshRateChangerSettings.cs
+++ MediaPortal/Source/UI/Players/RefreshRateChanger/Settings/RefreshRateChangerSettings.cs
@@ -23,11 +23,29 @@
 #endregion
 
 using MediaPortal.Common.Settings;
+using MediaPortal.Utilities.Xml;
 
 namespace MediaPortal.Plugins.RefreshRateChanger.Settings
 {
   public class RefreshRateChangerSettings
   {
+    public static SerializableDictionary<int, int> DEFAULT_MAPPINGS = new SerializableDictionary<int, int>
+    {
+      { 23, 23 }, // CINEMA
+      { 24, 24 }, // CINEMA24
+      { 25, 50 }, // PAL
+      { 50, 50 }, // PALHD
+      { 29, 59 }, // NTSC
+      { 59, 59 }, // NTSCHD
+      { 30, 60 }, // ATSC
+      { 60, 60 }  // ATSCHD
+    };
+
+    public RefreshRateChangerSettings()
+    {
+      RateMappings = DEFAULT_MAPPINGS;
+    }
+
     /// <summary>
     /// IsEnabled or disable refresh rate changes
     /// </summary>
@@ -39,5 +57,11 @@ namespace MediaPortal.Plugins.RefreshRateChanger.Settings
     /// </summary>
     [Setting(SettingScope.User, "")]
     public string NoChangeForRate { get; set; }
+
+    /// <summary>
+    /// Contains a custom mapping of refresh rates. This can be used to override the default mappings.
+    /// </summary>
+    [Setting(SettingScope.User)]
+    public SerializableDictionary<int, int> RateMappings { get; set; }
   }
 }
