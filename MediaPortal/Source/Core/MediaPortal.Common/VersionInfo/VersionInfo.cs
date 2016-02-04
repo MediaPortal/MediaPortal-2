0ee583107fa3136c0d32769df237ee7b12163c99
MrTechno <MrTechno@users.noreply.github.com>
2016-02-04 23:17:50 +0000

    Updated version info

--- MediaPortal/Source/Core/MediaPortal.Common/VersionInfo/VersionInfo.cs
+++ MediaPortal/Source/Core/MediaPortal.Common/VersionInfo/VersionInfo.cs
@@ -1,13 +1,13 @@
-﻿using System.Reflection;
+using System.Reflection;
 
 // This file provides common information about assembly company, copyright and version number. It should be referenced
 // in any MP2 project (as link). The existing AssemblyInfo.cs files must be edited, the duplicated attributes need to 
 // be deleted.
 [assembly: AssemblyCompany("Team MediaPortal")]
 [assembly: AssemblyProduct("MediaPortal 2")]
-[assembly: AssemblyCopyright("Copyright © Team MediaPortal 2007 - 2015")]
+[assembly: AssemblyCopyright("Copyright © Team MediaPortal 2007 - 2016")]
 // Note: Following two line will be modified by AssemblyInfoHelper in build process. The last 4 digits will be replaced by "YYMM" (year-month)
-[assembly: AssemblyVersion("2.0.0.1511")]
-[assembly: AssemblyFileVersion("2.0.0.1511")]
+[assembly: AssemblyVersion("2.0.0.1601")]
+[assembly: AssemblyFileVersion("2.0.0.1601")]
 // Note: This line will be filled by AssemblyInfoHelper in build process.
-[assembly: AssemblyInformationalVersion("origin/MIA_DLNA_MPExtended-7a4a93")]
+[assembly: AssemblyInformationalVersion("origin/FEAT_MIA_DLNA_MP2Ext-41d44a")]
