3a47f89005a940c87176f7ea05438b6f51019e5e
brownard <brownard@live.co.uk>
2016-06-04 22:11:32 +0100

    Implement skin settings and add 'EnableFanart' setting

---
+++ MediaPortal/Source/UI/Skins/WMC/Actions/SkinSettingsAction.cs
@@ -0,0 +1,27 @@
+﻿using MediaPortal.UI.Presentation.Workflow;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediaPortal.Common.Localization;
+using MediaPortal.Common;
+using HomeEditor.Actions;
+
+namespace MediaPortal.UiComponents.WMCSkin.Actions
+{
+  public class SkinSettingsAction : AbstractConfigurationAction
+  {
+    public const string CONFIG_LOCATION = "/Appearance/Skin/SkinSettings";
+
+    public override IResourceString DisplayTitle
+    {
+      get { return LocalizationHelper.CreateResourceString("[WMC.Configuration.SkinSettings]"); }
+    }
+
+    protected override string ConfigLocation
+    {
+      get { return CONFIG_LOCATION; }
+    }
+  }
+}
