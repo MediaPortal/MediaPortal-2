e86dd8f90163622945d8be856caf0e5c65b1b998
morpheusxx <morpheus_xx@team-mediaportal.com>
2016-07-25 19:17:45 +0200

    Use status monitor icon for log window

--- MediaPortal/Incubator/Diagnostics/Service/FormLogMonitor.Designer.cs
+++ MediaPortal/Incubator/Diagnostics/Service/FormLogMonitor.Designer.cs
@@ -28,28 +28,30 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.tabControlContainer = new System.Windows.Forms.TabControl();
-            this.SuspendLayout();
-            // 
-            // tabControlContainer
-            // 
-            this.tabControlContainer.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tabControlContainer.Location = new System.Drawing.Point(0, 0);
-            this.tabControlContainer.Name = "tabControlContainer";
-            this.tabControlContainer.SelectedIndex = 0;
-            this.tabControlContainer.Size = new System.Drawing.Size(394, 350);
-            this.tabControlContainer.TabIndex = 1;
-            // 
-            // FormLogMonitor
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(394, 350);
-            this.Controls.Add(this.tabControlContainer);
-            this.Name = "FormLogMonitor";
-            this.Text = "Logs";
-            this.Load += new System.EventHandler(this.FormLogMonitor_Load);
-            this.ResumeLayout(false);
+      System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormLogMonitor));
+      this.tabControlContainer = new System.Windows.Forms.TabControl();
+      this.SuspendLayout();
+      // 
+      // tabControlContainer
+      // 
+      this.tabControlContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+      this.tabControlContainer.Location = new System.Drawing.Point(0, 0);
+      this.tabControlContainer.Name = "tabControlContainer";
+      this.tabControlContainer.SelectedIndex = 0;
+      this.tabControlContainer.Size = new System.Drawing.Size(394, 350);
+      this.tabControlContainer.TabIndex = 1;
+      // 
+      // FormLogMonitor
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(394, 350);
+      this.Controls.Add(this.tabControlContainer);
+      this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+      this.Name = "FormLogMonitor";
+      this.Text = "Logs";
+      this.Load += new System.EventHandler(this.FormLogMonitor_Load);
+      this.ResumeLayout(false);
 
         }
 
