f337b9b4ad3f0725854caedc9b15ad3208dd67cf
brownard <brownard@live.co.uk>
2016-08-02 23:00:19 +0100

    Use invariant culture to parse double

--- MediaPortal/Source/UI/TV/SlimTvClient/Controls/SlimTvGuideTimeFormatConverter.cs
+++ MediaPortal/Source/UI/TV/SlimTvClient/Controls/SlimTvGuideTimeFormatConverter.cs
@@ -3,6 +3,7 @@ using MediaPortal.UI.SkinEngine.MarkupExtensions;
 using MediaPortal.UI.SkinEngine.Xaml;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,7 +17,8 @@ namespace MediaPortal.Plugins.SlimTv.Client.Controls
       result = null;
       DateTime dtVal = (DateTime)values[0].Value;
       double durationPerc;
-      if (double.TryParse(parameter as string, out durationPerc) && values.Length > 1 && values[1].Value is double)
+      if (double.TryParse(parameter as string, NumberStyles.Any, CultureInfo.InvariantCulture, out durationPerc)
+        && values.Length > 1 && values[1].Value is double)
         dtVal = dtVal.AddHours((double)values[1].Value * durationPerc);
       result = dtVal.FormatProgramTime();
       return true;
