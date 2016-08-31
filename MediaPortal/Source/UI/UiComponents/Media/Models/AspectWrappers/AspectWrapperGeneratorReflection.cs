c3f74bbd10df6d179229764800055583a94a8910
offbyoneBB <bborgsdorf@gmail.com>
2016-08-31 13:06:06 +0200

    added t4 templates to generate the aspect wrappers one that uses reflection (as implemented already in the external tool) one (unfinished) that uses the CodeModel

---
+++ MediaPortal/Source/UI/UiComponents/Media/Models/AspectWrappers/AspectWrapperGeneratorReflection.cs
@@ -0,0 +1,23 @@
+﻿
+
+// Generated helper templates
+// C:\Code\MediaPortal\MediaPortal-2\MediaPortal\Source\UI\TV\SlimTvClient\Models\AspectWrappers\AspectWrapperGeneratorReflection.txt4
+// Generated items
+// C:\Code\MediaPortal\MediaPortal-2\MediaPortal\Source\UI\TV\SlimTvClient\Models\AspectWrappers\RecordingAspectWrapper.cs
+// C:\Code\MediaPortal\MediaPortal-2\MediaPortal\Source\UI\UiComponents\Media\Models\AspectWrappers\SubtitleAspectWrapper.cs
+// C:\Code\MediaPortal\MediaPortal-2\MediaPortal\Source\UI\UiComponents\Media\Models\AspectWrappers\CompanyAspectWrapper.cs
+// C:\Code\MediaPortal\MediaPortal-2\MediaPortal\Source\UI\UiComponents\Media\Models\AspectWrappers\CharacterAspectWrapper.cs
+// C:\Code\MediaPortal\MediaPortal-2\MediaPortal\Source\UI\UiComponents\Media\Models\AspectWrappers\PersonAspectWrapper.cs
+// C:\Code\MediaPortal\MediaPortal-2\MediaPortal\Source\UI\UiComponents\Media\Models\AspectWrappers\EpisodeAspectWrapper.cs
+// C:\Code\MediaPortal\MediaPortal-2\MediaPortal\Source\UI\UiComponents\Media\Models\AspectWrappers\SeasonAspectWrapper.cs
+// C:\Code\MediaPortal\MediaPortal-2\MediaPortal\Source\UI\UiComponents\Media\Models\AspectWrappers\SeriesAspectWrapper.cs
+// C:\Code\MediaPortal\MediaPortal-2\MediaPortal\Source\UI\UiComponents\Media\Models\AspectWrappers\MovieCollectionAspectWrapper.cs
+// C:\Code\MediaPortal\MediaPortal-2\MediaPortal\Source\UI\UiComponents\Media\Models\AspectWrappers\MovieAspectWrapper.cs
+// C:\Code\MediaPortal\MediaPortal-2\MediaPortal\Source\UI\UiComponents\Media\Models\AspectWrappers\ImageAspectWrapper.cs
+// C:\Code\MediaPortal\MediaPortal-2\MediaPortal\Source\UI\UiComponents\Media\Models\AspectWrappers\AudioAlbumAspectWrapper.cs
+// C:\Code\MediaPortal\MediaPortal-2\MediaPortal\Source\UI\UiComponents\Media\Models\AspectWrappers\AudioAspectWrapper.cs
+// C:\Code\MediaPortal\MediaPortal-2\MediaPortal\Source\UI\UiComponents\Media\Models\AspectWrappers\VideoAudioStreamAspectWrapper.cs
+// C:\Code\MediaPortal\MediaPortal-2\MediaPortal\Source\UI\UiComponents\Media\Models\AspectWrappers\VideoStreamAspectWrapper.cs
+// C:\Code\MediaPortal\MediaPortal-2\MediaPortal\Source\UI\UiComponents\Media\Models\AspectWrappers\VideoAspectWrapper.cs
+// C:\Code\MediaPortal\MediaPortal-2\MediaPortal\Source\UI\UiComponents\Media\Models\AspectWrappers\ImporterAspectWrapper.cs
+// C:\Code\MediaPortal\MediaPortal-2\MediaPortal\Source\UI\UiComponents\Media\Models\AspectWrappers\MediaAspectWrapper.cs
--- MediaPortal/Source/UI/UiComponents/Media/Models/AspectWrappers/SeasonAspectWrapper.cs
+++ MediaPortal/Source/UI/UiComponents/Media/Models/AspectWrappers/SeasonAspectWrapper.cs
@@ -1,4 +1,4 @@
-#region Copyright (C) 2007-2015 Team MediaPortal
+﻿#region Copyright (C) 2007-2015 Team MediaPortal
 
 /*
     Copyright (C) 2007-2015 Team MediaPortal
