dd19236097361dad53ed5c9a5b87f121d3b4c675
brownard <brownard@live.co.uk>
2016-11-07 20:17:13 +0000

    Add MultiAspectAttributeConverter with example usage in MediaItemListMovieDetails.inc

---
+++ MediaPortal/Source/UI/SkinEngine/MpfElements/Converters/MultiAspectAttributeConverter.cs
@@ -0,0 +1,44 @@
+﻿using MediaPortal.Common.MediaManagement;
+using MediaPortal.UI.SkinEngine.MarkupExtensions;
+using MediaPortal.UI.SkinEngine.Xaml;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MediaPortal.UI.SkinEngine.MpfElements.Converters
+{
+  public class MultiAspectAttributeConverter : IMultiValueConverter
+  {
+    protected const string DEFAULT_SEPARATOR = ", ";
+     
+    public bool Convert(IDataDescriptor[] values, Type targetType, object parameter, out object result)
+    {
+      result = null;
+      if (values.Length < 2)
+        return false;
+
+      MediaItem mi = values[0].Value as MediaItem;
+      if (mi == null)
+        return false;
+      MediaItemAspectMetadata.MultipleAttributeSpecification mas = values[1].Value as MediaItemAspectMetadata.MultipleAttributeSpecification;
+      if (mas == null)
+        return false;
+      List<object> results;
+      if (!MediaItemAspect.TryGetAttribute(mi.Aspects, mas, out results) || results.Count == 0)
+        return false;
+
+      string separator = parameter as string;
+      if (string.IsNullOrEmpty(separator))
+        separator = DEFAULT_SEPARATOR;
+
+      StringBuilder sb = new StringBuilder(results[0].ToString());
+      for (int i = 1; i < results.Count; i++)
+      {
+        sb.Append(separator);
+        sb.Append(results[i]);
+      }
+      result = sb.ToString();
+      return true;
+    }
+  }
+}
