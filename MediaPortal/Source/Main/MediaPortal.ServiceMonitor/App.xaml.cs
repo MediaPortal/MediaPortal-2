87b974dfbff98b5587b5b002c30121e7277ce28b
osre <osre@aarpg.net>
2016-12-12 01:13:45 +0100

    Initial implementation - IpcServer to add in every application or service - IpcClient to connect to the IpcServer - added IpcServer to MP2 Server, Client, ClientLauncher and ServiceMonitor - added custom action to installer to shutdown all running MP2 processes.   The action is currently not active. The entries in MediaPortal2.wxs and CustomActions.wxs are added, but are currently commented and need to be checkt to be correct as well as tested

--- MediaPortal/Source/Main/MediaPortal.ServiceMonitor/App.xaml.cs
+++ MediaPortal/Source/Main/MediaPortal.ServiceMonitor/App.xaml.cs
@@ -63,6 +63,10 @@ namespace MediaPortal.ServiceMonitor
 
     #endregion
 
+    #region fileds
+    private static IpcServer _ipcServer;
+    #endregion
+
     #region OnStartUp
 
     /// <summary>
@@ -141,6 +145,8 @@ namespace MediaPortal.ServiceMonitor
           throw;
         }
 
+        InitIpc();
+
         var appController = new AppController();
         ServiceRegistration.Set<IAppController>(appController);
 
@@ -187,10 +193,50 @@ namespace MediaPortal.ServiceMonitor
       ApplicationCore.DisposeCoreServices();
       systemStateService.SwitchSystemState(SystemState.Ending, false);
 
+      CloseIpc();
+
       if (_mutex != null)
         _mutex.ReleaseMutex();
     }
 
+    #endregion
+
+    #region IPC
+    private void InitIpc()
+    {
+      if (_ipcServer != null)
+        return;
+      ServiceRegistration.Get<ILogger>().Debug("Initializing IPC");
+      try
+      {
+        _ipcServer = new IpcServer("ServiceMonitor");
+        _ipcServer.CustomShutdownCallback = () =>
+        {
+          Dispatcher.BeginInvoke(new Action(Shutdown));
+          return true;
+        };
+        _ipcServer.Open();
+      }
+      catch (Exception ex)
+      {
+        ServiceRegistration.Get<ILogger>().Error(ex);
+      }
+    }
+
+    private void CloseIpc()
+    {
+      if (_ipcServer == null)
+        return;
+      try
+      {
+        _ipcServer.Close();
+      }
+      catch (Exception ex)
+      {
+        ServiceRegistration.Get<ILogger>().Error(ex);
+      }
+      _ipcServer = null;
+    }
     #endregion
 
     #region Unhandled Exceptions
