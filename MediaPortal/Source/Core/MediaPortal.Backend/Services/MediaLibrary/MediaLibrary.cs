3431a88fec2fac177e28b0b922f0b91b642d79dc
morpheusxx <morpheus_xx@team-mediaportal.com>
2017-02-14 17:43:35 +0100

    MP2-604: More sanity checks for accessing resource path locks

--- MediaPortal/Source/Core/MediaPortal.Backend/Services/MediaLibrary/MediaLibrary.cs
+++ MediaPortal/Source/Core/MediaPortal.Backend/Services/MediaLibrary/MediaLibrary.cs
@@ -149,9 +149,7 @@ namespace MediaPortal.Backend.Services.MediaLibrary
       {
         try
         {
-          if(!_parent._shareDeleteSync.ContainsKey(path))
-            _parent._shareDeleteSync.Add(path, new object());
-          lock (_parent._shareDeleteSync[basePath])
+          lock (_parent.GetResourcePathLock(basePath))
           {
             return _parent.AddOrUpdateMediaItem(parentDirectoryId, _parent.LocalSystemId, path, null, updatedAspects, true, isRefresh, cancelToken);
           }
@@ -321,7 +319,8 @@ namespace MediaPortal.Backend.Services.MediaLibrary
     protected AsynchronousMessageQueue _messageQueue;
     protected bool _shutdown = false;
     protected readonly Dictionary<Guid, ShareWatcher> _shareWatchers = new Dictionary<Guid, ShareWatcher>();
-    protected Dictionary<ResourcePath, object> _shareDeleteSync = new Dictionary<ResourcePath, object>();
+    // Should be accessed only by GetResourcePathLock
+    private readonly Dictionary<ResourcePath, object> _shareDeleteSync = new Dictionary<ResourcePath, object>();
     protected List<RelationshipHierarchy> _hierarchies = new List<RelationshipHierarchy>();
     protected object _shareImportSync = new object();
     protected Dictionary<Guid, ShareImportState> _shareImportStates = new Dictionary<Guid, ShareImportState>();
@@ -492,6 +491,16 @@ namespace MediaPortal.Backend.Services.MediaLibrary
     }
 
     #region Protected methods
+
+    protected object GetResourcePathLock(ResourcePath path)
+    {
+      lock (_syncObj)
+      {
+        if (!_shareDeleteSync.ContainsKey(path))
+          _shareDeleteSync.Add(path, new object());
+        return _shareDeleteSync[path];
+      }
+    }
 
     protected MediaItemQuery BuildLoadItemQuery(string systemId, ResourcePath path)
     {
@@ -2995,9 +3004,6 @@ namespace MediaPortal.Backend.Services.MediaLibrary
         IDictionary<Guid, Share> shares = GetShares(_localSystemId);
         foreach (Share share in shares.Values)
         {
-          // Needs to be created for all shares
-          _shareDeleteSync[share.BaseResourcePath]= new object();
-
           if (!share.UseShareWatcher)
           {
             Logger.Info("MediaLibrary: Share watcher not enabled for path {0}", share.BaseResourcePath);
@@ -3087,7 +3093,6 @@ namespace MediaPortal.Backend.Services.MediaLibrary
             ShareWatcher watcher = new ShareWatcher(share, this, false);
             _shareWatchers.Add(share.ShareId, watcher);
           }
-        _shareDeleteSync.Add(share.BaseResourcePath, new object());
       }
       catch (Exception e)
       {
@@ -3113,9 +3118,7 @@ namespace MediaPortal.Backend.Services.MediaLibrary
       Share share = GetShare(shareId);
       TryCancelLocalImportJobs(share);
 
-      if (!_shareDeleteSync.ContainsKey(share.BaseResourcePath))
-        _shareDeleteSync.Add(share.BaseResourcePath, new object());
-      lock (_shareDeleteSync[share.BaseResourcePath])
+      lock (GetResourcePathLock(share.BaseResourcePath))
       {
         ISQLDatabase database = ServiceRegistration.Get<ISQLDatabase>();
         ITransaction transaction = database.BeginTransaction();
@@ -3144,7 +3147,8 @@ namespace MediaPortal.Backend.Services.MediaLibrary
           throw;
         }
       }
-      _shareDeleteSync.Remove(share.BaseResourcePath);
+      lock (_syncObj)
+        _shareDeleteSync.Remove(share.BaseResourcePath);
     }
 
     public void RemoveSharesOfSystem(string systemId)
@@ -3172,8 +3176,8 @@ namespace MediaPortal.Backend.Services.MediaLibrary
             _shareWatchers[shareId].Dispose();
           }
           _shareWatchers.Clear();
+          _shareDeleteSync.Clear();
         }
-        _shareDeleteSync.Clear();
 
         ContentDirectoryMessaging.SendRegisteredSharesChangedMessage();
       }
