4d8712c983b6aee0607b9f784c34b6afb97ed542
brownard <brownard@live.co.uk>
2017-08-31 01:27:41 +0100

    MP2-639: Add query markup extensions to allow binding the result of a media item query to properties in xaml

---
+++ MediaPortal/Source/UI/SkinEngine/MarkupExtensions/MediaItemQueryExtension.cs
@@ -0,0 +1,239 @@
+﻿#region Copyright (C) 2007-2017 Team MediaPortal
+
+/*
+    Copyright (C) 2007-2017 Team MediaPortal
+    http://www.team-mediaportal.com
+
+    This file is part of MediaPortal 2
+
+    MediaPortal 2 is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    MediaPortal 2 is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with MediaPortal 2. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#endregion
+
+using MediaPortal.Common;
+using MediaPortal.Common.General;
+using MediaPortal.Common.MediaManagement;
+using MediaPortal.Common.MediaManagement.MLQueries;
+using MediaPortal.Common.SystemCommunication;
+using MediaPortal.Common.Threading;
+using MediaPortal.UI.ServerCommunication;
+using MediaPortal.UI.Services.UserManagement;
+using MediaPortal.UI.SkinEngine.Xaml;
+using MediaPortal.Utilities.DeepCopy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MediaPortal.UI.SkinEngine.MarkupExtensions
+{
+  /// <summary>
+  /// Markup extension which performs a media item query and
+  /// updates the target property with the returned <see cref="MediaItem"/>(s).
+  /// </summary>
+  public class MediaItemQueryExtension : BindingBase
+  {
+    #region Protected fields
+    
+    protected readonly object _syncObj = new object();
+    //Whether we are currently performing a query
+    protected bool _isUpdating = false;
+    //Whether state has changed whilst performing a query -> perform new query when current query has finished
+    protected bool _isDirty = false;
+
+    protected AbstractProperty _necessaryRequestedMIAsProperty;
+    protected AbstractProperty _optionalRequestedMIAsProperty;
+    protected AbstractProperty _filterProperty;
+
+    #endregion
+
+    #region Ctor/Init
+
+    public MediaItemQueryExtension()
+    {
+      Init();
+      Attach();
+    }
+
+    void Init()
+    {
+      _necessaryRequestedMIAsProperty = new SProperty(typeof(IEnumerable<Guid>), null);
+      _optionalRequestedMIAsProperty = new SProperty(typeof(IEnumerable<Guid>), null);
+      _filterProperty = new SProperty(typeof(IFilter), null);
+    }
+
+    void Attach()
+    {
+      _filterProperty.Attach(OnFilterChanged);
+    }
+
+    void Detach()
+    {
+      _filterProperty.Detach(OnFilterChanged);
+    }
+
+    #endregion
+
+    #region Event handlers
+
+    protected void OnFilterChanged(AbstractProperty property, object oldValue)
+    {
+      if (_active)
+        UpdateAsync();
+    }
+
+    #endregion
+
+    #region Protected methods
+
+    //Asynchronously performs a query using the specified filter
+    protected void UpdateAsync()
+    {
+      //Check state is valid before invoking a thread pool thread
+      if (Filter == null)
+        return;
+      //Update using the thread pool
+      IThreadPool tp = ServiceRegistration.Get<IThreadPool>();
+      tp.Add(Update);
+    }
+
+
+    protected void Update()
+    {
+      lock (_syncObj)
+      {
+        if (_isUpdating)
+        {
+          //If we are currently querying, mark as dirty so
+          //we can re-update when the current query has finished
+          _isDirty = true;
+          return;
+        }
+        _isUpdating = true;
+      }
+
+      bool isDirty;
+      try
+      {
+        QueryMediaItems();
+      }
+      finally
+      {
+        lock (_syncObj)
+        {
+          //reset inside lock
+          isDirty = _isDirty;
+          _isDirty = false;
+          _isUpdating = false;
+        }
+      }
+
+      //update outside lock
+      if (isDirty)
+        Update();
+    }
+
+    //Performs the actual query and updates the target property with the
+    //returned media item(s)
+    protected void QueryMediaItems()
+    {
+      IFilter filter = Filter;
+      if (filter == null)
+        return;
+
+      IContentDirectory cd = ServiceRegistration.Get<IServerConnectionManager>().ContentDirectory;
+      if (cd == null)
+        return;
+
+      MediaItemQuery query = new MediaItemQuery(NecessaryRequestedMIAs, OptionalRequestedMIAs, filter);
+      IList<MediaItem> items = cd.Search(query, true, GetCurrentUserId(), false);
+
+      object result;
+      if (_targetDataDescriptor != null)
+      {
+        //Try and update our target property with either a media item enumeration or the first media item
+        if (TypeConverter.Convert(items, _targetDataDescriptor.DataType, out result) ||
+          (items != null && TypeConverter.Convert(items.FirstOrDefault(), _targetDataDescriptor.DataType, out result)))
+          _targetDataDescriptor.Value = result;
+      }
+    }
+
+    protected Guid? GetCurrentUserId()
+    {
+      IUserManagement userProfileDataManagement = ServiceRegistration.Get<IUserManagement>();
+      return userProfileDataManagement != null && userProfileDataManagement.IsValidUser ?
+        userProfileDataManagement.CurrentUser.ProfileId : (Guid?)null;
+    }
+
+    #endregion
+
+    #region Base overrides
+
+    public override void DeepCopy(IDeepCopyable source, ICopyManager copyManager)
+    {
+      Detach();
+      base.DeepCopy(source, copyManager);
+      MediaItemQueryExtension miqe = (MediaItemQueryExtension)source;
+      NecessaryRequestedMIAs = miqe.NecessaryRequestedMIAs;
+      OptionalRequestedMIAs = miqe.OptionalRequestedMIAs;
+      Filter = miqe.Filter;
+      Attach();
+    }
+
+    public override void Dispose()
+    {
+      base.Dispose();
+      Detach();
+    }
+
+    #endregion
+
+    #region GUI Properties
+
+    public AbstractProperty NecessaryRequestedMIAsProperty
+    {
+      get { return _necessaryRequestedMIAsProperty; }
+    }
+
+    public IEnumerable<Guid> NecessaryRequestedMIAs
+    {
+      get { return (IEnumerable<Guid>)_necessaryRequestedMIAsProperty.GetValue(); }
+      set { _necessaryRequestedMIAsProperty.SetValue(value); }
+    }
+
+    public AbstractProperty OptionalRequestedMIAsProperty
+    {
+      get { return _optionalRequestedMIAsProperty; }
+    }
+
+    public IEnumerable<Guid> OptionalRequestedMIAs
+    {
+      get { return (IEnumerable<Guid>)_optionalRequestedMIAsProperty.GetValue(); }
+      set { _optionalRequestedMIAsProperty.SetValue(value); }
+    }
+
+    public AbstractProperty FilterProperty
+    {
+      get { return _filterProperty; }
+    }
+
+    public IFilter Filter
+    {
+      get { return (IFilter)_filterProperty.GetValue(); }
+      set { _filterProperty.SetValue(value); }
+    }
+
+    #endregion
+  }
+}
---
+++ MediaPortal/Source/UI/SkinEngine/MarkupExtensions/RelationshipQueryExtension.cs
@@ -0,0 +1,161 @@
+﻿#region Copyright (C) 2007-2017 Team MediaPortal
+
+/*
+    Copyright (C) 2007-2017 Team MediaPortal
+    http://www.team-mediaportal.com
+
+    This file is part of MediaPortal 2
+
+    MediaPortal 2 is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    MediaPortal 2 is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with MediaPortal 2. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#endregion
+
+using MediaPortal.Common.General;
+using MediaPortal.Common.MediaManagement.MLQueries;
+using MediaPortal.Utilities.DeepCopy;
+using System;
+
+namespace MediaPortal.UI.SkinEngine.MarkupExtensions
+{
+  /// <summary>
+  /// Markup extension which performs a media item query using a <see cref="RelationshipFilter"/> and
+  /// updates the target property with the returned <see cref="Common.MediaManagement.MediaItem"/>(s).
+  /// </summary>
+  public class RelationshipQueryExtension : MediaItemQueryExtension
+  {
+    #region Protected fields
+
+    protected AbstractProperty _roleProperty;
+    protected AbstractProperty _linkedRoleProperty;
+    protected AbstractProperty _linkedMediaItemIdProperty;
+
+    #endregion
+
+    #region Ctor/Init
+
+    public RelationshipQueryExtension()
+    {
+      Init();
+      Attach();
+    }
+
+    void Init()
+    {
+      _roleProperty = new SProperty(typeof(Guid?), null);
+      _linkedRoleProperty = new SProperty(typeof(Guid?), null);
+      _linkedMediaItemIdProperty = new SProperty(typeof(Guid?), null);
+    }
+
+    void Attach()
+    {
+      _roleProperty.Attach(OnPropertyChanged);
+      _linkedRoleProperty.Attach(OnPropertyChanged);
+      _linkedMediaItemIdProperty.Attach(OnPropertyChanged);
+    }
+
+    void Detach()
+    {
+      _roleProperty.Detach(OnPropertyChanged);
+      _linkedRoleProperty.Detach(OnPropertyChanged);
+      _linkedMediaItemIdProperty.Detach(OnPropertyChanged);
+    }
+
+    #endregion
+
+    #region Event handlers
+
+    protected void OnPropertyChanged(AbstractProperty property, object oldValue)
+    {
+      if (_active)
+        UpdateFilter();
+    }
+
+    #endregion
+
+    #region Protected methods
+
+    protected void UpdateFilter()
+    {
+      Guid? role = Role;
+      Guid? linkedRole = LinkedRole;
+      Guid? linkedMediaItemId = LinkedMediaItemId;
+      //If all properties are valid, create the RelationshipFilter
+      if (role.HasValue && linkedRole.HasValue && linkedMediaItemId.HasValue)
+        //Setting a valid filter causes the underlying MediaItemQueryExtension to
+        //perform the actual query
+        Filter = new RelationshipFilter(role.Value, linkedRole.Value, linkedMediaItemId.Value);
+    }
+
+    #endregion
+
+    #region Base overrides
+
+    public override void DeepCopy(IDeepCopyable source, ICopyManager copyManager)
+    {
+      Detach();
+      base.DeepCopy(source, copyManager);
+      RelationshipQueryExtension rqe = (RelationshipQueryExtension)source;
+      Role = rqe.Role;
+      LinkedRole = rqe.LinkedRole;
+      LinkedMediaItemId = rqe.LinkedMediaItemId;
+      Attach();
+    }
+
+    public override void Dispose()
+    {
+      base.Dispose();
+      Detach();
+    }
+
+    #endregion
+
+    #region GUI Properties
+
+    public AbstractProperty RoleProperty
+    {
+      get { return _roleProperty; }
+    }
+
+    public Guid? Role
+    {
+      get { return (Guid?)_roleProperty.GetValue(); }
+      set { _roleProperty.SetValue(value); }
+    }
+
+    public AbstractProperty LinkedRoleProperty
+    {
+      get { return _linkedRoleProperty; }
+    }
+
+    public Guid? LinkedRole
+    {
+      get { return (Guid?)_linkedRoleProperty.GetValue(); }
+      set { _linkedRoleProperty.SetValue(value); }
+    }
+
+    public AbstractProperty LinkedMediaItemIdProperty
+    {
+      get { return _linkedMediaItemIdProperty; }
+    }
+
+    public Guid? LinkedMediaItemId
+    {
+      get { return (Guid?)_linkedMediaItemIdProperty.GetValue(); }
+      set { _linkedMediaItemIdProperty.SetValue(value); }
+    }
+
+    #endregion
+  }
+}
