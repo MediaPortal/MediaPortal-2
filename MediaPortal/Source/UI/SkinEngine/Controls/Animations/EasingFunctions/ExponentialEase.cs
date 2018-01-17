291ff19144087f1e972f205f6ce7016dd8b8e8cf
brownard <brownard@live.co.uk>
2018-01-17 22:17:41 +0000

    MP2-728: Add easing functions and implement in animations

---
+++ MediaPortal/Source/UI/SkinEngine/Controls/Animations/EasingFunctions/ExponentialEase.cs
@@ -0,0 +1,93 @@
+﻿#region Copyright (C) 2007-2017 Team MediaPortal
+
+/*
+    Copyright (C) 2007-2017 Team MediaPortal
+    http://www.team-mediaportal.com
+
+    This file is part of MediaPortal 2
+
+    MediaPortal 2 is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    MediaPortal 2 is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with MediaPortal 2. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#endregion
+
+using MediaPortal.Common.General;
+using MediaPortal.UI.SkinEngine.Utils;
+using MediaPortal.Utilities.DeepCopy;
+using System;
+
+namespace MediaPortal.UI.SkinEngine.Controls.Animations.EasingFunctions
+{
+  /// <summary>
+  /// Easing function that gives an exponential curve.
+  /// </summary>
+  public class ExponentialEase : EasingFunctionBase
+  {
+    #region Protected fields
+
+    protected AbstractProperty _exponentProperty;
+
+    #endregion
+
+    #region Ctor
+
+    public ExponentialEase()
+    {
+      Init();
+    }
+
+    protected void Init()
+    {
+      _exponentProperty = new SProperty(typeof(double), 2.0);
+    }
+
+    public override void DeepCopy(IDeepCopyable source, ICopyManager copyManager)
+    {
+      base.DeepCopy(source, copyManager);
+      ExponentialEase e = (ExponentialEase)source;
+      Exponent = e.Exponent;
+    }
+
+    #endregion
+
+    #region Public properties
+
+    public AbstractProperty ExponentProperty
+    {
+      get { return _exponentProperty; }
+    }
+
+    /// <summary>
+    /// Gets or sets the exponent.
+    /// </summary>
+    public double Exponent
+    {
+      get { return (double)_exponentProperty.GetValue(); }
+      set { _exponentProperty.SetValue(value); }
+    }
+
+    #endregion
+
+    #region EasingFunctionBase overrides
+
+    protected override double EaseInCore(double normalizedTime)
+    {
+      double exponent = Exponent;
+      return DoubleUtils.IsZero(exponent) ?
+        normalizedTime : (Math.Exp(exponent * normalizedTime) - 1.0) / (Math.Exp(exponent) - 1.0);
+    }
+
+    #endregion
+  }
+}
---
+++ MediaPortal/Source/UI/SkinEngine/Utils/DoubleUtils.cs
@@ -0,0 +1,53 @@
+﻿#region Copyright (C) 2007-2017 Team MediaPortal
+
+/*
+    Copyright (C) 2007-2017 Team MediaPortal
+    http://www.team-mediaportal.com
+
+    This file is part of MediaPortal 2
+
+    MediaPortal 2 is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    MediaPortal 2 is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with MediaPortal 2. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#endregion
+
+using System;
+
+namespace MediaPortal.UI.SkinEngine.Utils
+{
+  public static class DoubleUtils
+  {
+    internal const double DBL_EPSILON = 2.2204460492503131e-016; //smallest value where 1 + DBLE_EPSILON != 1
+
+    /// <summary>
+    /// Returns whether the double is 'close' to zero.
+    /// </summary>
+    /// <param name="value">The double to compare.</param>
+    /// <returns>True if the double to compare is close to zero.</returns>
+    public static bool IsZero(double value)
+    {
+      return Math.Abs(value) < 10.0 * DBL_EPSILON;
+    }
+
+    /// <summary>
+    /// Returns whether the double is 'close' to one.
+    /// </summary>
+    /// <param name="value">The double to compare.</param>
+    /// <returns>True if the double to compare is close to one.</returns>
+    public static bool IsOne(double value)
+    {
+      return Math.Abs(value - 1.0) < 10.0 * DBL_EPSILON;
+    }
+  }
+}
