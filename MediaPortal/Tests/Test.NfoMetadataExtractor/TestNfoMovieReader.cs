9b5b5886abeffab87c048ef53f44796a77103679
brownard <brownard@live.co.uk>
2018-02-07 22:23:16 +0000

    MP2-726: Add more NfoMovieReader tests

--- MediaPortal/Tests/Test.NfoMetadataExtractor/TestNfoMovieReader.cs
+++ MediaPortal/Tests/Test.NfoMetadataExtractor/TestNfoMovieReader.cs
@@ -46,69 +46,6 @@ namespace Test.NfoMetadataExtractor
   public class TestNfoMovieReader
   {
     [Test]
-    public void TestNfoMovieReaderWriteMetadata()
-    {
-      MovieStub movieStub = CreateTestMovieStub();
-
-      IDictionary<Guid, IList<MediaItemAspect>> aspects = new Dictionary<Guid, IList<MediaItemAspect>>();
-
-      //Video only
-      NfoMovieReader reader = new NfoMovieReader(new ConsoleLogger(LogLevel.All, true), 1, true, false, false, null, new NfoMovieMetadataExtractorSettings());
-      reader.GetMovieStubs().Add(movieStub);
-      reader.TryWriteMetadata(aspects);
-
-      MediaItemAspect mediaAspect = MediaItemAspect.GetAspect(aspects, MediaAspect.Metadata);
-      Assert.NotNull(mediaAspect);
-      Assert.AreEqual(movieStub.Title, mediaAspect.GetAttributeValue<string>(MediaAspect.ATTR_TITLE));
-      Assert.AreEqual(movieStub.SortTitle, mediaAspect.GetAttributeValue<string>(MediaAspect.ATTR_SORT_TITLE));
-      Assert.AreEqual(movieStub.Premiered, mediaAspect.GetAttributeValue<DateTime?>(MediaAspect.ATTR_RECORDINGTIME));
-      Assert.AreEqual(movieStub.PlayCount, mediaAspect.GetAttributeValue<int?>(MediaAspect.ATTR_PLAYCOUNT));
-      Assert.AreEqual(movieStub.LastPlayed, mediaAspect.GetAttributeValue<DateTime?>(MediaAspect.ATTR_LASTPLAYED));
-
-      MediaItemAspect videoAspect = MediaItemAspect.GetAspect(aspects, VideoAspect.Metadata);
-      Assert.NotNull(videoAspect);
-      Assert.AreEqual(movieStub.Plot, videoAspect.GetAttributeValue<string>(VideoAspect.ATTR_STORYPLOT));
-      CollectionAssert.AreEqual(movieStub.Actors.OrderBy(p => p.Order).Select(p => p.Name), videoAspect.GetCollectionAttribute<string>(VideoAspect.ATTR_ACTORS));
-      Assert.AreEqual(movieStub.Director, videoAspect.GetCollectionAttribute<string>(VideoAspect.ATTR_DIRECTORS).First());
-      CollectionAssert.AreEqual(movieStub.Credits, videoAspect.GetCollectionAttribute<string>(VideoAspect.ATTR_WRITERS));
-
-      //ToDo: Rework Genre Mapper to make it testable, currently depends on IPathManager/language files!
-      //IList<MediaItemAspect> genreAspects;
-      //Assert.IsTrue(aspects.TryGetValue(GenreAspect.ASPECT_ID, out genreAspects));
-      //CollectionAssert.AreEqual(movieStub.Genres, genreAspects.Select(g => g.GetAttributeValue<string>(GenreAspect.ATTR_GENRE)));
-
-      MediaItemAspect thumbnailAspect = MediaItemAspect.GetAspect(aspects, ThumbnailLargeAspect.Metadata);
-      Assert.NotNull(thumbnailAspect);
-      CollectionAssert.AreEqual(movieStub.Thumb, thumbnailAspect.GetAttributeValue<byte[]>(ThumbnailLargeAspect.ATTR_THUMBNAIL));
-
-      //Movie only
-      aspects.Clear();
-      reader = new NfoMovieReader(new ConsoleLogger(LogLevel.All, true), 1, false, false, false, null, new NfoMovieMetadataExtractorSettings());
-      reader.GetMovieStubs().Add(movieStub);
-      reader.TryWriteMetadata(aspects);
-
-      MediaItemAspect movieAspect = MediaItemAspect.GetAspect(aspects, MovieAspect.Metadata);
-      Assert.NotNull(movieAspect);
-      CollectionAssert.AreEqual(movieStub.Companies, movieAspect.GetCollectionAttribute<string>(MovieAspect.ATTR_COMPANIES));
-      Assert.AreEqual(movieStub.Title, movieAspect.GetAttributeValue<string>(MovieAspect.ATTR_MOVIE_NAME));
-      Assert.AreEqual(movieStub.OriginalTitle, movieAspect.GetAttributeValue<string>(MovieAspect.ATTR_ORIG_MOVIE_NAME));
-      Assert.AreEqual(movieStub.Sets.OrderBy(set => set.Order).First().Name, movieAspect.GetAttributeValue<string>(MovieAspect.ATTR_COLLECTION_NAME));
-      Assert.AreEqual((int)movieStub.Runtime.Value.TotalMinutes, movieAspect.GetAttributeValue<int?>(MovieAspect.ATTR_RUNTIME_M));
-      Assert.AreEqual("US_PG", movieAspect.GetAttributeValue<string>(MovieAspect.ATTR_CERTIFICATION));
-      Assert.AreEqual(movieStub.Tagline, movieAspect.GetAttributeValue<string>(MovieAspect.ATTR_TAGLINE));
-      Assert.AreEqual((double)movieStub.Rating, movieAspect.GetAttributeValue<double?>(MovieAspect.ATTR_TOTAL_RATING));
-      Assert.AreEqual(movieStub.Votes, movieAspect.GetAttributeValue<int?>(MovieAspect.ATTR_RATING_COUNT));
-
-      IList<MediaItemAspect> externalIdentifiers;
-      Assert.IsTrue(aspects.TryGetValue(ExternalIdentifierAspect.ASPECT_ID, out externalIdentifiers));
-      Assert.IsTrue(TestUtils.HasExternalId(externalIdentifiers, ExternalIdentifierAspect.SOURCE_TMDB, ExternalIdentifierAspect.TYPE_MOVIE, movieStub.TmdbId.ToString()));
-      Assert.IsTrue(TestUtils.HasExternalId(externalIdentifiers, ExternalIdentifierAspect.SOURCE_TMDB, ExternalIdentifierAspect.TYPE_COLLECTION, movieStub.TmdbCollectionId.ToString()));
-      Assert.IsTrue(TestUtils.HasExternalId(externalIdentifiers, ExternalIdentifierAspect.SOURCE_IMDB, ExternalIdentifierAspect.TYPE_MOVIE, movieStub.Id));
-      Assert.IsTrue(TestUtils.HasExternalId(externalIdentifiers, ExternalIdentifierAspect.SOURCE_ALLOCINE, ExternalIdentifierAspect.TYPE_MOVIE, movieStub.Allocine.ToString()));
-      Assert.IsTrue(TestUtils.HasExternalId(externalIdentifiers, ExternalIdentifierAspect.SOURCE_CINEPASSION, ExternalIdentifierAspect.TYPE_MOVIE, movieStub.Cinepassion.ToString()));
-    }
-
-    [Test]
     public void TestNfoMovieReaderReadMetadata()
     {
       //Arrange
@@ -118,7 +55,7 @@ namespace Test.NfoMetadataExtractor
       ServiceRegistration.Set(mockMediaAccessor.Object);
       mockMediaAccessor.Setup(x => x.LocalResourceProviders).Returns(resourceProviders);
 
-      Mock <IFileSystemResourceAccessor> mockRA = new Mock<IFileSystemResourceAccessor>();
+      Mock<IFileSystemResourceAccessor> mockRA = new Mock<IFileSystemResourceAccessor>();
       mockRA.Setup(r => r.OpenReadAsync()).Returns(Task.FromResult(
         Assembly.GetExecutingAssembly().GetManifestResourceStream("Test.NfoMetadataExtractor.TestData.MovieNfo.movie.nfo")));
       mockRA.SetupGet(t => t.CanonicalLocalResourcePath).Returns(ResourcePath.BuildBaseProviderPath(LocalFsResourceProviderBase.LOCAL_FS_RESOURCE_PROVIDER_ID, @"TestData\MovieNfo\movie.nfo"));
@@ -156,36 +93,111 @@ namespace Test.NfoMetadataExtractor
       CollectionAssert.AreEqual(new[] { "English" }, stub.Languages);
     }
 
-    protected MovieStub CreateTestMovieStub()
+    [Test]
+    public void TestNfoMovieReaderWriteMetadata()
     {
-      HashSet<PersonStub> actors = new HashSet<PersonStub>(new[]
-      {
-        new PersonStub
-        {
-          Name = "Actor1",
-          Role = "Role1"
-        },
-        new PersonStub
-        {
-          Name = "Actor2",
-          Role = "Role2"
-      }});
+      //Arrange
+      MovieStub movieStub = CreateTestMovieStub(CreateTestActors());
+      NfoMovieReader readerVideoOnly = new NfoMovieReader(new ConsoleLogger(LogLevel.All, true), 1, true, false, false, null, new NfoMovieMetadataExtractorSettings());
+      readerVideoOnly.GetMovieStubs().Add(movieStub);
+      NfoMovieReader readerMovieOnly = new NfoMovieReader(new ConsoleLogger(LogLevel.All, true), 1, false, false, false, null, new NfoMovieMetadataExtractorSettings());
+      readerMovieOnly.GetMovieStubs().Add(movieStub);
 
-      HashSet<SetStub> sets = new HashSet<SetStub>(new[]
-      {
-        new SetStub
-        {
-          Name = "Set2",
-          Order = 2,
-          TmdbId = 5
-        },
-        new SetStub
-        {
-          Name = "Set1",
-          Order = 1,
-          TmdbId = 6
-      }});
+      //Act
+      IDictionary<Guid, IList<MediaItemAspect>> aspectsVideoOnly = new Dictionary<Guid, IList<MediaItemAspect>>();
+      readerVideoOnly.TryWriteMetadata(aspectsVideoOnly);
+      IDictionary<Guid, IList<MediaItemAspect>> aspectsMovieOnly = new Dictionary<Guid, IList<MediaItemAspect>>();
+      readerMovieOnly.TryWriteMetadata(aspectsMovieOnly);
 
+      //Assert
+
+      //Video aspects only
+      MediaItemAspect mediaAspect = MediaItemAspect.GetAspect(aspectsVideoOnly, MediaAspect.Metadata);
+      Assert.NotNull(mediaAspect);
+      Assert.AreEqual(movieStub.Title, mediaAspect.GetAttributeValue<string>(MediaAspect.ATTR_TITLE));
+      Assert.AreEqual(movieStub.SortTitle, mediaAspect.GetAttributeValue<string>(MediaAspect.ATTR_SORT_TITLE));
+      Assert.AreEqual(movieStub.Premiered, mediaAspect.GetAttributeValue<DateTime?>(MediaAspect.ATTR_RECORDINGTIME));
+      Assert.AreEqual(movieStub.PlayCount, mediaAspect.GetAttributeValue<int?>(MediaAspect.ATTR_PLAYCOUNT));
+      Assert.AreEqual(movieStub.LastPlayed, mediaAspect.GetAttributeValue<DateTime?>(MediaAspect.ATTR_LASTPLAYED));
+
+      MediaItemAspect videoAspect = MediaItemAspect.GetAspect(aspectsVideoOnly, VideoAspect.Metadata);
+      Assert.NotNull(videoAspect);
+      Assert.AreEqual(movieStub.Plot, videoAspect.GetAttributeValue<string>(VideoAspect.ATTR_STORYPLOT));
+      CollectionAssert.AreEqual(movieStub.Actors.OrderBy(p => p.Order).Select(p => p.Name), videoAspect.GetCollectionAttribute<string>(VideoAspect.ATTR_ACTORS));
+      Assert.AreEqual(movieStub.Director, videoAspect.GetCollectionAttribute<string>(VideoAspect.ATTR_DIRECTORS).First());
+      CollectionAssert.AreEqual(movieStub.Credits, videoAspect.GetCollectionAttribute<string>(VideoAspect.ATTR_WRITERS));
+
+      //ToDo: Rework Genre Mapper to make it testable, currently depends on IPathManager/language files!
+      //IList<MediaItemAspect> genreAspects;
+      //Assert.IsTrue(aspects.TryGetValue(GenreAspect.ASPECT_ID, out genreAspects));
+      //CollectionAssert.AreEqual(movieStub.Genres, genreAspects.Select(g => g.GetAttributeValue<string>(GenreAspect.ATTR_GENRE)));
+
+      MediaItemAspect thumbnailAspect = MediaItemAspect.GetAspect(aspectsVideoOnly, ThumbnailLargeAspect.Metadata);
+      Assert.NotNull(thumbnailAspect);
+      CollectionAssert.AreEqual(movieStub.Thumb, thumbnailAspect.GetAttributeValue<byte[]>(ThumbnailLargeAspect.ATTR_THUMBNAIL));
+
+      //Movie aspects only
+      MediaItemAspect movieAspect = MediaItemAspect.GetAspect(aspectsMovieOnly, MovieAspect.Metadata);
+      Assert.NotNull(movieAspect);
+      CollectionAssert.AreEqual(movieStub.Companies, movieAspect.GetCollectionAttribute<string>(MovieAspect.ATTR_COMPANIES));
+      Assert.AreEqual(movieStub.Title, movieAspect.GetAttributeValue<string>(MovieAspect.ATTR_MOVIE_NAME));
+      Assert.AreEqual(movieStub.OriginalTitle, movieAspect.GetAttributeValue<string>(MovieAspect.ATTR_ORIG_MOVIE_NAME));
+      Assert.AreEqual(movieStub.Sets.OrderBy(set => set.Order).First().Name, movieAspect.GetAttributeValue<string>(MovieAspect.ATTR_COLLECTION_NAME));
+      Assert.AreEqual((int)movieStub.Runtime.Value.TotalMinutes, movieAspect.GetAttributeValue<int?>(MovieAspect.ATTR_RUNTIME_M));
+      Assert.AreEqual("US_PG", movieAspect.GetAttributeValue<string>(MovieAspect.ATTR_CERTIFICATION));
+      Assert.AreEqual(movieStub.Tagline, movieAspect.GetAttributeValue<string>(MovieAspect.ATTR_TAGLINE));
+      Assert.AreEqual((double)movieStub.Rating, movieAspect.GetAttributeValue<double?>(MovieAspect.ATTR_TOTAL_RATING));
+      Assert.AreEqual(movieStub.Votes, movieAspect.GetAttributeValue<int?>(MovieAspect.ATTR_RATING_COUNT));
+
+      IList<MediaItemAspect> externalIdentifiers;
+      Assert.IsTrue(aspectsMovieOnly.TryGetValue(ExternalIdentifierAspect.ASPECT_ID, out externalIdentifiers));
+      Assert.IsTrue(TestUtils.HasExternalId(externalIdentifiers, ExternalIdentifierAspect.SOURCE_TMDB, ExternalIdentifierAspect.TYPE_MOVIE, movieStub.TmdbId.ToString()));
+      Assert.IsTrue(TestUtils.HasExternalId(externalIdentifiers, ExternalIdentifierAspect.SOURCE_TMDB, ExternalIdentifierAspect.TYPE_COLLECTION, movieStub.TmdbCollectionId.ToString()));
+      Assert.IsTrue(TestUtils.HasExternalId(externalIdentifiers, ExternalIdentifierAspect.SOURCE_IMDB, ExternalIdentifierAspect.TYPE_MOVIE, movieStub.Id));
+      Assert.IsTrue(TestUtils.HasExternalId(externalIdentifiers, ExternalIdentifierAspect.SOURCE_ALLOCINE, ExternalIdentifierAspect.TYPE_MOVIE, movieStub.Allocine.ToString()));
+      Assert.IsTrue(TestUtils.HasExternalId(externalIdentifiers, ExternalIdentifierAspect.SOURCE_CINEPASSION, ExternalIdentifierAspect.TYPE_MOVIE, movieStub.Cinepassion.ToString()));
+    }
+
+    [Test]
+    public void TestNfoMovieReaderWriteActorMetadata()
+    {
+      //Arrange
+      IList<PersonStub> actors = CreateTestActors();
+      MovieStub movieStub = CreateTestMovieStub(actors);
+      NfoMovieReader reader = new NfoMovieReader(new ConsoleLogger(LogLevel.All, true), 1, true, false, false, null, new NfoMovieMetadataExtractorSettings());
+      reader.GetMovieStubs().Add(movieStub);
+
+      //Act
+      IList<IDictionary<Guid, IList<MediaItemAspect>>> aspects = new List<IDictionary<Guid, IList<MediaItemAspect>>>();
+      reader.TryWriteActorMetadata(aspects);
+
+      //Assert
+      Assert.AreEqual(2, aspects.Count);
+      MediaItemAspect personAspect0 = MediaItemAspect.GetAspect(aspects[0], PersonAspect.Metadata);
+      Assert.NotNull(personAspect0);
+      Assert.AreEqual(actors[0].Name, personAspect0.GetAttributeValue<string>(PersonAspect.ATTR_PERSON_NAME));
+      Assert.AreEqual(actors[0].Biography, personAspect0.GetAttributeValue<string>(PersonAspect.ATTR_BIOGRAPHY));
+      Assert.AreEqual(actors[0].Birthdate, personAspect0.GetAttributeValue<DateTime?>(PersonAspect.ATTR_DATEOFBIRTH));
+      Assert.AreEqual(actors[0].Deathdate, personAspect0.GetAttributeValue<DateTime?>(PersonAspect.ATTR_DATEOFDEATH));
+      Assert.AreEqual(PersonAspect.OCCUPATION_ACTOR, personAspect0.GetAttributeValue<string>(PersonAspect.ATTR_OCCUPATION));
+      IList<MediaItemAspect> externalIdentifiers0;
+      Assert.IsTrue(aspects[0].TryGetValue(ExternalIdentifierAspect.ASPECT_ID, out externalIdentifiers0));
+      Assert.IsTrue(TestUtils.HasExternalId(externalIdentifiers0, ExternalIdentifierAspect.SOURCE_IMDB, ExternalIdentifierAspect.TYPE_PERSON, actors[0].ImdbId));
+
+      MediaItemAspect personAspect1 = MediaItemAspect.GetAspect(aspects[1], PersonAspect.Metadata);
+      Assert.NotNull(personAspect1);
+      Assert.AreEqual(actors[1].Name, personAspect1.GetAttributeValue<string>(PersonAspect.ATTR_PERSON_NAME));
+      Assert.AreEqual(actors[1].Biography, personAspect1.GetAttributeValue<string>(PersonAspect.ATTR_BIOGRAPHY));
+      Assert.AreEqual(actors[1].Birthdate, personAspect1.GetAttributeValue<DateTime?>(PersonAspect.ATTR_DATEOFBIRTH));
+      Assert.AreEqual(actors[1].Deathdate, personAspect1.GetAttributeValue<DateTime?>(PersonAspect.ATTR_DATEOFDEATH));
+      Assert.AreEqual(PersonAspect.OCCUPATION_ACTOR, personAspect1.GetAttributeValue<string>(PersonAspect.ATTR_OCCUPATION));
+      IList<MediaItemAspect> externalIdentifiers1;
+      Assert.IsTrue(aspects[1].TryGetValue(ExternalIdentifierAspect.ASPECT_ID, out externalIdentifiers1));
+      Assert.IsTrue(TestUtils.HasExternalId(externalIdentifiers1, ExternalIdentifierAspect.SOURCE_IMDB, ExternalIdentifierAspect.TYPE_PERSON, actors[1].ImdbId));
+    }
+
+    protected MovieStub CreateTestMovieStub(IEnumerable<PersonStub> actors)
+    {
       return new MovieStub
       {
         Title = "Test Movie",
@@ -196,7 +208,7 @@ namespace Test.NfoMetadataExtractor
         Watched = true,
         LastPlayed = new DateTime(2000, 1, 31),
         Genres = new HashSet<string>(new[] { "Action", "Comedy" }),
-        Actors = actors,
+        Actors = new HashSet<PersonStub>(actors),
         Director = "TestDirector",
         DirectorImdb = "DirectorId",
         Credits = new HashSet<string>(new[] { "Writer1", "Writer2" }),
@@ -209,7 +221,7 @@ namespace Test.NfoMetadataExtractor
         Thmdb = 2,
         IdsTmdbId = 3,
         TmdbCollectionId = 4,
-        Sets = sets,
+        Sets = new HashSet<SetStub>(CreateTestSets()),
         Id = "Imdb1",
         Imdb = "Imdb2",
         IdsImdbId = "Imdb3",
@@ -223,6 +235,51 @@ namespace Test.NfoMetadataExtractor
         Ratings = new Dictionary<string, decimal> { { "tmdb", 2 }, { "imdb", 3 } },
         Votes = 10,
         Thumb = new byte[] { 0x01, 0x02, 0x03 }
+      };
+    }
+
+    protected IList<PersonStub> CreateTestActors()
+    {
+      return new List<PersonStub>
+      {
+        new PersonStub
+        {
+          Name = "Actor1",
+          Role = "Role1",
+          Biography = "Bio1",
+          Birthdate = new DateTime(2000,1,1),
+          Deathdate = new DateTime(2001,1,1),
+          Order = 1,
+          ImdbId = "tt11111",
+        },
+        new PersonStub
+        {
+          Name = "Actor2",
+          Role = "Role2",
+          Biography = "Bio2",
+          Birthdate = new DateTime(2002,1,1),
+          Deathdate = new DateTime(2003,1,1),
+          Order = 2,
+          ImdbId = "tt22222",
+        }};
+    }
+
+    protected IList<SetStub> CreateTestSets()
+    {
+      return new List<SetStub>
+      {
+        new SetStub
+        {
+          Name = "Set2",
+          Order = 2,
+          TmdbId = 5
+        },
+        new SetStub
+        {
+          Name = "Set1",
+          Order = 1,
+          TmdbId = 6
+        }
       };
     }
   }
