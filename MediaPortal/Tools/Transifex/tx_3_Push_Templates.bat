85cc0fa7d4093887b44e6fa5c9dd50daa4e89dce
morpheusxx <morpheus_xx@team-mediaportal.com>
2018-03-13 19:36:10 +0100

    Changed TX batch files to use new MSBUILD versions

--- MediaPortal/Tools/Transifex/tx_3_Push_Templates.bat
+++ MediaPortal/Tools/Transifex/tx_3_Push_Templates.bat
@@ -1,2 +1,2 @@
-
-@"%WINDIR%\Microsoft.NET\Framework\v4.0.30319\MSBUILD.exe" ..\..\Build\Transifex.targets /target:UploadEnglishLanguageFiles /l:FileLogger,Microsoft.Build.Engine;logfile=tx_3_Push_Templates.log
+call tx_0_initbuild.bat || exit 1
+%MB% ..\..\Build\Transifex.targets /target:UploadEnglishLanguageFiles /l:FileLogger,Microsoft.Build.Engine;logfile=tx_3_Push_Templates.log
