de620d238115a88ed909ec9e29e25671009d8c84
henso96 <henso_96@hotmail.com>
2018-07-22 21:38:25 +0200

    Convert obsolete client user to admin user

--- MediaPortal/Source/Core/MediaPortal.UI/Services/UserManagement/UserManagement.cs
+++ MediaPortal/Source/Core/MediaPortal.UI/Services/UserManagement/UserManagement.cs
@@ -128,10 +128,11 @@ namespace MediaPortal.UI.Services.UserManagement
 
         // First check if there is an "old" client profile with same name but different ID. This happens only for older versions.
         // This needs to be done to avoid unique constraint violations when creating the new profile by name.
+        // If client profile exists rename it and convert it to a user profile so it can be deleted or used otherwise.
         string profileName = SystemInformation.ComputerName;
         var existingProfile = await updm.GetProfileByNameAsync(profileName);
         if (existingProfile.Success && existingProfile.Result.ProfileId != systemId)
-          if (!await updm.UpdateProfileAsync(existingProfile.Result.ProfileId, profileName + "_old", existingProfile.Result.ProfileType, null))
+          if (!await updm.UpdateProfileAsync(existingProfile.Result.ProfileId, profileName + " Admin", UserProfileType.UserProfile, null))
             return null;
 
         // Create a login profile which uses the LocalSystemId and the associated ComputerName
