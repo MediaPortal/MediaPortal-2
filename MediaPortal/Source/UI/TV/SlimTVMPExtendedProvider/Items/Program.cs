4f93818d8bc23e1c813b3c181d85fcfe5f03d912
morpheusxx <morpheus_xx@team-mediaportal.com>
2018-10-09 21:13:25 +0200

    Updated SlimTVMPExtendedProvider to new async interfaces

--- MediaPortal/Source/UI/TV/SlimTVMPExtendedProvider/Items/Program.cs
+++ MediaPortal/Source/UI/TV/SlimTVMPExtendedProvider/Items/Program.cs
@@ -75,6 +75,8 @@ namespace MediaPortal.Plugins.SlimTv.Providers.Items
     public string Title { get; set; }
     public string Description { get; set; }
     public string Genre { get; set; }
+    public int EpgGenreId { get; set; }
+    public string EpgGenreColor { get; set; }
     public DateTime StartTime { get; set; }
     public DateTime EndTime { get; set; }
     public RecordingStatus RecordingStatus { get; set; }
