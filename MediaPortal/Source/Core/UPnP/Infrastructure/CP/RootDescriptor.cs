ec8d21ccd1a1dad5865313805fcb419d65b6a22e
morpheusxx <morpheus_xx@team-mediaportal.com>
2019-01-06 12:26:37 +0100

    Changed locking in UPnP library to ReaderWriterLockSlim

--- MediaPortal/Source/Core/UPnP/Infrastructure/CP/RootDescriptor.cs
+++ MediaPortal/Source/Core/UPnP/Infrastructure/CP/RootDescriptor.cs
@@ -87,7 +87,7 @@ namespace UPnP.Infrastructure.CP
     }
 
     /// <summary>
-    /// Gets the ynchronization object to use inside <c>lock()</c> statements when accessing data in this instance.
+    /// Gets the synchronization object to use inside <c>lock()</c> statements when accessing data in this instance.
     /// </summary>
     public object SyncObj
     {
