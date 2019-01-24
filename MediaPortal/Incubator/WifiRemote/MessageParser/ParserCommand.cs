4346c7edc0b47028d968427e36bd941df0753812
henso96 <henso_96@hotmail.com>
2019-01-24 22:01:09 +0100

    WifiRemote added missing number keys

--- MediaPortal/Incubator/WifiRemote/MessageParser/ParserCommand.cs
+++ MediaPortal/Incubator/WifiRemote/MessageParser/ParserCommand.cs
@@ -190,18 +190,18 @@ namespace MediaPortal.Plugins.WifiRemote.MessageParser
           button = Key.DVDMenu;
           break;
 
-        //case "0":
-        //case "1":
-        //case "2":
-        //case "3":
-        //case "4":
-        //case "5":
-        //case "6":
-        //case "7":
-        //case "8":
-        //case "9":
-        //  button = new Key(command.ToLower());
-        //  break;
+        case "0":
+        case "1":
+        case "2":
+        case "3":
+        case "4":
+        case "5":
+        case "6":
+        case "7":
+        case "8":
+        case "9":
+          button = new Key(command[0]);
+          break;
 
         case "clear":
           button = Key.Clear;
