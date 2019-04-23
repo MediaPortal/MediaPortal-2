3ff15fc39fe5a5c417c880fb057baf025eb88572
henso96 <henso_96@hotmail.com>
2019-04-23 23:45:46 +0200

    Added current news lists

--- MediaPortal/Source/UI/UiComponents/News/Models/CurrentNewsModel.cs
+++ MediaPortal/Source/UI/UiComponents/News/Models/CurrentNewsModel.cs
@@ -25,6 +25,8 @@
 using MediaPortal.UI.Presentation.Models;
 using MediaPortal.Common;
 using MediaPortal.Common.General;
+using MediaPortal.UI.Presentation.DataObjects;
+using System.Linq;
 
 namespace MediaPortal.UiComponents.News.Models
 {
@@ -34,8 +36,12 @@ namespace MediaPortal.UiComponents.News.Models
     /// Update interval for the current news item.
     /// </summary>
     const long NEWSITEM_UPDATE_INTERVAL = 10 * 1000;
+    const int NEWSITEM_TOP_COUNT = 10;
 
     protected readonly AbstractProperty _currentNewsItemProperty = new WProperty(typeof(NewsItem), new NewsItem());
+    protected ItemsList _currentNewsItems = new ItemsList();
+    protected ItemsList _currentTopNewsItems = new ItemsList();
+    protected bool _inited = false;
 
     /// <summary>
     /// Exposes the current news item to the skin.
@@ -54,9 +60,26 @@ namespace MediaPortal.UiComponents.News.Models
       set { _currentNewsItemProperty.SetValue(value); }
     }
 
+    /// <summary>
+    /// Current news items ordered by date
+    /// </summary>
+    public ItemsList CurrentNewsItems
+    {
+      get { return _currentNewsItems; }
+    }
+
+    /// <summary>
+    /// Current top news items ordered by date
+    /// </summary>
+    public ItemsList CurrentTopNewsItems
+    {
+      get { return _currentTopNewsItems; }
+    }
+
     public CurrentNewsModel()
       : base(true, 100)
     {
+      _inited = false;
     }
 
     protected override void Update()
@@ -69,8 +92,27 @@ namespace MediaPortal.UiComponents.News.Models
         {
           newNewsItem.CopyTo(CurrentNewsItem);
 
-          // Decrease interval once we have the first item
-          ChangeInterval(NEWSITEM_UPDATE_INTERVAL);
+          _currentNewsItems.Clear();
+          _currentTopNewsItems.Clear();
+          var items = newsCollector.GetAllNewsItems();
+          if (items?.Count > 0)
+          {
+            foreach (var newsItem in items.OrderByDescending(i => i.PublishDate))
+            {
+              _currentNewsItems.Add(newsItem);
+              if (_currentTopNewsItems.Count < NEWSITEM_TOP_COUNT)
+                _currentTopNewsItems.Add(newsItem);
+            }
+          }
+          _currentNewsItems.FireChange();
+          _currentTopNewsItems.FireChange();
+
+          if (!_inited)
+          {
+            // Decrease interval once we have the first item
+            ChangeInterval(NEWSITEM_UPDATE_INTERVAL);
+          }
+          _inited = true;
         }
       } 
     }
