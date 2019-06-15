bb887146c60e4695af0fdbf923707a0ae4770241
brownard <brownard@live.co.uk>
2019-06-15 21:37:13 +0100

    Nereus: Add mouse wheel scrolling to home content

--- MediaPortal/Source/UI/Skins/Nereus/Controls/HomeTilesScrollContentPresenter.cs
+++ MediaPortal/Source/UI/Skins/Nereus/Controls/HomeTilesScrollContentPresenter.cs
@@ -23,17 +23,53 @@
 #endregion
 
 using MediaPortal.UI.SkinEngine.Controls.Visuals;
-using SharpDX;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using MediaPortal.UI.SkinEngine.MpfElements.Input;
+using System.Windows.Forms;
 
 namespace MediaPortal.UiComponents.Nereus.Controls
 {
   public class HomeTilesScrollContentPresenter : AnimatedScrollContentPresenter
   {
+    protected const int TILE_SCROLL_PIXEL = 100;
+
+    public HomeTilesScrollContentPresenter()
+    {
+      PreviewMouseWheel += OnMouseWheel;      
+    }
+
+    public void OnMouseWheel(object sender, MouseWheelEventArgs e)
+    {
+      int scrollByLines = SystemInformation.MouseWheelScrollLines; // Use the system setting as default.
+
+      if (NumberOfVisibleLines != 0) // If ScrollControl can shown less items, use this as limit.
+        scrollByLines = NumberOfVisibleLines;
+
+      int numLines = e.NumDetents * scrollByLines;
+
+      if (numLines < 0)
+        ScrollRight(-1 * numLines);
+      else if (numLines > 0)
+        ScrollLeft(numLines);
+
+      e.Handled = true;
+    }
+
+    public bool ScrollLeft(int numLines)
+    {
+      if (base.IsViewPortAtLeft)
+        return false;
+      SetScrollOffset(_scrollOffsetX + numLines * TILE_SCROLL_PIXEL, _scrollOffsetY);
+      return true;
+    }
+
+    public bool ScrollRight(int numLines)
+    {
+      if (base.IsViewPortAtRight)
+        return false;
+      SetScrollOffset(_scrollOffsetX - numLines * TILE_SCROLL_PIXEL, _scrollOffsetY);
+      return true;
+    }
+
     public override bool IsViewPortAtLeft => true;
     public override bool IsViewPortAtRight => true;
     public override bool IsViewPortAtTop => true;
