cbd4ba61f088b3044e5d01ffd5da34ad9ef1b4bc
morpheusxx <morpheus_xx@team-mediaportal.com>
2019-08-28 20:24:35 +0200

    Copied LiveTvPlayer to LiveTvStreamPlayer which uses VideoPlayer as base (TsReader can't playback http stream)
    Fixed IsFile in SlimTvFsResourceAccessor

--- MediaPortal/Source/UI/TV/TvMosaic/SlimTv.TvMosaicProvider/TvMosaicProvider.cs
+++ MediaPortal/Source/UI/TV/TvMosaic/SlimTv.TvMosaicProvider/TvMosaicProvider.cs
@@ -4,9 +4,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MediaPortal.Common;
 using MediaPortal.Common.Async;
+using MediaPortal.Common.General;
+using MediaPortal.Common.Logging;
+using MediaPortal.Common.MediaManagement;
+using MediaPortal.Common.MediaManagement.DefaultItemAspects;
 using MediaPortal.Plugins.SlimTv.Interfaces;
 using MediaPortal.Plugins.SlimTv.Interfaces.Items;
+using MediaPortal.Plugins.SlimTv.Interfaces.LiveTvMediaItem;
+using MediaPortal.Plugins.SlimTv.Interfaces.ResourceProvider;
 using MediaPortal.Plugins.SlimTv.Interfaces.UPnP.Items;
 using TvMosaic.API;
 using MPChannel = MediaPortal.Plugins.SlimTv.Interfaces.UPnP.Items.Channel;
@@ -18,12 +25,15 @@ namespace SlimTv.TvMosaicProvider
     public string TvMosaicId { get; set; }
   }
 
-  public class TvMosaicProvider : ITvProvider, IChannelAndGroupInfoAsync
+  public class TvMosaicProvider : ITvProvider, IChannelAndGroupInfoAsync, ITimeshiftControlAsync, IProgramInfoAsync
   {
-    HttpDataProvider _dvbLink;
+    private static readonly string LOCAL_SYSTEM = SystemName.LocalHostName;
+    private HttpDataProvider _dvbLink;
+    private readonly object _syncObj = new object();
     private readonly IDictionary<string, int> _idMapping = new ConcurrentDictionary<string, int>();
     private readonly IDictionary<IChannelGroup, List<IChannel>> _channelGroups = new ConcurrentDictionary<IChannelGroup, List<IChannel>>();
     private readonly IList<IChannel> _mpChannels = new List<IChannel>();
+    private readonly Dictionary<int, IChannel> _tunedChannels = new Dictionary<int, IChannel>();
 
     public bool Init()
     {
@@ -51,37 +61,47 @@ namespace SlimTv.TvMosaicProvider
 
     private async Task<bool> LoadChannels()
     {
-      if (_channelGroups.Any() || _mpChannels.Any())
-        return true;
-      DVBLinkResponse<Channels> channels = await _dvbLink.GetChannels(new ChannelsRequest());
-      foreach (var channel in channels.Result)
+      lock (_syncObj)
       {
-        var mappedId = GetId(channel.Id);
-        IChannel mpChannel = new TvMosaicChannel
+        if (_channelGroups.Any() || _mpChannels.Any())
+          return true;
+      }
+
+      DVBLinkResponse<Channels> channels = await _dvbLink.GetChannels(new ChannelsRequest());
+      lock (_syncObj)
+      {
+        foreach (var channel in channels.Result)
         {
-          TvMosaicId = channel.Id,
-          Name = channel.Name,
-          ChannelId = mappedId,
-          MediaType = MediaType.TV,
-          ChannelNumber = channel.Number
-        };
-        _mpChannels.Add(mpChannel);
+          var mappedId = GetId(channel.Id);
+          IChannel mpChannel = new TvMosaicChannel
+          {
+            TvMosaicId = channel.Id,
+            Name = channel.Name,
+            ChannelId = mappedId,
+            MediaType = MediaType.TV,
+            ChannelNumber = channel.Number
+          };
+          _mpChannels.Add(mpChannel);
+        }
       }
 
       var favorites = await _dvbLink.GetFavorites(new FavoritesRequest());
-
-      foreach (var favorite in favorites.Result)
+      lock (_syncObj)
       {
-        var groupId = favorite.Id.ToString();
-        IChannelGroup group = new ChannelGroup
+        foreach (var favorite in favorites.Result)
         {
-          Name = favorite.Name,
-          ChannelGroupId = GetId(groupId)
-        };
+          var groupId = favorite.Id.ToString();
+          IChannelGroup group = new ChannelGroup
+          {
+            Name = favorite.Name,
+            ChannelGroupId = GetId(groupId)
+          };
 
-        IEnumerable<IChannel> groupChannels = _mpChannels.OfType<TvMosaicChannel>().Where(c => favorite.Channels.Contains(c.TvMosaicId));
-        _channelGroups[group] = new List<IChannel>(groupChannels);
+          IEnumerable<IChannel> groupChannels = _mpChannels.OfType<TvMosaicChannel>().Where(c => favorite.Channels.Contains(c.TvMosaicId));
+          _channelGroups[group] = new List<IChannel>(groupChannels);
+        }
       }
+
       return true;
     }
 
@@ -121,5 +141,115 @@ namespace SlimTv.TvMosaicProvider
     public int SelectedChannelGroupId { get; set; } = 0;
 
     #endregion
+
+    #region IProgramInfoAsync
+
+    public async Task<AsyncResult<IProgram[]>> GetNowNextProgramAsync(IChannel channel)
+    {
+      return new AsyncResult<IProgram[]>(false, null);
+    }
+
+    public async Task<AsyncResult<IDictionary<int, IProgram[]>>> GetNowAndNextForChannelGroupAsync(IChannelGroup channelGroup)
+    {
+      return new AsyncResult<IDictionary<int, IProgram[]>>(false, null);
+    }
+
+    public async Task<AsyncResult<IList<IProgram>>> GetProgramsAsync(IChannel channel, DateTime @from, DateTime to)
+    {
+      return new AsyncResult<IList<IProgram>>(false, null);
+    }
+
+    public async Task<AsyncResult<IList<IProgram>>> GetProgramsAsync(string title, DateTime @from, DateTime to)
+    {
+      return new AsyncResult<IList<IProgram>>(false, null);
+    }
+
+    public async Task<AsyncResult<IList<IProgram>>> GetProgramsGroupAsync(IChannelGroup channelGroup, DateTime @from, DateTime to)
+    {
+      return new AsyncResult<IList<IProgram>>(false, null);
+    }
+
+    public async Task<AsyncResult<IChannel>> GetChannelAsync(IProgram program)
+    {
+      return new AsyncResult<IChannel>(false, null);
+    }
+
+    public bool GetProgram(int programId, out IProgram program)
+    {
+      program = null;
+      return false;
+    }
+
+    #endregion
+
+    public String GetTimeshiftUserName(int slotIndex)
+    {
+      return String.Format("STC_{0}_{1}", LOCAL_SYSTEM, slotIndex);
+    }
+
+    public async Task<AsyncResult<MediaItem>> StartTimeshiftAsync(int slotIndex, IChannel channel)
+    {
+      try
+      {
+        var tvMosaicChannel = _mpChannels.OfType<TvMosaicChannel>().FirstOrDefault(c => c.ChannelId == channel.ChannelId);
+        var serverAddress = "127.0.0.1";
+        Transcoder transcoder = null;
+        var reqStream = new RequestStream(serverAddress, tvMosaicChannel.TvMosaicId, GetTimeshiftUserName(slotIndex), "raw_http_timeshift", transcoder);
+        DVBLinkResponse<Streamer> strm = await _dvbLink.PlayChannel(reqStream);
+        if (strm.Status == StatusCode.STATUS_OK)
+        {
+          var streamUrl = strm.Result.Url;
+
+          _tunedChannels[slotIndex] = channel;
+
+          // assign a MediaItem, can be null if streamUrl is the same.
+          var timeshiftMediaItem = CreateMediaItem(slotIndex, streamUrl, channel);
+          return new AsyncResult<MediaItem>(true, timeshiftMediaItem);
+        }
+      }
+      catch (Exception ex)
+      {
+        ServiceRegistration.Get<ILogger>().Error("TvMosaic: error playing channel {0}", ex, channel.Name);
+      }
+      return new AsyncResult<MediaItem>(false, null);
+    }
+
+    public async Task<bool> StopTimeshiftAsync(int slotIndex)
+    {
+      var request = new StopStream(GetTimeshiftUserName(slotIndex));
+      var result = await _dvbLink.StopStream(request);
+      return true;
+    }
+
+    public IChannel GetChannel(int slotIndex)
+    {
+      return _tunedChannels.TryGetValue(slotIndex, out IChannel channel) ? channel : null;
+    }
+
+
+    public MediaItem CreateMediaItem(int slotIndex, string streamUrl, IChannel channel)
+    {
+      LiveTvMediaItem tvStream = SlimTvMediaItemBuilder.CreateMediaItem(slotIndex, streamUrl, channel);
+      if (tvStream != null)
+      {
+        IList<MultipleMediaItemAspect> providerAspects;
+        if (MediaItemAspect.TryGetAspects(tvStream.Aspects, ProviderResourceAspect.Metadata, out providerAspects))
+        {
+          var providerResourceAspect = providerAspects.First();
+          providerResourceAspect.SetAttribute(ProviderResourceAspect.ATTR_MIME_TYPE, LiveTvMediaItem.MIME_TYPE_TV_STREAM);
+        }
+        // Add program infos to the LiveTvMediaItem
+        //IProgram currentProgram;
+        //if (GetCurrentProgram(channel, out currentProgram))
+        //  tvStream.AdditionalProperties[LiveTvMediaItem.CURRENT_PROGRAM] = currentProgram;
+
+        //IProgram nextProgram;
+        //if (GetNextProgram(channel, out nextProgram))
+        //  tvStream.AdditionalProperties[LiveTvMediaItem.NEXT_PROGRAM] = nextProgram;
+
+        return tvStream;
+      }
+      return null;
+    }
   }
 }
