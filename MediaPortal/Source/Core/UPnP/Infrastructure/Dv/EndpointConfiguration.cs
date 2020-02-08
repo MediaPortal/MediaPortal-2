0c5cf09b6c7b2c07d8c1d6826adfcb9dee3d9d7d
morpheusxx <morpheus_xx@team-mediaportal.com>
2020-02-08 15:27:52 +0100

    Fixed signed/unsigned conflict when handling configid in notify messages (negative value were generated and caused parsing to uint to fail)

--- MediaPortal/Source/Core/UPnP/Infrastructure/Dv/EndpointConfiguration.cs
+++ MediaPortal/Source/Core/UPnP/Infrastructure/Dv/EndpointConfiguration.cs
@@ -80,7 +80,7 @@ namespace UPnP.Infrastructure.Dv
     protected IDictionary<DvService, ServicePaths> _servicePaths = new Dictionary<DvService, ServicePaths>();
 
     protected ICollection<EventSubscription> _eventSubscriptions = new List<EventSubscription>();
-    protected Int32 _configId = 0;
+    protected UInt32 _configId = 0;
 
     /// <summary>
     /// Socket which is used to a) send unicast and multicast messages and b) receive unicast messages for the SSDP protocol.
@@ -256,7 +256,7 @@ namespace UPnP.Infrastructure.Dv
     /// UPnP CONFIGID. Contains a value indicating the current UPnP server configuration.
     /// Must be increased when the configuration changes. Must be in the range from 0 to 16777215 (2^24-1).
     /// </summary>
-    public Int32 ConfigId
+    public UInt32 ConfigId
     {
       get { return _configId; }
       internal set { _configId = value; }
