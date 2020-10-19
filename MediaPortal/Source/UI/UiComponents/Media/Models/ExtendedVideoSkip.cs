883b0a9a93b091c06d8db0ed0ccc75928191d260
morpheusxx <morpheus_xx@team-mediaportal.com>
2020-10-19 10:04:56 +0200

    Added feature to do instant skips in seconds (instead of relative percents). Option can be enabled or disabled

--- MediaPortal/Source/UI/UiComponents/Media/Models/ExtendedVideoSkip.cs
+++ MediaPortal/Source/UI/UiComponents/Media/Models/ExtendedVideoSkip.cs
@@ -93,7 +93,7 @@ namespace MediaPortal.UiComponents.Media.Models
     }
 
     /// <summary>
-    /// Called from the skin if the user invokes the "SkipStepBackwar" action. It will switch between the available skip steps
+    /// Called from the skin if the user invokes the "SkipStepBackward" action. It will switch between the available skip steps
     /// and execute the skip after the skip timer is elapsed.
     /// </summary>
     public void SkipStepBackward()
@@ -103,7 +103,7 @@ namespace MediaPortal.UiComponents.Media.Models
 
     /// <summary>
     /// Called from the skin if the user invokes the "InstantSkip" action. This will start the InstantSkip in the
-    /// underlaying player.
+    /// underlying player.
     /// </summary>
     public void InstantSkipForward()
     {
@@ -113,12 +113,15 @@ namespace MediaPortal.UiComponents.Media.Models
 
       ClearSkipTimer();
       MediaModelSettings settings = ServiceRegistration.Get<ISettingsManager>().Load<MediaModelSettings>();
-      pc.InstantSkip((int) settings.InstantSkipPercent);
+      if (settings.PreferInstantSkipSeconds)
+        pc.SkipRelative(TimeSpan.FromSeconds(settings.InstantSkipSeconds));
+      else
+        pc.InstantSkip((int)settings.InstantSkipPercent);
     }
 
     /// <summary>
     /// Called from the skin if the user invokes the "InstantSkip" action. This will start the InstantSkip in the
-    /// underlaying player.
+    /// underlying player.
     /// </summary>
     public void InstantSkipBackward()
     {
@@ -128,7 +131,10 @@ namespace MediaPortal.UiComponents.Media.Models
 
       ClearSkipTimer();
       MediaModelSettings settings = ServiceRegistration.Get<ISettingsManager>().Load<MediaModelSettings>();
-      pc.InstantSkip(-(int) settings.InstantSkipPercent);
+      if (settings.PreferInstantSkipSeconds)
+        pc.SkipRelative(TimeSpan.FromSeconds(-settings.InstantSkipSeconds));
+      else
+        pc.InstantSkip(-(int) settings.InstantSkipPercent);
     }
 
     #endregion
---
+++ MediaPortal/Source/UI/UiComponents/Media/Settings/Configuration/PreferInstantSkipSeconds.cs
@@ -0,0 +1,46 @@
+#region Copyright (C) 2007-2018 Team MediaPortal
+
+/*
+    Copyright (C) 2007-2018 Team MediaPortal
+    http://www.team-mediaportal.com
+
+    This file is part of MediaPortal 2
+
+    MediaPortal 2 is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    MediaPortal 2 is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with MediaPortal 2. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#endregion
+
+using MediaPortal.Common;
+using MediaPortal.Common.Configuration.ConfigurationClasses;
+using MediaPortal.UI.Presentation.Players;
+
+namespace MediaPortal.UiComponents.Media.Settings.Configuration
+{
+  public class PreferInstantSkipSeconds : YesNo
+  {
+    public override void Load()
+    {
+      _yes = SettingsManager.Load<MediaModelSettings>().PreferInstantSkipSeconds;
+    }
+
+    public override void Save()
+    {
+      base.Save();
+      MediaModelSettings settings = SettingsManager.Load<MediaModelSettings>();
+      settings.PreferInstantSkipSeconds = _yes;
+      SettingsManager.Save(settings);
+    }
+  }
+}
