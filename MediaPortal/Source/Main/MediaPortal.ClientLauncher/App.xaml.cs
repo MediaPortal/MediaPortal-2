dcefdd535c00e00a888ae3835475f0cf872d3ba6
morpheusxx <morpheus_xx@team-mediaportal.com>
2021-01-24 15:30:09 +0100

    Ported Client Launcher to NET5 / WPF, as the former NotifyIcon of WinForms does no longer support a context menu

---
+++ MediaPortal/Source/Main/MediaPortal.ClientLauncher/App.xaml.cs
@@ -0,0 +1,109 @@
+﻿using Hardcodet.Wpf.TaskbarNotification;
+using MediaPortal.Common;
+using MediaPortal.Common.Logging;
+using MediaPortal.Utilities.Process;
+using System;
+using System.Threading;
+using System.Windows;
+
+namespace MediaPortal.Client.Launcher
+{
+  /// <summary>
+  /// Simple application. Check the XAML for comments.
+  /// </summary>
+  public partial class App : Application
+  {
+    private TaskbarIcon _notifyIcon;
+    private Mutex _mutex = null;
+
+    protected override void OnStartup(StartupEventArgs e)
+    {
+      base.OnStartup(e);
+
+      //create the notifyicon (it's a resource declared in NotifyIconResources.xaml
+      _notifyIcon = (TaskbarIcon)FindResource("NotifyIcon");
+
+
+      // Parse command line options
+      var mpOptions = new CommandLineOptions();
+      var parser = new CommandLine.Parser(with => with.HelpWriter = Console.Out);
+      parser.ParseArgumentsStrict(e.Args, mpOptions, () => Environment.Exit(1));
+
+      // Check if another instance is already running
+      if (SingleInstanceHelper.IsAlreadyRunning(ApplicationLauncher.MUTEX_ID, out _mutex))
+      {
+        _mutex = null;
+        // Stop current instance
+        Console.Out.WriteLine("Application already running.");
+        Environment.Exit(2);
+      }
+
+#if !DEBUG
+      string logPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData),
+                                    @"Team MediaPortal", "MP2-Client", "Log");
+#endif
+
+      //// todo: Make sure we're properly handling exceptions
+      //DispatcherUnhandledException += OnUnhandledException;
+      //AppDomain.CurrentDomain.UnhandledException += LauncherExceptionHandling.CurrentDomain_UnhandledException;
+
+      // Start core services
+      ILogger logger = null;
+      try
+      {
+        //FormDpiAwarenessExtension.TryEnableDPIAwareness();
+
+        // Check if user wants to override the default Application Data location.
+        ApplicationCore.RegisterVitalCoreServices(true, mpOptions.DataDirectory);
+
+        logger = ServiceRegistration.Get<ILogger>();
+
+#if !DEBUG
+        logPath = ServiceRegistration.Get<IPathManager>().GetPath("<LOG>");
+#endif
+      }
+      catch (Exception ex)
+      {
+        if (logger != null)
+          logger.Critical("Error starting application", ex);
+
+        ApplicationCore.DisposeCoreServices();
+
+        throw;
+      }
+
+      // Start application
+      logger.Info("Starting application");
+
+      try
+      {
+        if (ApplicationLauncher.TerminateProcess("ehtray"))
+          logger.Info("Terminating running instance(s) of ehtray.exe");
+
+        ApplicationLauncher.InitIpc();
+        ApplicationLauncher.InitMsgHandler();
+      }
+      catch (Exception ex)
+      {
+        logger.Critical("Error executing application", ex);
+      }
+    }
+
+    protected override void OnExit(ExitEventArgs e)
+    {
+      _notifyIcon.Dispose(); //the icon would clean up automatically, but this is cleaner
+
+      var logger = ServiceRegistration.Get<ILogger>();
+      logger.Info("Exiting...");
+
+      ApplicationLauncher.CloseMsgHandler();
+      ApplicationLauncher.CloseIpc();
+
+      // Release mutex for single instance
+      if (_mutex != null)
+        _mutex.ReleaseMutex();
+
+      base.OnExit(e);
+    }
+  }
+}
---
+++ MediaPortal/Source/Main/MediaPortal.ClientLauncher/NotifyIconViewModel.cs
@@ -0,0 +1,133 @@
+﻿using MediaPortal.Utilities.SystemAPI;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Forms;
+using System.Windows.Input;
+using Application = System.Windows.Application;
+
+namespace MediaPortal.Client.Launcher
+{
+  /// <summary>
+  /// Provides bindable properties and commands for the NotifyIcon. In this sample, the
+  /// view model is assigned to the NotifyIcon in XAML. Alternatively, the startup routing
+  /// in App.xaml.cs could have created this view model, and assigned it to the NotifyIcon.
+  /// </summary>
+  public class NotifyIconViewModel : INotifyPropertyChanged
+  {
+    #region Static fields
+
+    private bool _isAutoStartEnabled;
+
+    #endregion
+
+    #region Properties
+
+    private bool IsAutoStartEnabled
+    {
+      get => _isAutoStartEnabled;
+      set
+      {
+        _isAutoStartEnabled = value;
+        OnPropertyChanged();
+      }
+    }
+
+    //private bool UseX64
+    //{
+    //  get { return ServiceRegistration.Get<ISettingsManager>().Load<ClientLauncherSettings>().UseX64; }
+    //  set
+    //  {
+    //    var settingsManager = ServiceRegistration.Get<ISettingsManager>();
+    //    var clientLauncherSettings = settingsManager.Load<ClientLauncherSettings>();
+    //    clientLauncherSettings.UseX64 = value;
+    //    settingsManager.Save(clientLauncherSettings);
+    //    OnPropertyChanged();
+    //  }
+    //}
+
+    #endregion
+
+    #region Constructor
+
+    public NotifyIconViewModel()
+    {
+      IsAutoStartEnabled = !string.IsNullOrEmpty(WindowsAPI.GetAutostartApplicationPath(ApplicationLauncher.AUTOSTART_REGISTER_NAME, true));
+    }
+
+    #endregion
+
+    #region Commands to invoke from XAML
+
+    public ICommand StartClientCommand => new DelegateCommand { CommandAction = ApplicationLauncher.StartClient };
+
+    public ICommand RemoveFromAutoStartCommand
+    {
+      get
+      {
+        return new DelegateCommand
+        {
+          CanExecuteFunc = () => IsAutoStartEnabled,
+          CommandAction = () =>
+          {
+            IsAutoStartEnabled = false;
+            ApplicationLauncher.WriteAutostartAppEntryInRegistry(IsAutoStartEnabled);
+          }
+        };
+      }
+    }
+
+    public ICommand AddToAutoStartCommand
+    {
+      get
+      {
+        return new DelegateCommand
+        {
+          CanExecuteFunc = () => !IsAutoStartEnabled,
+          CommandAction = () =>
+          {
+            IsAutoStartEnabled = true;
+            ApplicationLauncher.WriteAutostartAppEntryInRegistry(IsAutoStartEnabled);
+          }
+        };
+      }
+    }
+
+    //public ICommand PreferX64Command
+    //{
+    //  get
+    //  {
+    //    return new DelegateCommand
+    //    {
+    //      CanExecuteFunc = () => SupportsX64,
+    //      CommandAction = () => { UseX64 = !UseX64; }
+    //    };
+    //  }
+    //}
+
+    /// <summary>
+    /// Shuts down the application.
+    /// </summary>
+    public ICommand ExitApplicationCommand
+    {
+      get
+      {
+        return new DelegateCommand
+        {
+          CommandAction = () =>
+          {
+            Application.Current.Shutdown();
+          }
+        };
+      }
+    }
+
+    #endregion
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+    {
+      PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+  }
+}
