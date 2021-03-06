a20f944b8db1685b91574d4b2e1923a9411db059
henso96 <henso_96@hotmail.com>
2021-03-07 00:40:51 +0100

    Allow movie/series search for recordings/videos

--- MediaPortal/Source/Extensions/MetadataExtractors/SeriesMetadataExtractor/SeriesMetadataExtractor.cs
+++ MediaPortal/Source/Extensions/MetadataExtractors/SeriesMetadataExtractor/SeriesMetadataExtractor.cs
@@ -63,6 +63,7 @@ namespace MediaPortal.Extensions.MetadataExtractors.SeriesMetadataExtractor
     public static Guid METADATAEXTRACTOR_ID = new Guid(METADATAEXTRACTOR_ID_STR);
 
     public const string MEDIA_CATEGORY_NAME_SERIES = "Series";
+    public const string MEDIA_CATEGORY_NAME_VIDEO = "Video";
     public const double MINIMUM_HOUR_AGE_BEFORE_UPDATE = 0.5;
 
     #endregion
@@ -365,13 +366,15 @@ namespace MediaPortal.Extensions.MetadataExtractors.SeriesMetadataExtractor
     {
       try
       {
-        if (!(searchCategories?.Contains(_category) ?? true))
+        if (!(searchCategories?.Intersect(new[] { MEDIA_CATEGORY_NAME_SERIES, MEDIA_CATEGORY_NAME_VIDEO }).Any() ?? true))
           return null;
 
         string searchData = null;
         var reimportAspect = MediaItemAspect.GetAspect(searchAspectData, ReimportAspect.Metadata);
         if (reimportAspect != null)
           searchData = reimportAspect.GetAttributeValue<string>(ReimportAspect.ATTR_SEARCH);
+        if (string.IsNullOrEmpty(searchData) && !searchAspectData.ContainsKey(EpisodeAspect.ASPECT_ID) && !searchAspectData.ContainsKey(SeriesAspect.ASPECT_ID))
+          return null;
 
         ServiceRegistration.Get<ILogger>().Debug("SeriesMetadataExtractor: Search aspects to use: '{0}'", string.Join(",", searchAspectData.Keys));
 
