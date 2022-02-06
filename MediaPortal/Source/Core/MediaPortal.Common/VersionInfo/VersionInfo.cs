e14affc1348f12ff422ff74e941b2eca58242b8a
brownard <brownard@live.co.uk>
2022-02-06 15:40:28 +0000

    MP2-672: Update build version

--- MediaPortal/Source/Core/MediaPortal.Common/VersionInfo/VersionInfo.cs
+++ MediaPortal/Source/Core/MediaPortal.Common/VersionInfo/VersionInfo.cs
@@ -5,10 +5,10 @@ using System.Reflection;
 // be deleted.
 [assembly: AssemblyCompany("Team MediaPortal")]
 [assembly: AssemblyProduct("MediaPortal 2")]
-[assembly: AssemblyCopyright("Copyright © Team MediaPortal 2007 - 2021")]
+[assembly: AssemblyCopyright("Copyright © Team MediaPortal 2007 - 2022")]
 // Note: Following two line will be modified by AssemblyInfoHelper in build process:
 // The version number will have format: 2.YY.MM.{BuildNumber}
-[assembly: AssemblyVersion("2.3.2101.13503")]
-[assembly: AssemblyFileVersion("2.3.2101.13503")]
+[assembly: AssemblyVersion("2.4.2202.13838")]
+[assembly: AssemblyFileVersion("2.4.2202.13838")]
 // Note: This line will be filled by AssemblyInfoHelper in build process.
-[assembly: AssemblyInformationalVersion("origin/WIP_2.4-d8fa66")]
+[assembly: AssemblyInformationalVersion("WIP_MP2-672_CustomInstaller_2.4-0c7ab9")]
