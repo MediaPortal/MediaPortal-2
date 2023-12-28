e2b3beecab86c414fb657d55e8d0e73eda94ad84
brownard <brownard@live.co.uk>
2023-12-28 21:56:09 +0000

    MP2-942: Always create LogViewerInstance form on the main thread to avoid the form installing a SynchronizationContext on a background thread

--- MediaPortal/Incubator/Diagnostics/Settings/Configuration/DiagnosticsSettingsOutputWindows.cs
+++ MediaPortal/Incubator/Diagnostics/Settings/Configuration/DiagnosticsSettingsOutputWindows.cs
@@ -22,15 +22,15 @@
 
 #endregion
 
-using System.Collections.Generic;
-using System.IO;
 using MediaPortal.Common;
 using MediaPortal.Common.Configuration.ConfigurationClasses;
-using MediaPortal.Common.General;
 using MediaPortal.Common.PathManager;
+using MediaPortal.UI.SkinEngine.SkinManagement;
 using MediaPortal.UiComponents.Diagnostics.Service;
 using MediaPortal.UiComponents.Diagnostics.Tools;
-using MediaPortal.UI.SkinEngine.SkinManagement;
+using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace MediaPortal.UiComponents.Diagnostics.Settings.Configuration
 {
@@ -40,33 +40,39 @@ namespace MediaPortal.UiComponents.Diagnostics.Settings.Configuration
 
     public override void Load()
     {
-      _yes = DiagnosticsHandler.LogViewerInstance.Visible;
+      SkinContext.Form.Invoke(new Action(()=> 
+      { 
+        _yes = DiagnosticsHandler.LogViewerInstance.Visible; 
+      }));
     }
 
     public override void Save()
     {
-      if (_yes)
+      SkinContext.Form.Invoke(new Action(() =>
       {
-        if (DiagnosticsHandler.LogViewerInstance.Visible)
-          return;
-
-        string logPath = ServiceRegistration.Get<IPathManager>().GetPath("<LOG>");
-        List<FileInfo> logsfiles = FileSorter.SortByLastWriteTime(logPath, "*.log");
-
-        foreach (var file in logsfiles)
+        if (_yes)
         {
-          DiagnosticsHandler.LogViewerInstance.AddLog(file.FullName);
+          if (DiagnosticsHandler.LogViewerInstance.Visible)
+            return;
+
+          string logPath = ServiceRegistration.Get<IPathManager>().GetPath("<LOG>");
+          List<FileInfo> logsfiles = FileSorter.SortByLastWriteTime(logPath, "*.log");
+
+          foreach (var file in logsfiles)
+          {
+            DiagnosticsHandler.LogViewerInstance.AddLog(file.FullName);
+          }
+
+          DiagnosticsHandler.LogViewerInstance.Show();
         }
+        else
+        {
+          if (!DiagnosticsHandler.LogViewerInstance.Visible)
+            return;
 
-        SkinContext.Form.Invoke(new ParameterlessMethod(DiagnosticsHandler.LogViewerInstance.Show));
-      }
-      else
-      {
-        if (!DiagnosticsHandler.LogViewerInstance.Visible)
-          return;
-
-        DiagnosticsHandler.LogViewerInstance.Dispose();
-      }
+          DiagnosticsHandler.LogViewerInstance.Dispose();
+        }
+      }));
     }
 
     #endregion Public Methods
