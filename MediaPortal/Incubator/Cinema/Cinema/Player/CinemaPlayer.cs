457efdee1276e25774f38c7d42785b752f432991
brownard <brownard@live.co.uk>
2024-01-25 22:07:08 +0000

    Cinema: Fix youtube video playback

--- MediaPortal/Incubator/Cinema/Cinema/Player/CinemaPlayer.cs
+++ MediaPortal/Incubator/Cinema/Cinema/Player/CinemaPlayer.cs
@@ -22,54 +22,143 @@
 
 #endregion
 
-using System;
-using DirectShow;
 using DirectShow.Helper;
 using MediaPortal.Common;
 using MediaPortal.Common.Logging;
+using MediaPortal.Common.ResourceAccess;
+using MediaPortal.UI.Players;
 using MediaPortal.UI.Players.Video;
 using MediaPortal.UI.Players.Video.Tools;
 using MediaPortal.UI.Presentation.Players;
+using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using YoutubeExplode;
+using YoutubeExplode.Videos.Streams;
 
 namespace Cinema.Player
 {
   public class CinemaPlayer : VideoPlayer, IUIContributorPlayer
   {
     public const string CINEMA_MIMETYPE = "cinema/stream";
-    private const string SOURCE_FILTER_NAME = "LAV Splitter Source";
 
-    protected void AddFileSource()
-    {
-      IBaseFilter sourceFilter = null;
-      try
-      {
-        sourceFilter = FilterGraphTools.AddFilterByName(_graphBuilder, FilterCategory.LegacyAmFilterCategory, SOURCE_FILTER_NAME);
-        FilterGraphTools.RenderOutputPins(_graphBuilder, sourceFilter);
-      }
-      finally
-      {
-        FilterGraphTools.TryRelease(ref sourceFilter);
-      }
-    }
+    protected DotNetStreamSourceFilter _videoStreamFilter;
+    protected Stream _videoStream;
 
-    protected override void AddSourceFilter()
-    {
-      base.AddSourceFilter();
-      // HD streams tend to come with a separate audio stream, check if that's the case and also add the audio stream to the graph
-      if(_mediaItem is CinemaMediaItem cinemaMediaItem && cinemaMediaItem.AudioUrl != null)
-      {
-        ServiceRegistration.Get<ILogger>().Debug("{0}: Initializing separate audio stream for network media item '{1}'", PlayerTitle, cinemaMediaItem.AudioUrl);
-
-        // try to render the url and let DirectShow choose the source filter
-        int hr = _graphBuilder.RenderFile(cinemaMediaItem.AudioUrl, null);
-        new HRESULT(hr).Throw();
-      }
-
-    }
+    protected DotNetStreamSourceFilter _audioStreamFilter;
+    protected Stream _audioStream;
 
     public Type UIContributorType
     {
       get { return typeof(CinemaUiContributor); }
+    }
+
+    protected override void AddSourceFilter()
+    {
+      var networkResourceAccessor = _resourceAccessor as INetworkResourceAccessor;
+      if (networkResourceAccessor == null || !TryAddYouTubeStreams(networkResourceAccessor.URL).Result)
+      {
+        base.AddSourceFilter();
+        return;
+      }
+
+      int hr;
+      using (DSFilter source2 = new DSFilter(_videoStreamFilter))
+        hr = source2.OutputPin.Render();
+      new HRESULT(hr).Throw();
+
+      if (_audioStreamFilter != null)
+      {
+        using (DSFilter source2 = new DSFilter(_audioStreamFilter))
+          hr = source2.OutputPin.Render();
+        new HRESULT(hr).Throw();
+      }
+    }
+
+    protected override void FreeCodecs()
+    {
+      if (_graphBuilder != null)
+      {
+        if (_videoStreamFilter != null)
+        {
+          _graphBuilder.RemoveFilter(_videoStreamFilter);
+          FilterGraphTools.TryDispose(ref _videoStreamFilter);
+          FilterGraphTools.TryDispose(ref _videoStream);
+
+        }
+        if (_audioStreamFilter != null)
+        {
+          _graphBuilder.RemoveFilter(_audioStreamFilter);
+          FilterGraphTools.TryDispose(ref _audioStreamFilter);
+          FilterGraphTools.TryDispose(ref _audioStream);
+        }
+      }
+
+      base.FreeCodecs();
+    }
+
+    private async Task<bool> TryAddYouTubeStreams(string url)
+    {
+      if (!IsYouTubeUrl(url))
+        return false;
+
+      var youtube = new YoutubeClient();
+      var streamManifest = await youtube.Videos.Streams.GetManifestAsync(url);
+
+      // Try and get the highest quality video stream
+      var videoStream = streamManifest.GetVideoStreams().TryGetWithHighestVideoQuality();
+      if (videoStream == null)
+      {
+        ServiceRegistration.Get<ILogger>().Error("{0}: Unable to find a video stream for YouTube url '{1}'", PlayerTitle, url);
+        return false;
+      }
+
+      // If the stream is muxed and therefore contains audio, simply add it and return
+      AddVideoStream(await youtube.Videos.Streams.GetAsync(videoStream), videoStream.Url);
+      if (videoStream is MuxedStreamInfo)
+        return true;
+
+      // else the video stream does not contain audio so try and get the highest quality audio stream.
+      // WebM audio streams don't seem to work so limit the restults to Mp4 streams
+      ServiceRegistration.Get<ILogger>().Debug("{0}: Initializing separate audio stream for Youtube url '{1}'", PlayerTitle, url);
+      var audioStream = streamManifest.GetAudioOnlyStreams().Where(s => s.Container == Container.Mp4).TryGetWithHighestBitrate();
+      if (audioStream == null)
+      {
+        ServiceRegistration.Get<ILogger>().Warn("{0}: Unable to find an audio stream for YouTube url '{1}'", PlayerTitle, url);
+        // There's still a video stream, so at least allow that to play
+        return true;
+      }
+
+      AddAudioStream(await youtube.Videos.Streams.GetAsync(audioStream), audioStream.Url);
+      return true;
+    }
+
+    protected void AddVideoStream(Stream stream, string fileName)
+    {
+      if (stream == null)
+        return;
+      _videoStream = stream;
+      _videoStreamFilter = new DotNetStreamSourceFilter();
+      _videoStreamFilter.SetSourceStream(_videoStream, fileName);
+      int hr = _graphBuilder.AddFilter(_videoStreamFilter, _videoStreamFilter.Name);
+      new HRESULT(hr).Throw();
+    }
+
+    protected void AddAudioStream(Stream stream, string fileName)
+    {
+      if (stream == null)
+        return;
+      _audioStream = stream;
+      _audioStreamFilter = new DotNetStreamSourceFilter();
+      _audioStreamFilter.SetSourceStream(_audioStream, fileName);
+      int hr = _graphBuilder.AddFilter(_audioStreamFilter, _audioStreamFilter.Name);
+      new HRESULT(hr).Throw();
+    }
+
+    private static bool IsYouTubeUrl(string url)
+    {
+      return url?.StartsWith("https://youtu.be/") ?? false;
     }
   }
 }
--- MediaPortal/Incubator/Cinema/Cinema/Player/CinemaPlayerHelper.cs
+++ MediaPortal/Incubator/Cinema/Cinema/Player/CinemaPlayerHelper.cs
@@ -32,10 +32,6 @@ using MediaPortal.Common.SystemResolver;
 using MediaPortal.UiComponents.Media.Models;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using YoutubeExplode;
-using YoutubeExplode.Videos.Streams;
 
 namespace Cinema.Player
 {
@@ -62,76 +58,15 @@ namespace Cinema.Player
       SingleMediaItemAspect mediaAspect = MediaItemAspect.GetOrCreateAspect(aspects, MediaAspect.Metadata);
       // VideoAspect is required to ensure the correct player is used
       SingleMediaItemAspect videoAspect = MediaItemAspect.GetOrCreateAspect(aspects, VideoAspect.Metadata);
-      var trailerUrl = TryGetDirectVideoUrl(trailer.Url).Result;
       providerResourceAspect.SetAttribute(ProviderResourceAspect.ATTR_TYPE, ProviderResourceAspect.TYPE_PRIMARY);
-      providerResourceAspect.SetAttribute(ProviderResourceAspect.ATTR_RESOURCE_ACCESSOR_PATH, RawUrlResourceProvider.ToProviderResourcePath(trailerUrl.videoUrl).Serialize());
+      providerResourceAspect.SetAttribute(ProviderResourceAspect.ATTR_RESOURCE_ACCESSOR_PATH, RawUrlResourceProvider.ToProviderResourcePath(trailer.Url).Serialize());
       providerResourceAspect.SetAttribute(ProviderResourceAspect.ATTR_SYSTEM_ID, ServiceRegistration.Get<ISystemResolver>().LocalSystemId);
       providerResourceAspect.SetAttribute(ProviderResourceAspect.ATTR_MIME_TYPE, CINEMA_MIMETYPE);
 
       mediaAspect.SetAttribute(MediaAspect.ATTR_TITLE, trailer.Title);
 
-      var mediaItem = new CinemaMediaItem(Guid.Empty, aspects, trailerUrl.audioUrl);
+      var mediaItem = new MediaItem(Guid.Empty, aspects);
       return mediaItem;
     }
-
-    private static async Task<(string videoUrl, string audioUrl)> TryGetDirectVideoUrl(string trailerUrl)
-    {
-      try
-      {
-        if (trailerUrl.StartsWith("https://youtu.be/"))
-        {
-          return await GetYoutubeUrl(trailerUrl);
-        }
-        else
-        {
-          return (trailerUrl, null);
-        }
-      }
-      catch (Exception e)
-      {
-        Console.WriteLine(e);
-      }
-
-      return ("", null);
-    }
-
-    /// <summary>
-    /// Attempts to get the direct url for the highest quality youtube video. If the url points to a muxed stream of video and audio
-    /// then only videoUrl in the returned tuple will be populated and audioUrl will be <c>null</c>; else if the video and audio
-    /// streams are separate then both videoUrl and audioUrl will be populated with the urls to the video and audio streams respectively.
-    /// </summary>
-    /// <param name="trailerUrl">The url to a youtube video.</param>
-    /// <returns>A tuple containing links to either the muxed video stream or separate streams for video and audio.</returns>
-    private static async Task<(string videoUrl, string audioUrl)> GetYoutubeUrl(string trailerUrl)
-    {
-      var youtube = new YoutubeClient();
-
-      var streamManifest = await youtube.Videos.Streams.GetManifestAsync(trailerUrl);
-
-      // Try and get the highest quality video stream
-      var videoStream = streamManifest.GetVideoStreams().TryGetWithHighestVideoQuality();
-      if (videoStream == null)
-        return (null, null);
-
-      // If the stream is muxed and therefore contains audio, simply return it
-      if (videoStream is MuxedStreamInfo)
-        return (videoStream.Url, null);
-
-      // else the video stream does not contain audio so try and get the highest quality audio stream.
-      // WebM audio streams don't seem to work so limit the restults to Mp4 streams
-      var audioStream = streamManifest.GetAudioOnlyStreams().Where(s => s.Container == Container.Mp4).TryGetWithHighestBitrate();
-      return (videoStream.Url, audioStream.Url);
-    }
-  }
-
-  public class CinemaMediaItem : MediaItem
-  {
-    public CinemaMediaItem(Guid mediaItemId, IDictionary<Guid, IList<MediaItemAspect>> aspects, string audioUrl = null)
-      : base(mediaItemId, aspects)
-    {
-      AudioUrl = audioUrl;
-    }
-
-    public string AudioUrl { get; }
   }
 }
